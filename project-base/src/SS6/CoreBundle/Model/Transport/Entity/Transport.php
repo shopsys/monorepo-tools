f3fb0165707bd5d9bee94623effbadfc53e7b44e
Petr Kadlec <kadlec@netdevelo.cz>
2014-04-04 11:20:24 +0200

    create transport entity with custom repository

---
+++ project-base/src/SS6/CoreBundle/Model/Transport/Entity/Transport.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace SS6\CoreBundle\Model\Transport\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="transports")
+ * @ORM\Entity(repositoryClass="SS6\CoreBundle\Model\Transport\Repository\TransportRepository")
+ */
+class Transport {
+
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="IDENTITY")
+	 */
+	private $id;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $name;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(type="decimal", precision=20, scale=6)
+	 */
+	private $price;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(type="text", nullable=true)
+	 */
+	private $description;
+
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(type="boolean")
+	 */
+	private $hidden;
+	
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(type="boolean")
+	 */
+	private $deleted;
+	
+	public function __construct() {
+		$this->hidden = false;
+		$this->deleted = false;
+	}
+
+	/**
+	 * @return integer 
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * @param string $name
+	 */
+	public function setName($name) {
+		$this->name = $name;
+	}
+
+	/**
+	 * @return string 
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	 * @param string $price
+	 */
+	public function setPrice($price) {
+		$this->price = $price;
+	}
+
+	/**
+	 * @return string 
+	 */
+	public function getPrice() {
+		return $this->price;
+	}
+
+	/**
+	 * @param string|null $description
+	 */
+	public function setDescription($description) {
+		$this->description = $description;
+	}
+
+	/**
+	 * @return string|null 
+	 */
+	public function getDescription() {
+		return $this->description;
+	}
+
+	/**
+	 * @param boolean $hide
+	 */
+	public function setHidden($hide) {
+		$this->hidden = $hide;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function isHidden() {
+		return $this->hidden;
+	}
+	
+	/**
+	 * @return boolean
+	 */
+	public function idDeleted() {
+		return $this->deleted;
+	}
+
+	/**
+	 * @param boolean $deleted
+	 */
+	public function setDeleted($deleted) {
+		$this->deleted = $deleted;
+	}
+}
---
+++ project-base/src/SS6/CoreBundle/Model/Transport/Repository/TransportRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SS6\CoreBundle\Model\Transport\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class TransportRepository extends EntityRepository {
+	
+}
