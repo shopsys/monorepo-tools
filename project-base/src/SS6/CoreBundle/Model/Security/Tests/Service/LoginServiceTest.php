e55f153ee7c0e45f470aafffd65033cbcc666a11
Petr Kadlec <kadlec@netdevelo.cz>
2014-04-04 09:17:35 +0200

    test for LoginService

--- project-base/src/SS6/CoreBundle/Model/Security/Tests/Service/LoginServiceTest.php
+++ project-base/src/SS6/CoreBundle/Model/Security/Tests/Service/LoginServiceTest.php
@@ -1,32 +1,59 @@
 <?php
 
-namespace SS6\CoreBundle\Model\Security\Service;
+namespace SS6\CoreBundle\Model\Security\Tests\Service;
 
-use SS6\CoreBundle\Model\Security\Exception\LoginFailedException;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\SecurityContext;
+use PHPUnit_Framework_TestCase;
+use SS6\CoreBundle\Model\Security\Service\LoginService;
+use StdClass;
 
-class LoginServiceTest {
+class LoginServiceTest extends PHPUnit_Framework_TestCase {
 
-	/**
-	 * @param \Symfony\Component\HttpFoundation\Request $request
-	 * @return bool
-	 */
-	public function checkLoginProcess(Request $request) {
-		$error = null;
-		
-		if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-			$error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-		} else {
-			$session = $request->getSession();
-			$error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-			$session->remove(SecurityContext::AUTHENTICATION_ERROR);
-		}
+	public function testCheckLoginProcessWithRequestError() {
+		$loginService = new LoginService();
 
-		if ($error !== null) {
-			throw new LoginFailedException('Login failed.');
-		}
-		
-		return true;
+		$requestMock = $this->getMock('\Symfony\Component\HttpFoundation\Request');
+		$requestMock->expects($this->never())->method('getSession');
+
+		$requestMock->attributes = $this->getMock('\Symfony\Component\HttpFoundation\ParameterBag');
+		$requestMock->attributes->expects($this->once())->method('has')->will($this->returnValue(true));
+		$requestMock->attributes->expects($this->once())->method('get')->will($this->returnValue(new StdClass));
+
+		$this->setExpectedException('SS6\CoreBundle\Model\Security\Exception\LoginFailedException');
+		$loginService->checkLoginProcess($requestMock);
+	}
+
+	public function testCheckLoginProcessWithSessionError() {
+		$loginService = new LoginService();
+
+		$sessionMock = $this->getMock('\Symfony\Component\HttpFoundation\Session\SessionInterface');
+		$sessionMock->expects($this->atLeastOnce())->method('get')->will($this->returnValue(new StdClass));
+		$sessionMock->expects($this->atLeastOnce())->method('remove');
+
+		$requestMock = $this->getMock('\Symfony\Component\HttpFoundation\Request');
+		$requestMock->expects($this->once())->method('getSession')->will($this->returnValue($sessionMock));
+
+		$requestMock->attributes = $this->getMock('\Symfony\Component\HttpFoundation\ParameterBag');
+		$requestMock->attributes->expects($this->once())->method('has')->will($this->returnValue(false));
+		$requestMock->attributes->expects($this->never())->method('get');
+
+		$this->setExpectedException('SS6\CoreBundle\Model\Security\Exception\LoginFailedException');
+		$loginService->checkLoginProcess($requestMock);
+	}
+
+	public function testCheckLoginProcessWithoutSessionError() {
+		$loginService = new LoginService();
+
+		$sessionMock = $this->getMock('\Symfony\Component\HttpFoundation\Session\SessionInterface');
+		$sessionMock->expects($this->once())->method('get')->will($this->returnValue(null));
+		$sessionMock->expects($this->once())->method('remove');
+
+		$requestMock = $this->getMock('\Symfony\Component\HttpFoundation\Request');
+		$requestMock->expects($this->once())->method('getSession')->will($this->returnValue($sessionMock));
+
+		$requestMock->attributes = $this->getMock('\Symfony\Component\HttpFoundation\ParameterBag');
+		$requestMock->attributes->expects($this->once())->method('has')->will($this->returnValue(false));
+		$requestMock->attributes->expects($this->never())->method('get');
+
+		$this->assertEquals(true, $loginService->checkLoginProcess($requestMock));
 	}
 }
