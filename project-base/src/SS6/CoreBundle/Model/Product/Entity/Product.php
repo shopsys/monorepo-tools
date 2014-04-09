d97a551726efe6e8b8f417822c1099c0a8918409
Petr Kadlec <kadlec@netdevelo.cz>
2014-04-09 10:44:22 +0200

    change id generate strategy for product entity

--- project-base/src/SS6/CoreBundle/Model/Product/Entity/Product.php
+++ project-base/src/SS6/CoreBundle/Model/Product/Entity/Product.php
@@ -19,7 +19,7 @@ class Product {
 	 *
 	 * @ORM\Column(name="id", type="integer")
 	 * @ORM\Id
-	 * @ORM\GeneratedValue(strategy="AUTO")
+	 * @ORM\GeneratedValue(strategy="IDENTITY")
 	 */
 	private $id;
 
@@ -246,6 +246,9 @@ class Product {
 		$this->hidden = $hidden;
 	}
 	
+	/**
+	 * @return boolean
+	 */
 	public function isVisible() {
 		$datetime = new DateTime();
 		
