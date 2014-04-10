64f8c9570ac6bcdd12d75e2d44dbbd7240b80052
seedya <pavel@sidzina.cz>
2014-04-10 10:25:09 +0200

    setting grunt file, splitting admin and frontend tasks, setting autoreaload after saving less file

--- project-base/Gruntfile.js
+++ project-base/Gruntfile.js
@@ -6,14 +6,34 @@ module.exports = function(grunt) {
 
 
 		less: {
-			default: {
+			admin: {
 				files: {
-					"web/assets/frontend/index.css": "web/assets/frontend/styles/main.less"
+					'web/assets/admin/index.css': 'web/assets/admin/styles/bootstrap.less'
 				}
 			},
-			minified: {
+
+			admin_mini: {
 				files: {
-					'web/assets/frontend/index.min.css': 'web/assets/frontend/styles/main.less' 
+					'web/assets/admin/index.min.css': 'web/assets/admin/index.css' 
+				},
+				options: {
+					compress: true,
+					sourceMap: true,
+					sourceMapFilename: 'web/assets/admin/index.css.map',
+					sourceMapRootpath: 'web/assets/admin/',
+					sourceMapURL: 'web/assets/admin/index.css.map'
+				}
+			},
+
+			frontend: {
+				files: {
+					'web/assets/frontend/index.css': 'web/assets/frontend/styles/main.less'
+				}
+			},
+
+			frontend_mini: {
+				files: {
+					'web/assets/frontend/index.min.css': 'web/assets/frontend/index.css' 
 				},
 				options: {
 					compress: true,
@@ -27,34 +47,60 @@ module.exports = function(grunt) {
 
 
 		legacssy: {
-			default: {
+			admin: {
 				options: {
 					legacyWidth: 641,
 					matchingOnly: true,
 					overridesOnly: true
 				},
 				files: {
-			  		'web/assets/frontend/index-ie8.css': 'web/assets/frontend/index.css'
+						'web/assets/admin/index-ie8.css': 'web/assets/admin/index.css'
+				}
+			},
+			frontend: {
+				options: {
+					legacyWidth: 641,
+					matchingOnly: true,
+					overridesOnly: true
+				},
+				files: {
+						'web/assets/frontend/index-ie8.css': 'web/assets/frontend/index.css'
 				}
 			}
 		},
 
 
-		// 'grunt watch' hlida zmeny v souborech a provadi vyseuvedene tasky
 		watch: {
-			files: ['web/assets/frontend/styles/**/*.less'],
-			tasks: ['less', 'legacssy'],
-			options: {
-				livereload: true,
+			frontend: {
+				files: ['web/assets/frontend/styles/**/*.less'],
+				tasks: ['frontend'],
+				options: {
+					livereload: true,
+				}
+			},
+			admin: {
+				files: ['web/assets/admin/styles/**/*.less'],
+				tasks: ['admin'],
+				options: {
+					livereload: true,
+				}
 			}
+			
 		}
 
+
 	});
+
+	
 
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-legacssy');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ["less", "legacssy"]);
-	grunt.registerTask('minified', ["less"]);
+
+	grunt.registerTask('default', ['less', 'legacssy']);
+
+
+	grunt.registerTask('frontend', ['less:frontend', 'less:frontend_mini', 'legacssy:frontend']);
+	grunt.registerTask('admin', ['less:admin', 'less:admin_mini','legacssy:admin' ]);
 };
