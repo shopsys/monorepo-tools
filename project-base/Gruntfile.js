f567daf00ef4e25e23fb16640094c2876f4eace1
Miroslav Sustek <miroslav.sustek@netdevelo.cz>
2014-04-11 14:45:25 +0200

    creating sourcemaps, deleting minified css files, setting original css file with minification

--- project-base/Gruntfile.js
+++ project-base/Gruntfile.js
@@ -4,43 +4,32 @@ module.exports = function(grunt) {
 
 		pkg: grunt.file.readJSON('package.json'),
 
-
 		less: {
 			admin: {
 				files: {
 					'web/assets/admin/index.css': 'web/assets/admin/styles/bootstrap.less'
-				}
-			},
-
-			admin_mini: {
-				files: {
-					'web/assets/admin/index.min.css': 'web/assets/admin/index.css' 
 				},
 				options: {
 					compress: true,
 					sourceMap: true,
 					sourceMapFilename: 'web/assets/admin/index.css.map',
-					sourceMapRootpath: 'web/assets/admin/',
-					sourceMapURL: 'web/assets/admin/index.css.map'
+					sourceMapRootpath: '../../',
+					sourceMapBasepath: 'web/',
+					sourceMapURL: 'http://shopsys6:8080/assets/admin/index.css.map'
 				}
 			},
 
 			frontend: {
 				files: {
 					'web/assets/frontend/index.css': 'web/assets/frontend/styles/main.less'
-				}
-			},
-
-			frontend_mini: {
-				files: {
-					'web/assets/frontend/index.min.css': 'web/assets/frontend/index.css' 
 				},
 				options: {
 					compress: true,
 					sourceMap: true,
 					sourceMapFilename: 'web/assets/frontend/index.css.map',
-					sourceMapRootpath: 'web/assets/frontend/',
-					sourceMapURL: 'web/assets/frontend/index.css.map' //url primo
+					sourceMapRootpath: '../../',
+					sourceMapBasepath: 'web/',
+					sourceMapURL: 'http://shopsys6:8080/assets/frontend/index.css.map'
 				}
 			}
 		},
@@ -64,7 +53,7 @@ module.exports = function(grunt) {
 					overridesOnly: true
 				},
 				files: {
-						'web/assets/frontend/index-ie8.css': 'web/assets/frontend/index.css'
+					'web/assets/frontend/index-ie8.css': 'web/assets/frontend/index.css'
 				}
 			}
 		},
@@ -90,17 +79,13 @@ module.exports = function(grunt) {
 
 
 	});
-
 	
-
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-legacssy');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-
 	grunt.registerTask('default', ['less', 'legacssy']);
 
-
-	grunt.registerTask('frontend', ['less:frontend', 'less:frontend_mini', 'legacssy:frontend']);
-	grunt.registerTask('admin', ['less:admin', 'less:admin_mini','legacssy:admin' ]);
+	grunt.registerTask('frontend', ['less:frontend', 'legacssy:frontend']);
+	grunt.registerTask('admin', ['less:admin', 'legacssy:admin' ]);
 };
