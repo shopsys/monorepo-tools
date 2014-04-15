17f7e8a4225a2b02865a4a8f5262b9b6cb1b5698
Petr Kadlec <kadlec@netdevelo.cz>
2014-04-16 00:11:38 +0200

    move templates of default controllers to Content

--- project-base/src/SS6/ShopBundle/Controller/Admin/DefaultController.php
+++ project-base/src/SS6/ShopBundle/Controller/Admin/DefaultController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller {
 
 	public function indexAction() {
-		return $this->render('@SS6Shop/Admin/Default/index.html.twig');
+		return $this->render('@SS6Shop/Admin/Content/Default/index.html.twig');
 	}
 
 }
