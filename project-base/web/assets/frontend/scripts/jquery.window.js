a633ad9427923c9c3597b51228435681b9020b0a
Petr Kadlec <kadlec@netdevelo.cz>
2014-04-18 17:30:00 +0200

    js window component

---
+++ project-base/web/assets/frontend/scripts/jquery.window.js
@@ -0,0 +1,80 @@
+/**
+ * Custom plugin window
+ */
+
+(function ($) {
+
+	$.fn.SS6 = $.fn.SS6 || {};
+	$.fn.SS6.window = $.fn.SS6.Window || {};
+	
+	var windowPrefix = '#window-container-';
+	var windowButtonCloseSelector = '.window-button-close';
+	var windowButtonContinueSelector = '.window-button-continue';
+	var windowMainContainerId = 'window-main-container';
+	
+	/**
+	 * eventOnClose (function) - on close callback
+	 * eventCloseButton (function) - button Close callback
+	 * eventContinueButton (function) - button Yes callback
+	 */
+	$.fn.SS6.window.create = function (options) {
+		var defaults = {
+			eventOnClose: function () {},
+			eventCloseButton: function () {},
+			eventContinueButton: function () {},
+		};
+		var options = $.extend(defaults, options);
+		
+		var $window = $(windowPrefix + options.id);
+		
+		$window
+			.bind('windowClose', function (event) {
+				options.eventOnClose.apply($window, [event]);
+				$window.fadeOut('fast', function () { $(this).appendTo('body') });
+			})
+			.on('click.windowClose', windowButtonCloseSelector, function (event) {
+				options.eventCloseButton.apply($window, [event]);
+				$window.trigger('windowClose');
+				event.preventDefault();
+			})
+			.on('click.windowContinue', windowButtonContinueSelector, function (event) {
+				options.eventContinueButton.apply($window, [event]);
+				$window.trigger('windowClose');
+				if ($(this).attr('href') === '#') {
+					event.preventDefault();
+				}
+			});
+			
+	}
+	
+	$.fn.SS6.window.open = function(id) {
+		var $window = $(windowPrefix + id);
+		var windowId = $window.attr('id');
+		var $mainContainer = this.getMainContainer();
+		
+		var isOpenned = false;
+		$mainContainer.children().each(function() {
+			if ($(this).attr('id') === windowId) {
+				isOpenned = true;
+			} else {
+				$(this).trigger('windowClose');
+			}
+		});
+		
+		if (!isOpenned) {
+			$window.hide().appendTo($mainContainer).fadeIn('fast');
+		}
+	}
+	
+	$.fn.SS6.window.getMainContainer = function() {
+		var $mainContainer = $('#' + windowMainContainerId);
+		if ($mainContainer.size() === 0) {
+			$('body').append('<div id="' + windowMainContainerId + '"></div>');
+			$mainContainer = $('#' + windowMainContainerId);
+		}
+		return $mainContainer;
+	}
+	
+})(jQuery);
+
+
