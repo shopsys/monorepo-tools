8e823ae0e11f081c32f6d385f04856c3d4f1d377
Petr Kadlec <kadlec@netdevelo.cz>
2014-04-23 19:02:45 +0200

    CartItem and Summary of CartItems

---
+++ project-base/src/SS6/ShopBundle/Model/Cart/Cart.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace SS6\ShopBundle\Model\Cart;
+
+class Cart {
+	
+	/**
+	 * @var string
+	 */
+	private $price;
+	
+	/**
+	 * @var int
+	 */
+	private $quantity;
+	
+	/**
+	 * @var \SS6\ShopBundle\Model\Cart\CartItem[]
+	 */
+	private $cartItems;
+	
+	/**
+	 * @param \SS6\ShopBundle\Model\Cart\CartItem[] $cartItems
+	 */
+	public function __construct(array $cartItems) {
+		$this->cartItems = $cartItems;
+		$this->calcSummaryInfo();
+	}
+	
+	private function calcSummaryInfo() {
+		$this->quantity = 0;
+		foreach ($this->cartItems as $carItem) {
+			$this->quantity += $carItem->getQuantity();
+			$this->price += $carItem->getProduct()->getPrice() * $carItem->getQuantity();
+		}
+	}
+	
+	/**
+	 * @return \SS6\ShopBundle\Model\Cart\CartItem[]
+	 */
+	public function getItems() {
+		return $this->cartItems;
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getItemsCount() {
+		return count($this->getItems());
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getQuantity() {
+		return $this->quantity;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getPrice() {
+		return $this->price;
+	}
+
+}
---
+++ project-base/src/SS6/ShopBundle/Model/Cart/CartItem.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace SS6\ShopBundle\Model\Cart;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use SS6\ShopBundle\Model\Customer\CustomerIdentifier;
+use SS6\ShopBundle\Model\Product\Product;
+
+/**
+ * @ORM\Table(name="cart_items")
+ * @ORM\Entity
+ */
+class CartItem {
+
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="IDENTITY")
+	 */
+	private $id;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(type="string", length=127)
+	 */
+	private $sessionId;
+	
+	/**
+	 * @var \SS6\ShopBundle\Model\Product\Product
+	 * 
+	 * @ORM\ManyToOne(targetEntity="SS6\ShopBundle\Model\Product\Product")
+	 * @ORM\JoinColumn(name="product_id", referencedColumnName="id")
+	 */
+	private $product;
+
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(type="integer")
+	 */
+	private $quantity;
+	
+	/**
+	 * @var \DateTime
+	 * 
+	 * @ORM\Column(type="date")
+	 */
+	private $lastUpdate;
+	
+	
+	/**
+	 * @param \SS6\ShopBundle\Model\Customer\CustomerIdentifier $customerIdentifier
+	 * @param \SS6\ShopBundle\Model\Product\Product $product
+	 * @param int $quantity
+	 */
+	public function __construct(CustomerIdentifier $customerIdentifier, Product $product, $quantity) {
+		$this->sessionId = $customerIdentifier->getSessionId();
+		$this->product = $product;
+		$this->quantity = $quantity;
+		$this->setUpdate();
+	}
+	
+	private function setUpdate() {
+		$this->lastUpdate = new DateTime('now');
+	}
+
+	/**
+	 * @param int $newQuantity
+	 */
+	public function changeQuantity($newQuantity) {
+		$this->quantity = $newQuantity;
+	}
+	
+	/**
+	 * @return \SS6\ShopBundle\Model\Product\Product
+	 */
+	public function getProduct() {
+		return $this->product;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getQuantity() {
+		return $this->quantity;
+	}
+
+}
