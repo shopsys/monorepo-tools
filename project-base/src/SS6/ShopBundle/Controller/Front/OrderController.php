7d56ce9a4c9e5c1dc1dfe8c2a5e65727d12c359d
Pavel Sidzina <sidzina@netdevelo.cz>
2014-04-24 15:06:03 +0200

    new page - order - personal information (2nd step of order)

--- project-base/src/SS6/ShopBundle/Controller/Front/OrderController.php
+++ project-base/src/SS6/ShopBundle/Controller/Front/OrderController.php
@@ -9,4 +9,8 @@ class OrderController extends Controller {
 	public function indexAction() {		
 		return $this->render('@SS6Shop/Front/Content/Order/index.html.twig');
 	}
+
+	public function personalAction() {		
+		return $this->render('@SS6Shop/Front/Content/Order/personal.html.twig');
+	}
 }
