100b8b1a5feb294c7f793c00913450a6279b32d1
Miroslav Sustek <miroslav.sustek@netdevelo.cz>
2014-04-24 10:22:48 +0200

    validation for admin/product form

--- project-base/src/SS6/ShopBundle/Form/Admin/Product/ProductFormType.php
+++ project-base/src/SS6/ShopBundle/Form/Admin/Product/ProductFormType.php
@@ -5,6 +5,8 @@ namespace SS6\ShopBundle\Form\Admin\Product;
 use SS6\ShopBundle\Form\YesNoType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints;
 
 class ProductFormType extends AbstractType {
 
@@ -22,17 +24,55 @@ class ProductFormType extends AbstractType {
 	public function buildForm(FormBuilderInterface $builder, array $options) {
 		$builder
 			->add('id', 'integer', array('disabled' => true, 'required' => false))
-			->add('name', 'text')
-			->add('catnum', 'text', array('required' => false))
-			->add('partno', 'text', array('required' => false))
-			->add('ean', 'text', array('required' => false))
+			->add('name', 'text', array(
+				'constraints' => array(
+					new Constraints\NotBlank(array('message' => 'Prosím vyplňte název')),
+				),
+			))
+			->add('catnum', 'text', array(
+				'required' => false,
+				'constraints' => array(
+					new Constraints\Length(array('max' => 100, 'maxMessage' => 'Katalogové číslo nesmí být delší než {{ limit }} znaků')),
+				),
+			))
+			->add('partno', 'text', array(
+				'required' => false,
+				'constraints' => array(
+					new Constraints\Length(array('max' => 100, 'maxMessage' => 'Výrobní číslo nesmí být delší než {{ limit }} znaků')),
+				),
+			))
+			->add('ean', 'text', array(
+				'required' => false,
+				'constraints' => array(
+					new Constraints\Length(array('max' => 100, 'maxMessage' => 'EAN nesmí být delší než {{ limit }} znaků')),
+				),
+			))
 			->add('description', 'textarea', array('required' => false))
 			->add('price', 'money', array('currency' => false, 'required' => false))
-			->add('sellingFrom', 'datePicker', array('required' => false))
-			->add('sellingTo', 'datePicker', array('required' => false))
-			->add('stockQuantity', 'integer', array('required' => false))
+			->add('sellingFrom', 'datePicker', array(
+				'required' => false,
+				'constraints' => array(
+					new Constraints\Date(array('message' => 'Datum zadávejte ve formátu YYYY-MM-DD')),
+				),
+			))
+			->add('sellingTo', 'datePicker', array(
+				'required' => false,
+				'constraints' => array(
+					new Constraints\Date(array('message' => 'Datum zadávejte ve formátu YYYY-MM-DD')),
+				),
+			))
+			->add('stockQuantity', 'integer', array(
+				'required' => false,
+				'invalid_message' => 'Prosím zadejte číslo',
+			))
 			->add('hidden', new YesNoType(), array('required' => false))
 			->add('save', 'submit');
+	}
+
+	public function setDefaultOptions(OptionsResolverInterface $resolver) {
+		$resolver->setDefaults(array(
+			'attr' => array('novalidate' => 'novalidate'),
+    ));
 	}
 
 }
