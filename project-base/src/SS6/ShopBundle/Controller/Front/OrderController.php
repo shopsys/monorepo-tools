3a240baaf2c706c808df3c9ffb2dd99fdcbec534
Pavel Sidzina <sidzina@netdevelo.cz>
2014-04-25 10:04:11 +0200

    confirm page - last order step

--- project-base/src/SS6/ShopBundle/Controller/Front/OrderController.php
+++ project-base/src/SS6/ShopBundle/Controller/Front/OrderController.php
@@ -13,4 +13,8 @@ class OrderController extends Controller {
 	public function personalAction() {		
 		return $this->render('@SS6Shop/Front/Content/Order/personal.html.twig');
 	}
+
+	public function sendedAction() {		
+		return $this->render('@SS6Shop/Front/Content/Order/sended.html.twig');
+	}
 }
