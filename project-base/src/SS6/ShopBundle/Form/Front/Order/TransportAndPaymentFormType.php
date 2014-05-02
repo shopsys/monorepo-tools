5589fd2c18f205aa6a9fa2bbaa9dd5643d7bc30a
Miroslav Sustek <miroslav.sustek@netdevelo.cz>
2014-05-02 13:06:52 +0200

    order: fix "continue" button background changing

--- project-base/src/SS6/ShopBundle/Form/Front/Order/TransportAndPaymentFormType.php
+++ project-base/src/SS6/ShopBundle/Form/Front/Order/TransportAndPaymentFormType.php
@@ -55,7 +55,7 @@ class TransportAndPaymentFormType extends AbstractType {
 	 * @return string
 	 */
 	public function getName() {
-		return 'orderTransportAndPayment';
+		return 'transportAndPayment';
 	}
 
 	/**
