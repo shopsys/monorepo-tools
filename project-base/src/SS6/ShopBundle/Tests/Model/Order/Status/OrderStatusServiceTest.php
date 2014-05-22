e77a125cdaef414d2ce4af4bc26e8066c12fff26
Petr Kadlec <kadlec@netdevelo.cz>
2014-05-22 08:08:05 +0200

    unit test of OrderStatusService

--- project-base/src/SS6/ShopBundle/Tests/Model/Order/Status/OrderStatusServiceTest.php
+++ project-base/src/SS6/ShopBundle/Tests/Model/Order/Status/OrderStatusServiceTest.php
@@ -7,20 +7,36 @@ use SS6\ShopBundle\Model\Order\Status\OrderStatus;
 use SS6\ShopBundle\Model\Order\Status\OrderStatusRepository;
 use SS6\ShopBundle\Model\Order\Status\OrderStatusService;
 use SS6\ShopBundle\Model\Order\Status\Exception\OrderStatusDeletionForbiddenException;
+use SS6\ShopBundle\Model\Order\Status\Exception\OrderStatusDeletionWithOrdersException;
+use SS6\ShopBundle\Model\Order\Status\Exception\OrderStatusException;
 
 class OrderStatusServiceTest extends PHPUnit_Framework_TestCase {
 
 	public function testDelete() {
 		$orderStatusService = new OrderStatusService();
 		$orderStatus = new OrderStatus('statusName', OrderStatusRepository::STATUS_IN_PROGRESS);
-		$this->assertNull($orderStatusService->delete($orderStatus));
+		$this->assertNull($orderStatusService->delete($orderStatus, 0));
 	}
 
 	public function testDeleteForbidden() {
 		$orderStatusService = new OrderStatusService();
 		$orderStatus = new OrderStatus('statusName', OrderStatusRepository::STATUS_NEW);
 		$this->setExpectedException(OrderStatusDeletionForbiddenException::class);
-		$orderStatusService->delete($orderStatus);
+		$orderStatusService->delete($orderStatus, 0);
+	}
+
+	public function testDeleteWithOrder() {
+		$orderStatusService = new OrderStatusService();
+		$orderStatus = new OrderStatus('statusName', OrderStatusRepository::STATUS_IN_PROGRESS);
+		$this->setExpectedException(OrderStatusDeletionWithOrdersException::class);
+		$orderStatusService->delete($orderStatus, 1);
+	}
+
+	public function testDeleteForbiddenOrWithOrder() {
+		$orderStatusService = new OrderStatusService();
+		$orderStatus = new OrderStatus('statusName', OrderStatusRepository::STATUS_NEW);
+		$this->setExpectedException(OrderStatusException::class);
+		$orderStatusService->delete($orderStatus, 1);
 	}
 	
 }
