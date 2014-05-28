475b8c29ea703d46e369eb9f276f536c44e4bdb5
Miroslav Sustek <miroslav.sustek@netdevelo.cz>
2014-05-28 17:41:30 +0200

    admin: load menu in the left panel from configuration

--- project-base/src/SS6/ShopBundle/Controller/Admin/MenuController.php
+++ project-base/src/SS6/ShopBundle/Controller/Admin/MenuController.php
@@ -15,4 +15,23 @@ class MenuController extends Controller {
 		));
 	}
 
+	public function panelAction($route, array $parameters = null) {
+		$menu = $this->get('ss6.shop.admin_menu.menu');
+		/* @var $menu \SS6\ShopBundle\Model\AdminMenu\Menu */
+
+		$items = array();
+
+		$activeItem = $menu->getItemMatchingRoute($route, $parameters);
+		if ($activeItem !== null) {
+			$path = $menu->getItemPath($activeItem);
+				if (isset($path[0])) {
+				$items = $path[0]->getItems();
+			}
+		}
+
+		return $this->render('@SS6Shop/Admin/Inline/Menu/panel.html.twig', array(
+			'items' => $items,
+		));
+	}
+
 }
