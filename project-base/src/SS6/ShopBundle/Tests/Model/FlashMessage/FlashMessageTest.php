bd7527c2e447fb5a051b38e70d67b15a73d836b0
Petr Kadlec <kadlec@netdevelo.cz>
2014-06-02 23:08:06 +0200

    update flash message test

--- project-base/src/SS6/ShopBundle/Tests/Model/FlashMessage/FlashMessageTest.php
+++ project-base/src/SS6/ShopBundle/Tests/Model/FlashMessage/FlashMessageTest.php
@@ -62,4 +62,19 @@ class PaymentTest extends FunctionalTestCase {
 		$this->assertEquals(array($errorMessageAdmin), $flashMessageAdmin->getErrorMessages());
 		$this->assertEquals(array(), $flashMessageAdmin->getErrorMessages());
 	}
+
+	public function testIsEmpty() {
+		$flashMessageAdmin = $this->getContainer()->get('ss6.shop.flash_message.admin');
+		/* @var $flashMessageAdmin \SS6\ShopBundle\Model\FlashMessage\FlashMessage */
+
+		// clearing after previous tests
+		$flashMessageAdmin->getErrorMessages();
+		$flashMessageAdmin->getInfoMessages();
+		$flashMessageAdmin->getSuccessMessages();
+
+		$this->assertTrue($flashMessageAdmin->isEmpty());
+		$flashMessageAdmin->addInfo('Some message');
+		$this->assertFalse($flashMessageAdmin->isEmpty());
+		$this->assertFalse($flashMessageAdmin->isEmpty(), 'Flash message cannot modified content after call isEmpty()');
+	}
 }
