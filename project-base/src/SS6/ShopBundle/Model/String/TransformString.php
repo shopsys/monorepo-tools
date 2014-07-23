9a52a67eff46aff12a71ad570214b43812b427bf
Petr Kadlec <kadlec@netdevelo.cz>
2014-07-23 15:09:43 +0200

    fix annotation

--- project-base/src/SS6/ShopBundle/Model/String/TransformString.php
+++ project-base/src/SS6/ShopBundle/Model/String/TransformString.php
@@ -3,6 +3,11 @@
 namespace SS6\ShopBundle\Model\String;
 
 class TransformString {
+
+	/**
+	 * @param string $string
+	 * @return string
+	 */
 	public static function safeFilename($string) {
 		$string = preg_replace('~[^-\\.\\pL0-9_]+~u', '_', $string);
 		$string = preg_replace('~[\\.]{2,}~u', '.', $string);
@@ -10,6 +15,7 @@ class TransformString {
 		$string = iconv('utf-8', 'us-ascii//TRANSLIT', $string);
 		$string = preg_replace('~[^-\\.a-zA-Z0-9_]+~', '', $string);
 		$string = ltrim($string, '.');
+
 		return $string;
 	}
 }
