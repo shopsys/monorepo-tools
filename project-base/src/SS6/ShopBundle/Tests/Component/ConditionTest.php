8fe091285338d3baf49886f1f6803ef27f0bb95b
Petr Kadlec <kadlec@netdevelo.cz>
2014-07-28 08:42:37 +0200

    Component/Condition tests

---
+++ project-base/src/SS6/ShopBundle/Tests/Component/ConditionTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SS6\ShopBundle\Tests\Component;
+
+use PHPUnit_Framework_TestCase;
+use SS6\ShopBundle\Component\Condition;
+
+class ConditionTest extends PHPUnit_Framework_TestCase {
+
+	public function testIfNull() {
+		$this->assertTrue(Condition::ifNull(null, true));
+		$this->assertFalse(Condition::ifNull(false, true));
+		$this->assertTrue(Condition::ifNull(true, false));
+	}
+
+	public function testSetArrayDefaultValueExists() {
+		$array = array(
+			'key' => 'value',
+		);
+		$expectedArray = $array;
+		Condition::setArrayDefaultValue($array, 'key', 'defaultValue');
+
+		$this->assertEquals($expectedArray, $array);
+	}
+
+	public function testSetArrayDefaultValueExistsNull() {
+		$array = array(
+			'key' => null,
+		);
+		$expectedArray = $array;
+		Condition::setArrayDefaultValue($array, 'key', 'defaultValue');
+
+		$this->assertEquals($expectedArray, $array);
+	}
+
+	public function testSetArrayDefaultValueNotExist() {
+		$array = array(
+			'key' => null,
+		);
+		$expectedArray = array(
+			'key' => null,
+			0 => 'number',
+		);
+		Condition::setArrayDefaultValue($array, 0, 'number');
+
+		$this->assertEquals($expectedArray, $array);
+	}
+}
