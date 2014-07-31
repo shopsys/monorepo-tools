4ff21c61aad9845349996d874461c30f49a1ee4d
Miroslav Sustek <miroslav.sustek@netdevelo.cz>
2014-07-31 11:11:23 +0200

    english

--- project-base/src/SS6/ShopBundle/Model/Image/Config/Exception/EntityParseException.php
+++ project-base/src/SS6/ShopBundle/Model/Image/Config/Exception/EntityParseException.php
@@ -18,7 +18,7 @@ class EntityParseException extends Exception implements ImageConfigException {
 	public function __construct($entityClass, Exception $previous = null) {
 		$this->entityClass = $entityClass;
 
-		$message = sprintf('Parse config entity class "%s" failed.', $this->entityClass);
+		$message = sprintf('Parsing of config entity class "%s" failed.', $this->entityClass);
 		parent::__construct($message, 0, $previous);
 	}
 
