58192dc87ee82f181033e4d529504a5a6e4b7538
Miroslav Sustek <miroslav.sustek@netdevelo.cz>
2014-08-01 10:47:10 +0200

    fix permissions for newly created files (eg. cache or logs)

--- project-base/web/index.php
+++ project-base/web/index.php
@@ -1,5 +1,9 @@
 <?php 
 
+// if you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
+umask(0002);
+
 use SS6\Bootstrap;
 
 require_once __DIR__ . '/../app/Bootstrap.php';
