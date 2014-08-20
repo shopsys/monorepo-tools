564c76e99cd6c204ca018faede60aded7ec05ffd
Petr Kadlec <kadlec@netdevelo.cz>
2014-08-20 14:12:04 +0200

    entity Product/Availability

---
+++ project-base/src/SS6/ShopBundle/Model/Product/Availability/AvailabilityRepository.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace SS6\ShopBundle\Model\Product\Availability;
+
+use Doctrine\ORM\EntityManager;
+use SS6\ShopBundle\Model\Product\Availability\Availability;
+
+class AvailabilityRepository {
+
+	/** 
+	 * @var \Doctrine\ORM\EntityRepository
+	 */
+	private $em;
+
+	/**
+	 * @param \Doctrine\ORM\EntityManager $entityManager
+	 */
+	public function __construct(EntityManager $entityManager) {
+		$this->em = $entityManager;
+	}
+
+	/**
+	 * @return \Doctrine\ORM\EntityRepository
+	 */
+	private function getAvailabilityRepository() {
+		return $this->em->getRepository(Availability::class);
+	}
+	
+	/**
+	 * @param int $availabilityId
+	 * @return \SS6\ShopBundle\Model\Product\Availability\Availability|null
+	 */
+	public function findById($availabilityId) {
+		return $this->getAvailabilityRepository()->find($availabilityId);
+	}
+	
+	/**
+	 * @param int $availabilityId
+	 * @return \SS6\ShopBundle\Model\Product\Availability\Availability
+	 * @throws \SS6\ShopBundle\Model\Product\Availability\Exception\AvailabilityNotFoundException
+	 */
+	public function getById($availabilityId) {
+		$availability = $this->findById($availabilityId);
+
+		if ($availability === null) {
+			throw new \SS6\ShopBundle\Model\Product\Availability\Exception\AvailabilityNotFoundException($availabilityId);
+		}
+
+		return $availability;
+	}
+
+	/**
+	 * @return \SS6\ShopBundle\Model\Product\Availability\Availability[]
+	 */
+	public function findAll() {
+		return $this->getAvailabilityRepository()->findBy(array(), array('name' => 'asc'));
+	}
+
+}
---
+++ project-base/src/SS6/ShopBundle/Model/Product/Availability/Exception/AvailabilityException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SS6\ShopBundle\Model\Product\Availability\Exception;
+
+use SS6\ShopBundle\Model\Product\Exception\ProductException;
+
+interface AvailabilityException extends ProductException {
+	
+}
