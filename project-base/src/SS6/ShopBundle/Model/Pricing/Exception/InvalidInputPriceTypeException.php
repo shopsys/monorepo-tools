9afa163f81060ac0e98e9466e056fd8f2dcfcc66
Miroslav Sustek <miroslav.sustek@netdevelo.cz>
2014-08-21 10:45:11 +0200

    admin: input price label based on settings

---
+++ project-base/src/SS6/ShopBundle/Model/Pricing/Exception/InvalidInputPriceTypeException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SS6\ShopBundle\Model\Pricing\Exception;
+
+use Exception;
+
+class InvalidInputPriceTypeException extends Exception implements PricingException {
+	
+	/**
+	 * @param string $message
+	 * @param Exception $previous
+	 */
+	public function __construct($message, Exception $previous = null) {
+		parent::__construct($message, 0, $previous);
+	}
+}
