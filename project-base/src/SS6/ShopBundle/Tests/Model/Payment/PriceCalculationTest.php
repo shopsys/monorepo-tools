f33746b7a03978dbd195578a106bfc7120318b6e
Petr Kadlec <kadlec@netdevelo.cz>
2014-09-09 09:41:58 +0200

    tests price transport and payment calculation

---
+++ project-base/src/SS6/ShopBundle/Tests/Model/Payment/PriceCalculationTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace SS6\ShopBundle\Tests\Model\Payment;
+
+use PHPUnit_Framework_TestCase;
+use SS6\ShopBundle\Model\Payment\PriceCalculation;
+use SS6\ShopBundle\Model\Payment\Payment;
+use SS6\ShopBundle\Model\Payment\PaymentData;
+use SS6\ShopBundle\Model\Pricing\PriceCalculation as GenericPriceCalculation;
+use SS6\ShopBundle\Model\Pricing\PricingSetting;
+use SS6\ShopBundle\Model\Pricing\Vat\Vat;
+use SS6\ShopBundle\Model\Pricing\Vat\VatData;
+
+class PaymentCalculationTest extends PHPUnit_Framework_TestCase {
+
+	public function testCalculatePriceProvider() {
+		return array(
+			array(
+				'inputPriceType' => PricingSetting::INPUT_PRICE_TYPE_WITHOUT_VAT,
+				'inputPrice' => '6999',
+				'vatPercent' => '21',
+				'basePriceWithoutVat' => '6998.78',
+				'basePriceWithVat' => '8469',
+			),
+			array(
+				'inputPriceType' => PricingSetting::INPUT_PRICE_TYPE_WITH_VAT,
+				'inputPrice' => '6999.99',
+				'vatPercent' => '21',
+				'basePriceWithoutVat' => '5784.8',
+				'basePriceWithVat' => '7000',
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider testCalculatePriceProvider
+	 */
+	public function testCalculatePrice(
+		$inputPriceType,
+		$inputPrice,
+		$vatPercent,
+		$basePriceWithoutVat,
+		$basePriceWithVat
+	) {
+		$pricingCalculation = new GenericPriceCalculation();
+
+		$pricingSettingMock = $this->getMockBuilder(PricingSetting::class)
+			->setMethods(array('getInputPriceType'))
+			->disableOriginalConstructor()
+			->getMock();
+		$pricingSettingMock
+			->expects($this->any())->method('getInputPriceType')
+				->will($this->returnValue($inputPriceType));
+		$priceCalculation = new PriceCalculation($pricingCalculation, $pricingSettingMock);
+
+		$vat = new Vat(new VatData('vat', $vatPercent));
+
+		$payment = new Payment(new PaymentData('PaymentName', $inputPrice, $vat));
+
+		$price = $priceCalculation->calculatePrice($payment);
+
+		$this->assertEquals(round($basePriceWithoutVat, 6), round($price->getBasePriceWithoutVat(), 6));
+		$this->assertEquals(round($basePriceWithVat, 6), round($price->getBasePriceWithVat(), 6));
+	}
+
+}
