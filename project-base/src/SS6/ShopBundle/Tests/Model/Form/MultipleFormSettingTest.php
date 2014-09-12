d665c7a504dd948117c259cd7a2381a290bb3cf8
Petr Kadlec <kadlec@netdevelo.cz>
2014-09-12 12:59:59 +0200

    tests

---
+++ project-base/src/SS6/ShopBundle/Tests/Model/Form/MultipleFormSettingTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace SS6\ShopBundle\Tests\Model\Form;
+
+use PHPUnit_Framework_TestCase;
+use SS6\ShopBundle\Model\Form\MultipleFormSetting;
+
+class MultipleFormSettingTest extends PHPUnit_Framework_TestCase {
+
+	public function testCurrentFormIsMultiple() {
+		$multipleFormSetting = new MultipleFormSetting();
+		$multipleFormSetting->currentFormIsMultiple();
+
+		$this->assertTrue($multipleFormSetting->isCurrentFormMultiple());
+	}
+
+	public function testCurrentFormIsNotMultiple() {
+		$multipleFormSetting = new MultipleFormSetting();
+		$multipleFormSetting->currentFormIsNotMultiple();
+
+		$this->assertFalse($multipleFormSetting->isCurrentFormMultiple());
+	}
+
+	public function testDefaultValue() {
+		$multipleFormSetting = new MultipleFormSetting();
+		
+		$this->assertEquals(MultipleFormSetting::DEFAULT_MULTIPLE, $multipleFormSetting->isCurrentFormMultiple());
+	}
+
+	public function testReset() {
+		$multipleFormSetting = new MultipleFormSetting();
+
+		$multipleFormSetting->reset();
+		$this->assertEquals(MultipleFormSetting::DEFAULT_MULTIPLE, $multipleFormSetting->isCurrentFormMultiple());
+
+		$multipleFormSetting->currentFormIsMultiple();
+		$multipleFormSetting->reset();
+		$this->assertEquals(MultipleFormSetting::DEFAULT_MULTIPLE, $multipleFormSetting->isCurrentFormMultiple());
+		
+		$multipleFormSetting->currentFormIsNotMultiple();
+		$multipleFormSetting->reset();
+		$this->assertEquals(MultipleFormSetting::DEFAULT_MULTIPLE, $multipleFormSetting->isCurrentFormMultiple());
+	}
+}
