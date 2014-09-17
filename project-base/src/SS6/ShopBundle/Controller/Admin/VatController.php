e17f987c153b9322ee248e76f2edccc62310cb33
Petr Kadlec <kadlec@netdevelo.cz>
2014-09-17 10:31:28 +0200

    setting default vat

--- project-base/src/SS6/ShopBundle/Controller/Admin/VatController.php
+++ project-base/src/SS6/ShopBundle/Controller/Admin/VatController.php
@@ -3,7 +3,10 @@
 namespace SS6\ShopBundle\Controller\Admin;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use SS6\ShopBundle\Form\Admin\Vat\DefaultVatFormType;
+use SS6\ShopBundle\Model\Pricing\Vat\Vat;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class VatController extends Controller {
 
@@ -39,6 +42,40 @@ class VatController extends Controller {
 			'name' => $fullName,
 		));
 		return $this->redirect($this->generateUrl('admin_vat_list'));
+	}
+
+	/**
+	 * @param \Symfony\Component\HttpFoundation\Request $request
+	 */
+	public function defaultVatAction(Request $request) {
+		$vatRepository = $this->get('ss6.shop.pricing.vat.vat_repository');
+		/* @var $vatRepository \SS6\ShopBundle\Model\Pricing\Vat\VatRepository */
+		$vatFacade = $this->get('ss6.shop.pricing.vat.vat_facade');
+		/* @var $vatFacade \SS6\ShopBundle\Model\Pricing\Vat\VatFacade */
+
+		$vats = $vatRepository->findAll();
+		$form = $this->createForm(new DefaultVatFormType($vats));
+
+		$defaultVatFormData = array();
+		if (!$form->isSubmitted()) {
+			$defaultVatFormData['defaultVat'] = $vatFacade->getDefaultVat();
+		}
+		
+		$form->setData($defaultVatFormData);
+		$form->handleRequest($request);
+
+		if ($form->isValid()) {
+			$defaultVatFormData = $form->getData();
+			$vatFacade->setDefaultVat($defaultVatFormData['defaultVat']);
+
+			// @codingStandardsIgnoreStart
+			// TODO: redirect && $flashMessageText->addSuccess('Nastavení výchozí sazby DPH bylo upraveno');
+			// @codingStandardsIgnoreStop
+		}
+
+		return $this->render('@SS6Shop/Admin/Content/Vat/defaultVat.html.twig', array(
+			'form' => $form->createView(),
+		));
 	}
 
 }
--- project-base/src/SS6/ShopBundle/DataFixtures/Base/SettingValueDataFixture.php
+++ project-base/src/SS6/ShopBundle/DataFixtures/Base/SettingValueDataFixture.php
@@ -2,21 +2,36 @@
 
 namespace SS6\ShopBundle\DataFixtures\Base;
 
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use SS6\ShopBundle\DataFixtures\Base\VatDataFixture;
 use SS6\ShopBundle\Model\DataFixture\AbstractReferenceFixture;
+use SS6\ShopBundle\Model\Pricing\Vat\Vat;
 use SS6\ShopBundle\Model\Setting\Setting;
 use SS6\ShopBundle\Model\Setting\SettingValue;
 
-class SettingValueDataFixture extends AbstractReferenceFixture {
+class SettingValueDataFixture extends AbstractReferenceFixture implements DependentFixtureInterface {
 
 	/**
 	 * @param \Doctrine\Common\Persistence\ObjectManager $manager
 	 */
 	public function load(ObjectManager $manager) {
+		$vat = $this->getReference(VatDataFixture::VAT_HIGH);
+		/* @var $vat \SS6\ShopBundle\Model\Pricing\Vat\Vat */
 
 		$manager->persist(new SettingValue(Setting::INPUT_PRICE_TYPE, Setting::INPUT_PRICE_TYPE_WITHOUT_VAT));
+		$manager->persist(new SettingValue(Vat::SETTING_DEFAULT_VAT, $vat->getId()));
 
 		$manager->flush();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getDependencies() {
+		return array(
+			VatDataFixture::class,
+		);
 	}
 
 }
