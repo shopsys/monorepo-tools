e7f749ae9185166dcf0fb085b5c4222a9e70f4a7
Petr Kadlec <kadlec@netdevelo.cz>
2014-09-18 12:31:38 +0200

    changes DB after load demo images

--- project-base/src/SS6/ShopBundle/Command/DemoImagesCommand.php
+++ project-base/src/SS6/ShopBundle/Command/DemoImagesCommand.php
@@ -11,6 +11,11 @@ use ZipArchive;
 class DemoImagesCommand extends ContainerAwareCommand {
 
 	/**
+	 * @var \Doctrine\ORM\EntityManager
+	 */
+	private $em;
+
+	/**
 	 * @var \Symfony\Component\Filesystem\Filesystem
 	 */
 	private $filesystem;
@@ -27,14 +32,18 @@ class DemoImagesCommand extends ContainerAwareCommand {
 	 */
 	protected function execute(InputInterface $input, OutputInterface $output) {
 		$this->filesystem = $this->getContainer()->get('filesystem');
+		$this->em = $this->getContainer()->get('doctrine.orm.entity_manager');
 
 		$archiveUrl = $this->getContainer()->getParameter('ss6.demo_images_archive_url');
+		$dqlUrl = $this->getContainer()->getParameter('ss6.demo_images_dql_url');
 		$cachePath = $this->getContainer()->getParameter('kernel.cache_dir');
 		$localArchiveFilepath = $cachePath . DIRECTORY_SEPARATOR . 'demoImages.zip';
 		$imagesPath = $this->getContainer()->getParameter('ss6.image_dir');
 
 		if ($this->downloadImages($output, $archiveUrl, $localArchiveFilepath)) {
-			$this->unpackImages($output, $imagesPath, $localArchiveFilepath);
+			if ($this->unpackImages($output, $imagesPath, $localArchiveFilepath)) {
+				$this->loadDbChanges($output, $dqlUrl);
+			}
 		}
 
 		$this->cleanUp($output, $localArchiveFilepath);
@@ -44,17 +53,42 @@ class DemoImagesCommand extends ContainerAwareCommand {
 	 * @param \Symfony\Component\Console\Output\OutputInterface $output
 	 * @param string $imagesPath
 	 * @param string $localArchiveFilepath
+	 * @return bool
 	 */
 	private function unpackImages(OutputInterface $output, $imagesPath, $localArchiveFilepath) {
 		$zipArchive = new ZipArchive();
+		
 		$result = $zipArchive->open($localArchiveFilepath);
-		if ($result === true) {
-			$zipArchive->extractTo($imagesPath);
-			$zipArchive->close();
-			$output->writeln('<fg=green>Unpacking image archive was successfully completed</fg=green>');
-		} else {
+		if ($result !== true) {
 			$output->writeln('<fg=red>Unpacking image archive failed</fg=red>');
+			return false;
 		}
+
+		$zipArchive->extractTo($imagesPath);
+		$zipArchive->close();
+		$output->writeln('<fg=green>Unpacking image archive was successfully completed</fg=green>');
+		
+		return true;
+	}
+
+	/**
+	 * @param \Symfony\Component\Console\Output\OutputInterface $output
+	 * @param string $dqlUrl
+	 */
+	private function loadDbChanges(OutputInterface $output, $dqlUrl) {
+		$dqls = file_get_contents($dqlUrl);
+		if ($dqls === false) {
+			$output->writeln('<fg=red>Download db changes failed</fg=red>');
+			return;
+		}
+
+		$dqls = explode(';', $dqls);
+		$dqls = array_map('trim', $dqls);
+		$dqls = array_filter($dqls);
+		foreach ($dqls as $dql) {
+			$this->em->createQuery($dql)->execute();
+		}
+		$output->writeln('<fg=green>Db changes were successfully applied (queries: ' . count($dqls) . ')</fg=green>');
 	}
 
 	/**
