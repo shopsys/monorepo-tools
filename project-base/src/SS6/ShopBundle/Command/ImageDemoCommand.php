89e7c5939204fb98bae85cd7cc77d282f6ad159b
Miroslav Sustek <miroslav.sustek@netdevelo.cz>
2014-09-24 13:51:39 +0200

    rename DemoImagesCommand to ImageDemoCommand

--- project-base/src/SS6/ShopBundle/Command/ImageDemoCommand.php
+++ project-base/src/SS6/ShopBundle/Command/ImageDemoCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use ZipArchive;
 
-class DemoImagesCommand extends ContainerAwareCommand {
+class ImageDemoCommand extends ContainerAwareCommand {
 
 	/**
 	 * @var \Doctrine\ORM\EntityManager
