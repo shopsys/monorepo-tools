192fad62d1461b1199910206fc4c821a7ffba118
Rostislav Vitek <vitek@netdevelo.cz>
2014-09-30 11:52:24 +0200

    domain tabs in customer list page

--- project-base/src/SS6/ShopBundle/Controller/Admin/CustomerController.php
+++ project-base/src/SS6/ShopBundle/Controller/Admin/CustomerController.php
@@ -80,6 +80,8 @@ class CustomerController extends Controller {
 		/* @var $administrator \SS6\ShopBundle\Model\Administrator\Administrator */
 		$gridFactory = $this->get('ss6.shop.pkgrid.factory');
 		/* @var $gridFactory \SS6\ShopBundle\Model\PKGrid\PKGridFactory */
+		$selectedDomain = $this->get('ss6.shop.domain.selected_domain');
+		/* @var $selectedDomain \SS6\ShopBundle\Model\Domain\SelectedDomain */
 
 		$queryBuilder = $this->getDoctrine()->getManager()->createQueryBuilder();
 		/* @var $queryBuilder \Doctrine\ORM\QueryBuilder */
@@ -97,6 +99,8 @@ class CustomerController extends Controller {
 				SUM(o.totalPrice) ordersSumPrice,
 				MAX(o.createdAt) lastOrderAt')
 			->from(User::class, 'u')
+			->where('u.domainId = :selectedDomainId')
+			->setParameter('selectedDomainId', $selectedDomain->getId())
 			->leftJoin('u.billingAddress', 'ba')
 			->leftJoin(Order::class, 'o', 'WITH', 'o.customer = u.id')
 			->groupBy('u.id');
