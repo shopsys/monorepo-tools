e176a817b867530a7b5b0e4aefe0fbd57a49a1ca
Rostislav Vitek <vitek@netdevelo.cz>
2014-10-03 11:25:27 +0200

    test modification

--- project-base/src/SS6/ShopBundle/TestsDb/Model/Transport/VisibilityCalculationTest.php
+++ project-base/src/SS6/ShopBundle/TestsDb/Model/Transport/VisibilityCalculationTest.php
@@ -10,9 +10,9 @@ use SS6\ShopBundle\Model\Pricing\Vat\VatData;
 use SS6\ShopBundle\Model\Transport\Transport;
 use SS6\ShopBundle\Model\Transport\TransportData;
 
-class TransportRepositoryTest extends DatabaseTestCase {
+class VisibilityCalculationTest extends DatabaseTestCase {
 	
-	public function testFindAllDataWithVisibility() {
+	public function testFindAllVisible() {
 		$em = $this->getEntityManager();
 
 		$vat = new Vat(new VatData('vat', 21));
@@ -34,13 +34,17 @@ class TransportRepositoryTest extends DatabaseTestCase {
 
 		$paymentRepository = $this->getContainer()->get('ss6.shop.payment.payment_repository');
 		/* @var $paymentRepository \SS6\ShopBundle\Model\Payment\PaymentRepository */
-		$allPayments = $paymentRepository->findAllWithTransports();
 
 		$transportRepository = $this->getContainer()->get('ss6.shop.transport.transport_repository');
 		/* @var $transportRepository \SS6\ShopBundle\Model\Transport\TransportRepository */
-		$transportsDataWithVisibility = $transportRepository->findAllDataWithVisibility($allPayments);
 
-		foreach ($transportsDataWithVisibility as $row) {
+		$visibilityCalculation = $this->getContainer()->get('ss6.shop.transport.visibility_calculation');
+		/* @var $visibilityCalculation \SS6\ShopBundle\Model\Transport\VisibilityCalculation */
+		
+		$allPayments = $paymentRepository->findAllWithTransports();
+		$transports = $transportRepository->findAll();
+		$transportsDataWithVisibility = $visibilityCalculation->findAll($transports, $allPayments);
+		foreach ($transportsDataWithVisibility as $row) {			
 			if ($row['entity']->getId() === $transport1->getId()) {
 				$this->assertTrue($row['visible']);
 			} elseif ($row['entity']->getId() === $transport2->getId()) {
