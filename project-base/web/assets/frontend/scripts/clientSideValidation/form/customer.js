d860e2855b69e7edc516f5b9a8f63dee821c1ee7
Rostislav Vitek <vitek@netdevelo.cz>
2014-10-08 14:15:09 +0200

    validation groups - FE - customer detail

---
+++ project-base/web/assets/frontend/scripts/clientSideValidation/form/customer.js
@@ -0,0 +1,29 @@
+(function ($){
+	$(document).ready(function () {
+		var $customerDeliveryAddressForm = $('#customer_deliveryAddressData');
+		$customerDeliveryAddressForm.jsFormValidator({
+			'groups': function () {
+				
+				var groups = ['Default'];
+				if ($customerDeliveryAddressForm.find('#customer_deliveryAddressData_addressFilled').is(':checked')) {
+					groups.push('differentDeliveryAddress');
+				}
+				
+				return groups;
+			}
+		});
+		var $customerBillingAddressForm = $('#customer_billingAddressData');
+		$customerBillingAddressForm.jsFormValidator({
+			'groups': function () {
+
+			var groups = ['Default'];
+			if ($customerBillingAddressForm.find('#customer_billingAddressData_companyCustomer').is(':checked')) {
+				groups.push('companyCustomer');
+			}
+
+			return groups;
+			}
+		});
+		
+	});
+})(jQuery);
