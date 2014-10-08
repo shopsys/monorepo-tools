4507b7580a526534ef0f2fb4f4d1da0634d500e6
Rostislav Vitek <vitek@netdevelo.cz>
2014-10-08 14:13:21 +0200

    validation groups - admin - customer detail

--- project-base/web/assets/admin/scripts/clientSideValidation/form/customer.js
+++ project-base/web/assets/admin/scripts/clientSideValidation/form/customer.js
@@ -1,16 +1,29 @@
 (function ($){
 	$(document).ready(function () {
-		var $customerForm = $('#customer_deliveryAddressData');
-		$customerForm.jsFormValidator({
+		var $customerDeliveryAddressForm = $('#customer_deliveryAddressData');
+		$customerDeliveryAddressForm.jsFormValidator({
 			'groups': function () {
-				
+
 				var groups = ['Default'];
-				if ($customerForm.find('#customer_deliveryAddressData_addressFilled').is(':checked')) {
+				if ($customerDeliveryAddressForm.find('#customer_deliveryAddressData_addressFilled').is(':checked')) {
 					groups.push('differentDeliveryAddress');
 				}
 				
 				return groups;
 			}
 		});
+		var $customerBillingAddressForm = $('#customer_billingAddressData');
+		$customerBillingAddressForm.jsFormValidator({
+			'groups': function () {
+
+				var groups = ['Default'];
+				if ($customerBillingAddressForm.find('#customer_billingAddressData_companyCustomer').is(':checked')) {
+					groups.push('companyCustomer');
+				}
+
+				return groups;
+			}
+		});
+		
 	});
 })(jQuery);
