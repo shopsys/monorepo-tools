15328af68a21d78cbad3637c46bf42458f008b3d
Rostislav Vitek <vitek@netdevelo.cz>
2014-10-10 10:38:09 +0200

    greater than or equal constraint revision

---
+++ project-base/web/assets/admin/scripts/clientSideValidation/ConstraintGreaterThanOrEqual.js
@@ -0,0 +1,21 @@
+SymfonyComponentValidatorConstraintsGreaterThanOrEqual = function() {
+	this.message = '';
+	this.value = null;
+
+    this.validate = function(value) {
+
+	var f = FpJsFormValidator;
+	var compareValue = value.toString().replace(',', '.');
+	var regexpNumber = /^[-+]?[0-9]*(\.|,)?[0-9]+$/;
+	var isNumber = regexpNumber.test(compareValue);
+	if (f.isValueEmty(compareValue) || compareValue >= this.value || !isNumber) {
+		return [];
+	} else {
+		return [
+		this.message
+			.replace('{{ value }}', String(value))
+			.replace('{{ compared_value }}', String(this.value))
+		];
+	}
+	};
+};
