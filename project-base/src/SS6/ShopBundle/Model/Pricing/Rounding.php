d806788458f2e49d522b518bc90ade6b4feb1fb4
Miroslav Sustek <miroslav.sustek@netdevelo.cz>
2014-10-13 14:27:28 +0200

    round price with vat according to the setting

--- project-base/src/SS6/ShopBundle/Model/Pricing/Rounding.php
+++ project-base/src/SS6/ShopBundle/Model/Pricing/Rounding.php
@@ -5,11 +5,42 @@ namespace SS6\ShopBundle\Model\Pricing;
 class Rounding {
 
 	/**
+	 * @var \SS6\ShopBundle\Model\Pricing\PricingSetting
+	 */
+	private $pricingSetting;
+
+	/**
+	 * @param \SS6\ShopBundle\Model\Pricing\PricingSetting $pricingSetting
+	 */
+	public function __construct(PricingSetting $pricingSetting) {
+		$this->pricingSetting = $pricingSetting;
+	}
+
+	/**
 	 * @param string $priceWithVat
 	 * @return string
 	 */
 	public function roundPriceWithVat($priceWithVat) {
-		return round($priceWithVat, 0);
+		$roundingType = $this->pricingSetting->getRoundingType();
+
+		switch ($roundingType) {
+			case PricingSetting::ROUNDING_TYPE_HUNDREDTHS:
+				$roundedPriceWithVat = round($priceWithVat, 2);
+				break;
+
+			case PricingSetting::ROUNDING_TYPE_FIFTIES:
+				$roundedPriceWithVat = round($priceWithVat * 2, 0) / 2;
+				break;
+
+			case PricingSetting::ROUNDING_TYPE_INTEGER:
+				$roundedPriceWithVat = round($priceWithVat, 0);
+				break;
+
+			default:
+				throw new \SS6\ShopBundle\Model\Pricing\Exception\InvalidRoundingTypeException();
+		}
+
+		return $roundedPriceWithVat;
 	}
 
 	/**
