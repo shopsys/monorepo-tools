c63355a8f44c065028a1dd4e378b9b2c85158845
Rostislav Vitek <vitek@netdevelo.cz>
2014-10-13 16:23:43 +0200

    added slick carousel to project

---
+++ project-base/web/assets/frontend/scripts/slickInit.js
@@ -0,0 +1,20 @@
+(function ($) {
+
+	SS6 = window.SS6 || {};
+	SS6.slickInit = SS6.slickInit || {};
+
+	SS6.slickInit.init = function () {
+		$('.my-carousel').slick({
+			dots: true,
+			arrows: false,
+			autoplay: false,
+		});	
+	}
+
+
+	$(document).ready(function () {
+		SS6.slickInit.init();
+	});
+
+})(jQuery);
+
