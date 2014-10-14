e28e698f5290540a051021ae33ea3b4431c875b1
Rostislav Vitek <vitek@netdevelo.cz>
2014-10-14 14:32:05 +0200

    form for slider item

---
+++ project-base/src/SS6/ShopBundle/Form/Admin/Slider/SliderItemFormType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace SS6\ShopBundle\Form\Admin\Slider;
+
+use SS6\ShopBundle\Model\FileUpload\FileUpload;
+use SS6\ShopBundle\Form\FileUploadType;
+use SS6\ShopBundle\Model\Slider\SliderItemData;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints;
+
+class SliderItemFormType extends AbstractType {
+
+	/**
+	 * @var \SS6\ShopBundle\Model\FileUpload\FileUpload 
+	 */
+	private $fileUpload;
+	
+	/**
+	 * @param \SS6\ShopBundle\Model\FileUpload\FileUpload $fileUpload
+	 */
+	public function __construct(FileUpload $fileUpload) {
+		$this->fileUpload = $fileUpload;
+	}
+
+	public function getName() {
+		return 'sliderItem';
+	}
+
+	/**
+	 * @param \SS6\ShopBundle\Form\Admin\Slider\FormBuilderInterface $builder
+	 * @param array $options
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$builder
+			->add('name', 'text', array(
+				'required' => true,
+			))
+			->add('image', new FileUploadType($this->fileUpload), array(
+				'required' => true,
+				'file_constraints' => array(
+					new Constraints\Image(array(
+						'mimeTypes' => array('image/png', 'image/jpg', 'image/jpeg'),
+						'mimeTypesMessage' => 'Obrázek může být pouze ve formátech jpg, png, gif nebo bmp',
+						'maxSize' => '2M',
+						'maxSizeMessage' => 'Nahraný obrázek ({{ size }} {{ suffix }}) může mít velikost maximálně {{ limit }} {{ suffix }}',
+					)),
+				),
+			))
+			->add('link', 'text', array(
+				'required' => true,
+			))
+			->add('save', 'submit');
+	}
+
+	public function setDefaultOptions(OptionsResolverInterface $resolver) {
+		$resolver->setDefaults(array(
+			'data_class' => SliderItemData::class,
+			'attr' => array('novalidate' => 'novalidate'),
+		));
+	}
+
+}
