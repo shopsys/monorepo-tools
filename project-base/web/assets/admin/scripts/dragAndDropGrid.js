260c6dd67dcf8d1818dc8d928572aaf586123dfc
Pavel Sidzina <sidzina@netdevelo.cz>
2014-10-17 12:40:39 +0200

    drag and drop style

--- project-base/web/assets/admin/scripts/dragAndDropGrid.js
+++ project-base/web/assets/admin/scripts/dragAndDropGrid.js
@@ -2,35 +2,35 @@
 
 	SS6 = SS6 || {};
 	SS6.dragAndDropGrid = SS6.dragAndDropGrid || {};
-	
+
 	SS6.dragAndDropGrid.init = function () {
 		$('.js-drag-and-drop-grid-rows').sortable({
 			cursor: 'move',
 			handle: '.js-drag-and-drop-grid-handle',
 			items: '.js-grid-row',
-			placeholder: 'js-drag-and-drop-grid-placeholder'
+			placeholder: 'table-drop'
 		});
-		
+
 		$('.js-drag-and-drop-grid-submit').click(function () {
 			var $grid = $(this).closest('.js-grid');
-			
+
 			var rows = $grid.find('.js-grid-row');
-			
+
 			var rowIds = [];
 			$.each(rows, function(index, row) {
 				rowIds.push($(row).data('drag-and-drop-grid-row-id'));
 			});
-			
+
 			SS6.dragAndDropGrid.saveOrder($grid, rowIds);
 		});
 	};
-	
+
 	SS6.dragAndDropGrid.saveOrder = function ($grid, rowIds) {
 		var data = {
 			serviceName: $grid.data('drag-and-drop-ordering-service-name'),
 			rowIds: rowIds
 		};
-		
+
 		$.ajax({
 			url: $grid.data('drag-and-drop-url-save-order'),
 			type: 'POST',
@@ -41,9 +41,9 @@
 			}
 		});
 	};
-	
+
 	$(document).ready(function () {
 		SS6.dragAndDropGrid.init();
 	});
-	
+
 })(jQuery);
