7dbf20ec36a0905218ebd53c9871640252b80dba
Rostislav Vitek <vitek@netdevelo.cz>
2014-10-23 10:51:01 +0200

    mail exception

---
+++ project-base/src/SS6/ShopBundle/Model/Customer/Mail/Exception/MailException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SS6\ShopBundle\Model\Customer\Mail\Exception;
+
+use SS6\ShopBundle\Model\Customer\Exception\CustomerException;
+
+interface MailException extends CustomerException {
+
+}
