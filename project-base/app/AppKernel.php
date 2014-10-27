17a89d8b01712516f9dd12aa4043478b4818eab3
Petr Kadlec <kadlec@netdevelo.cz>
2014-10-27 13:21:39 +0100

    add and customization PrezentDoctrineTranslatableBundle

--- project-base/app/AppKernel.php
+++ project-base/app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel {
 			new FM\ElfinderBundle\FMElfinderBundle(),
 			new Fp\JsFormValidatorBundle\FpJsFormValidatorBundle(),
 			new Kutny\AutowiringBundle\KutnyAutowiringBundle(),
+			new Prezent\Doctrine\TranslatableBundle\PrezentDoctrineTranslatableBundle(),
 			new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle(),
 			new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 			new Symfony\Bundle\AsseticBundle\AsseticBundle(),
---
+++ project-base/src/SS6/ShopBundle/Model/Localize/TranslatableListener.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SS6\ShopBundle\Model\Localize;
+
+use Doctrine\ORM\Events;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Prezent\Doctrine\Translatable\EventListener\TranslatableListener as PrezentTranslatableListener;
+use Prezent\Doctrine\Translatable\Mapping\TranslatableMetadata;
+
+class TranslatableListener extends PrezentTranslatableListener {
+
+	public function getSubscribedEvents() {
+		return array(
+			Events::loadClassMetadata,
+			Events::postLoad,
+			Events::postPersist,
+		);
+	}
+
+	/**
+	 * @param LifecycleEventArgs $args
+	 */
+	public function postPersist(LifecycleEventArgs $args) {
+		$entity = $args->getEntity();
+		$metadata = $this->getTranslatableMetadata(get_class($entity));
+		if ($metadata instanceof TranslatableMetadata) {
+			if ($metadata->fallbackLocale) {
+				$metadata->fallbackLocale->setValue($entity, $this->getFallbackLocale());
+			}
+
+			if ($metadata->currentLocale) {
+				$metadata->currentLocale->setValue($entity, $this->getCurrentLocale());
+			}
+		}
+	}
+
+}
