3d05700c1439d0fb41075acf51c93f8086a5a93d
Miroslav Sustek <miroslav.sustek@netdevelo.cz>
2014-10-30 16:04:07 +0100

    test different locale on domains

---
+++ project-base/src/SS6/ShopBundle/TestsCrawler/Localize/LocalizeListenerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SS6\ShopBundle\TestsCrawler\Localize;
+
+use SS6\ShopBundle\Component\Test\DatabaseTestCase;
+
+class LocalizeListenerTest extends DatabaseTestCase {
+
+	public function testProductDetailLocaleCs() {
+		$crawler = $this->getClient()->request('GET', 'product/detail/3');
+
+		$this->assertGreaterThan(
+			0,
+			$crawler->filter('html:contains("Katalogové číslo")')->count()
+		);
+	}
+
+	public function testProductDetailLocaleEn() {
+		$domain = $this->getContainer()->get('ss6.shop.domain');
+		/* @var $domain \SS6\ShopBundle\Model\Domain\Domain */
+
+		$domain->switchDomainById(2);
+
+		$crawler = $this->getClient()->request('GET', 'product/detail/3');
+
+		$this->assertGreaterThan(
+			0,
+			$crawler->filter('html:contains("Catalogue number")')->count()
+		);
+	}
+
+}
