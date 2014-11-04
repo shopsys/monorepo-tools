eb7a1f44b67c8b6712009c5c18e65899e969f616
Miroslav Sustek <miroslav.sustek@netdevelo.cz>
2014-11-04 13:52:49 +0100

    AutoValidatorAnnotationLoaderTest

---
+++ project-base/src/SS6/ShopBundle/Tests/Model/Component/Validator/DataObject.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SS6\ShopBundle\Tests\Model\Component\Validator;
+
+use SS6\ShopBundle\Component\Validator;
+
+/**
+ * @Validator\Auto(entity="SS6\ShopBundle\Tests\Model\Component\Validator\Entity")
+ */
+class DataObject {
+
+	private $name;
+
+	private $short;
+
+}
