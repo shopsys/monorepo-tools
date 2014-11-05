0f7d159d10730446e55ca6292d923365750f7220
Petr Kadlec <kadlec@netdevelo.cz>
2014-11-05 14:13:58 +0100

    fix PHPMD

--- project-base/src/SS6/ShopBundle/Form/Admin/Transport/TransportFormType.php
+++ project-base/src/SS6/ShopBundle/Form/Admin/Transport/TransportFormType.php
@@ -43,6 +43,7 @@ class TransportFormType extends AbstractType {
 	/**
 	 * @param \Symfony\Component\Form\FormBuilderInterface $builder
 	 * @param array $options
+	 * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
 	 */
 	public function buildForm(FormBuilderInterface $builder, array $options) {
 		$builder
