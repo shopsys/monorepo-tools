168115d773c3f2a5b6039c1408ef2312fe8ec166
Petr Kadlec <kadlec@netdevelo.cz>
2014-11-11 09:29:42 +0100

    add RoutingBundle with chain router

--- project-base/app/AppKernel.php
+++ project-base/app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel {
 			new Symfony\Bundle\SecurityBundle\SecurityBundle(),
 			new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
 			new Symfony\Bundle\TwigBundle\TwigBundle(),
+			new Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
 			new Ivory\CKEditorBundle\IvoryCKEditorBundle(), // has to be loaded after FrameworkBundle and TwigBundle
 			new SS6\ShopBundle\SS6ShopBundle(), // must be loaded as last, because translations must overwrite other bundles
 		);
