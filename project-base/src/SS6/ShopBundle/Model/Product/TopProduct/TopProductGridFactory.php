e3c999606c550442b9c868dcf875541cb6210f8b
Petr Kadlec <kadlec@netdevelo.cz>
2014-11-25 14:28:57 +0100

    fix for multilangue of products

--- project-base/src/SS6/ShopBundle/Model/Product/TopProduct/TopProductGridFactory.php
+++ project-base/src/SS6/ShopBundle/Model/Product/TopProduct/TopProductGridFactory.php
@@ -2,12 +2,15 @@
 
 namespace SS6\ShopBundle\Model\Product\TopProduct;
 
+use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\ORM\EntityManager;
+use SS6\ShopBundle\Model\Domain\SelectedDomain;
 use SS6\ShopBundle\Model\Grid\QueryBuilderDataSource;
 use SS6\ShopBundle\Model\Grid\GridFactoryInterface;
 use SS6\ShopBundle\Model\Grid\GridFactory;
-use SS6\ShopBundle\Model\Domain\SelectedDomain;
+use SS6\ShopBundle\Model\Localization\Localization;
 use SS6\ShopBundle\Model\Product\Product;
+use SS6\ShopBundle\Model\Product\ProductTranslation;
 
 class TopProductGridFactory implements GridFactoryInterface {
 
@@ -27,17 +30,20 @@ class TopProductGridFactory implements GridFactoryInterface {
 	private $selectedDomain;
 
 	/**
-	 * @param \Doctrine\ORM\EntityManager $em
-	 * @param \SS6\ShopBundle\Model\Grid\GridFactory $gridFactory
+	 * @var \SS6\ShopBundle\Model\Localization\Localization
 	 */
+	private $localization;
+
 	public function __construct(
 		EntityManager $em,
 		GridFactory $gridFactory,
-		SelectedDomain $selectedDomain
+		SelectedDomain $selectedDomain,
+		Localization $localization
 	) {
 		$this->em = $em;
 		$this->gridFactory = $gridFactory;
 		$this->selectedDomain = $selectedDomain;
+		$this->localization = $localization;
 	}
 
 	/**
@@ -46,15 +52,17 @@ class TopProductGridFactory implements GridFactoryInterface {
 	public function create() {
 		$queryBuilder = $this->em->createQueryBuilder();
 		$queryBuilder
-			->select('tp, p')
+			->select('tp, pt')
 			->from(TopProduct::class, 'tp')
-			->join(Product::class, 'p', 'WITH', 'tp.product = p.id')
+			->join('tp.product', 'p')
+			->join('p.translations', 'pt', Join::WITH, 'pt.locale = :locale')
 			->where('tp.domainId = :selectedDomainId')
-			->setParameter('selectedDomainId', $this->selectedDomain->getId());
+			->setParameter('selectedDomainId', $this->selectedDomain->getId())
+			->setParameter('locale', $this->localization->getDefaultLocale());
 		$dataSource = new QueryBuilderDataSource($queryBuilder, 'tp.id');
 
 		$grid = $this->gridFactory->create('topProductList', $dataSource);
-		$grid->addColumn('product', 'p.name', 'Produkt');
+		$grid->addColumn('product', 'pt.name', 'Produkt');
 		$grid->setActionColumnClassAttribute('table-col table-col-10');
 		$grid->addActionColumn('delete', 'Smazat', 'admin_topproduct_delete', array('id' => 'tp.id'))
 			->setConfirmMessage('Opravdu chcete odebrat tento produkt z akce na titulní stránce?');
