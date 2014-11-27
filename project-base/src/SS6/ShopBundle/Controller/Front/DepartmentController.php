ed8ae4dbf708a0a0d3cf9425f4bb722544c4414b
Miroslav Sustek <miroslav.sustek@netdevelo.cz>
2014-11-27 21:42:40 +0100

    rename Department/menu to Department/panel

--- project-base/src/SS6/ShopBundle/Controller/Front/DepartmentController.php
+++ project-base/src/SS6/ShopBundle/Controller/Front/DepartmentController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DepartmentController extends Controller {
 
-	public function menuAction() {
+	public function panelAction() {
 		$departmentFacade = $this->get('ss6.shop.department.department_facade');
 		/* @var $departmentFacade \SS6\ShopBundle\Model\Department\DepartmentFacade */
 
 		$departments = $departmentFacade->getAllWithTranslation();
 
-		return $this->render('@SS6Shop/Front/Content/Department/menu.html.twig', array(
+		return $this->render('@SS6Shop/Front/Content/Department/panel.html.twig', array(
 			'departments' => $departments,
 		));
 	}
