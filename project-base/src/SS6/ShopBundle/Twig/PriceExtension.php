1bb176e9fb553d6e004e484bfbda0109ca4d2f87
Rostislav Vitek <vitek@netdevelo.cz>
2014-12-10 16:29:51 +0100

    view corresponding currency symbol on FE

--- project-base/src/SS6/ShopBundle/Twig/PriceExtension.php
+++ project-base/src/SS6/ShopBundle/Twig/PriceExtension.php
@@ -3,6 +3,7 @@
 namespace SS6\ShopBundle\Twig;
 
 use Symfony\Component\Translation\TranslatorInterface;
+use SS6\ShopBundle\Model\Domain\Domain;
 use SS6\ShopBundle\Model\Pricing\Currency\CurrencyFacade;
 use Twig_Extension;
 use Twig_SimpleFilter;
@@ -19,9 +20,15 @@ class PriceExtension extends Twig_Extension {
 	 */
 	private $currencyFacade;
 
-	public function __construct(TranslatorInterface $translator, CurrencyFacade $currencyFacade) {
+	/**
+	 * @var \SS6\ShopBundle\Model\Domain\Domain
+	 */
+	private $domain;
+
+	public function __construct(TranslatorInterface $translator, CurrencyFacade $currencyFacade, Domain $domain) {
 		$this->translator = $translator;
 		$this->currencyFacade = $currencyFacade;
+		$this->domain = $domain;
 	}
 
 	/**
@@ -29,6 +36,7 @@ class PriceExtension extends Twig_Extension {
 	 */
 	public function getFilters() {
 		return array(
+			new Twig_SimpleFilter('defaultPrice', array($this, 'defaultPriceFilter'), array('is_safe' => array('html'))),
 			new Twig_SimpleFilter('price', array($this, 'priceFilter'), array('is_safe' => array('html'))),
 			new Twig_SimpleFilter('priceText', array($this, 'priceTextFilter'), array('is_safe' => array('html'))),
 		);
@@ -41,9 +49,22 @@ class PriceExtension extends Twig_Extension {
 	public function priceFilter($price) {
 		$price = (float)$price;
 		$price = number_format($price, 2, ',', ' ');
+		$currencySymbol = $this->currencyFacade->getDomainDefaultCurrencyByDomainId($this->domain->getId())->getSymbol();
+		$price = htmlspecialchars($price, ENT_QUOTES, 'UTF-8') . '&nbsp;' . $currencySymbol;
+
+		return $price;
+	}
+
+	/**
+	 * @param string $price
+	 * @return string
+	 */
+	public function defaultPriceFilter($price) {
+		$price = (float)$price;
+		$price = number_format($price, 2, ',', ' ');
 		$currencySymbol = $this->currencyFacade->getDefaultCurrency()->getSymbol();
 		$price = htmlspecialchars($price, ENT_QUOTES, 'UTF-8') . '&nbsp;' . $currencySymbol;
-		
+
 		return $price;
 	}
 
