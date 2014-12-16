17a69a7c02a9bd480b127d2a2ca159a51d1be2a5
Petr Kadlec <kadlec@netdevelo.cz>
2014-12-16 16:49:21 +0100

    refactor showPriceCategoriesForDomain for more reusability

---
+++ project-base/web/assets/admin/scripts/customerForm.js
@@ -0,0 +1,10 @@
+(function ($) {
+
+	$(document).ready(function() {
+		SS6.selectToggle.toggleOptgroupOnControlChange(
+			$('#customer_userData_pricingGroup'),
+			$('#customer_userData_domainId')
+		);
+	});
+
+})(jQuery);
