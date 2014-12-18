f87c8fbae7ea60160f472173560606af2056e9b9
Rostislav Vitek <vitek@netdevelo.cz>
2014-12-18 11:19:36 +0100

    switching between button and info text+revert button when image is marked for deletion

--- project-base/web/assets/admin/scripts/productImagesDelete.js
+++ project-base/web/assets/admin/scripts/productImagesDelete.js
@@ -8,23 +8,27 @@
 	};
 
 	SS6.productImagesDelete.deleteImage = function (imageId) {
-		SS6.choiceControl.select('#product_imagesToDelete', imageId);
+		SS6.choiceControl.select('#product_edit_imagesToDelete', imageId);
 		SS6.productImagesDelete.updateCss();
 	};
 
-	SS6.productImagesDelete.revert = function () {
-		SS6.choiceControl.deselectAll('#product_imagesToDelete');
+	SS6.productImagesDelete.revert = function (imageId) {
+		SS6.choiceControl.deselect('#product_edit_imagesToDelete', imageId);
 		SS6.productImagesDelete.updateCss();
 	};
 
 	SS6.productImagesDelete.updateCss = function () {
-		var imageIds = SS6.choiceControl.getSelectedValues('#product_imagesToDelete');
+		var imageIds = SS6.choiceControl.getSelectedValues('#product_edit_imagesToDelete');
 		$('.js-image-to-delete').each(function (key, element) {
 			var $element = $(element);
 			if ($.inArray($element.data('id'), imageIds) !== -1) {
 				$element.addClass('list-image__item__in--removed');
+				$(this).closest('.js-image-delete-button-parent').find('.js-image-delete-button').addClass('display-none');
+				$(this).closest('.js-image-delete-button-parent').find('.js-image-delete-confirmed').removeClass('display-none');
 			} else {
 				$element.removeClass('list-image__item__in--removed');
+				$(this).closest('.js-image-delete-button-parent').find('.js-image-delete-confirmed').addClass('display-none');
+				$(this).closest('.js-image-delete-button-parent').find('.js-image-delete-button').removeClass('display-none');
 			}
 		});
 	};
