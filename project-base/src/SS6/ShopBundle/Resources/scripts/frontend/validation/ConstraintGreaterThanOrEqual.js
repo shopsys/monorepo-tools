d62f446b3f64597ed3c794408c0ddea6b53024d3
Rostislav Vitek <vitek@netdevelo.cz>
2015-01-20 13:48:04 +0100

    fixed formatting

--- project-base/src/SS6/ShopBundle/Resources/scripts/frontend/validation/ConstraintGreaterThanOrEqual.js
+++ project-base/src/SS6/ShopBundle/Resources/scripts/frontend/validation/ConstraintGreaterThanOrEqual.js
@@ -4,17 +4,17 @@ SymfonyComponentValidatorConstraintsGreaterThanOrEqual = function() {
 
 	this.validate = function(value) {
 
-	var f = FpJsFormValidator;
-	var compareValue = SS6.number.parseNumber(value);
+		var f = FpJsFormValidator;
+		var compareValue = SS6.number.parseNumber(value);
 
-	if (f.isValueEmty(value) || (compareValue !== null && compareValue >= this.value)) {
-		return [];
-	} else {
-		return [
-		this.message
-			.replace('{{ value }}', String(value))
-			.replace('{{ compared_value }}', String(this.value))
-		];
-	}
+		if (f.isValueEmty(value) || (compareValue !== null && compareValue >= this.value)) {
+			return [];
+		} else {
+			return [
+				this.message
+					.replace('{{ value }}', String(value))
+					.replace('{{ compared_value }}', String(this.value))
+			];
+		}
 	};
 };
