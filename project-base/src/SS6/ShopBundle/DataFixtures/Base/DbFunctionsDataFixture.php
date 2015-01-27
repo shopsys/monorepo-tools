0d1e5312c25d7f803a754983e951d14576712a17
Petr Kadlec <kadlec@netdevelo.cz>
2015-01-27 10:29:49 +0100

    enable postgres unaccent extension

--- project-base/src/SS6/ShopBundle/DataFixtures/Base/DbFunctionsDataFixture.php
+++ project-base/src/SS6/ShopBundle/DataFixtures/Base/DbFunctionsDataFixture.php
@@ -11,6 +11,7 @@ class DbFunctionsDataFixture extends AbstractNativeFixture {
 	 * @param \Doctrine\Common\Persistence\ObjectManager $manager
 	 */
 	public function load(ObjectManager $manager) {
+		$this->executeNativeQuery('CREATE EXTENSION IF NOT EXISTS unaccent');
 		$this->executeNativeQuery('DROP FUNCTION IF EXISTS immutable_unaccent(text)');
 		$this->executeNativeQuery('CREATE FUNCTION immutable_unaccent(text)
 			RETURNS text AS
