cda5f5404833a753b576f8c6c37d91c49258f11e
Rostislav Vitek <vitek@netdevelo.cz>
2015-01-28 10:13:01 +0100

    service for adding join safely to QB

---
+++ project-base/src/SS6/ShopBundle/Component/DoctrineWalker/QueryBuilderService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SS6\ShopBundle\Component\DoctrineWalker;
+
+use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\QueryBuilder;
+
+class QueryBuilderService {
+
+	/**
+	 * @param \Doctrine\ORM\QueryBuilder $queryBuilder
+	 * @param string $class
+	 * @param string $alias
+	 * @param string $condition
+	 */
+	public function addOrExtendJoin(QueryBuilder $queryBuilder, $class, $alias, $condition) {
+		$joinAlreadyUsed = false;
+		foreach ($queryBuilder->getDQLPart('join')['p'] as $join) {
+			if ($join->getJoin() === $class) {
+				$joinAlreadyUsed = true;
+			}
+		}
+		if (!$joinAlreadyUsed) {
+			$queryBuilder->join(
+				$class,
+				$alias,
+				Join::WITH,
+				$condition
+			);
+		} else {
+			$queryBuilder->andWhere($condition);
+		}
+	}
+}
