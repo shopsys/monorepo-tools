18cafaa469a497302ead45f1680ea8ca77f0d6ad
Miroslav Sustek <miroslav.sustek@netdevelo.cz>
2015-01-29 16:28:03 +0100

    tests

---
+++ project-base/src/SS6/ShopBundle/Tests/Model/Product/Availability/ProductAvailabilityCalculationTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace SS6\ShopBundle\Tests\Model\Product\Availability;
+
+use SS6\ShopBundle\Component\Test\FunctionalTestCase;
+use SS6\ShopBundle\DataFixtures\Base\AvailabilityDataFixture;
+use SS6\ShopBundle\Model\Product\Availability\AvailabilityFacade;
+use SS6\ShopBundle\Model\Product\Availability\ProductAvailabilityCalculation;
+use SS6\ShopBundle\Model\Product\Product;
+use SS6\ShopBundle\Model\Product\ProductData;
+
+class ProductAvailabilityCalculationTest extends FunctionalTestCase {
+
+	/**
+	 * @dataProvider getTestGetCalculatedAvailabilityData
+	 */
+	public function testGetCalculatedAvailability(
+		$usingStock,
+		$stockQuantity,
+		$availability,
+		$outOfStockAvailability,
+		$defaultInStockAvailability,
+		$expectedCalculatedAvailability
+	) {
+		$productData = new ProductData();
+		$productData->usingStock = $usingStock;
+		$productData->stockQuantity = $stockQuantity;
+		$productData->availability = $availability;
+		$productData->outOfStockAvailability = $outOfStockAvailability;
+
+		$product = new Product($productData);
+
+		$availabilityFacadeMock = $this->getMockBuilder(AvailabilityFacade::class)
+			->setMethods(['getDefaultInStockAvailability'])
+			->disableOriginalConstructor()
+			->getMock();
+		$availabilityFacadeMock->expects($this->any())->method('getDefaultInStockAvailability')
+			->will($this->returnValue($defaultInStockAvailability));
+
+		$productAvailabilityCalculation = new ProductAvailabilityCalculation($availabilityFacadeMock);
+
+		$calculatedAvailability = $productAvailabilityCalculation->getCalculatedAvailability($product);
+
+		$this->assertEquals($expectedCalculatedAvailability, $calculatedAvailability);
+	}
+
+	public function getTestGetCalculatedAvailabilityData() {
+		return [
+			[
+				'usingStock' => false,
+				'stockQuantity' => null,
+				'availability' => $this->getReference(AvailabilityDataFixture::IN_STOCK),
+				'outOfStockAvailability' => null,
+				'defaultInStockAvailability' => $this->getReference(AvailabilityDataFixture::IN_STOCK),
+				'calculatedAvailability' => $this->getReference(AvailabilityDataFixture::IN_STOCK),
+			],
+			[
+				'usingStock' => true,
+				'stockQuantity' => 5,
+				'availability' => null,
+				'outOfStockAvailability' => $this->getReference(AvailabilityDataFixture::OUT_OF_STOCK),
+				'defaultInStockAvailability' => $this->getReference(AvailabilityDataFixture::IN_STOCK),
+				'calculatedAvailability' => $this->getReference(AvailabilityDataFixture::IN_STOCK),
+			],
+			[
+				'usingStock' => true,
+				'stockQuantity' => 0,
+				'availability' => null,
+				'outOfStockAvailability' => $this->getReference(AvailabilityDataFixture::OUT_OF_STOCK),
+				'defaultInStockAvailability' => $this->getReference(AvailabilityDataFixture::IN_STOCK),
+				'calculatedAvailability' => $this->getReference(AvailabilityDataFixture::OUT_OF_STOCK),
+			],
+			[
+				'usingStock' => true,
+				'stockQuantity' => -1,
+				'availability' => null,
+				'outOfStockAvailability' => $this->getReference(AvailabilityDataFixture::OUT_OF_STOCK),
+				'defaultInStockAvailability' => $this->getReference(AvailabilityDataFixture::IN_STOCK),
+				'calculatedAvailability' => $this->getReference(AvailabilityDataFixture::OUT_OF_STOCK),
+			],
+		];
+	}
+
+}
