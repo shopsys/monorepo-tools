3baa90519897c3e11a0cee5922c939e1082e6e33
Petr Kadlec <kadlec@netdevelo.cz>
2015-01-30 16:29:14 +0100

    Admin: Product QuickSearch: separate general query builder for admin product list

--- project-base/src/SS6/ShopBundle/Model/Product/Listing/ProductListAdminFacade.php
+++ project-base/src/SS6/ShopBundle/Model/Product/Listing/ProductListAdminFacade.php
@@ -2,7 +2,7 @@
 
 namespace SS6\ShopBundle\Model\Product\Listing;
 
-use \SS6\ShopBundle\Model\Pricing\Group\PricingGroupFacade;
+use SS6\ShopBundle\Model\Pricing\Group\PricingGroupFacade;
 use SS6\ShopBundle\Model\Product\Listing\ProductListAdminRepository;
 
 class ProductListAdminFacade {
@@ -27,16 +27,27 @@ class ProductListAdminFacade {
 	}
 
 	/**
-	 * @param array|null $searchData
 	 * @return \Doctrine\ORM\QueryBuilder
 	 */
-	public function getQueryBuilderByQuickSearchData(array $searchData = null) {
+	public function getProductListQueryBuilder() {
 		/**
 		 * temporary solution -
 		 * when product price type calculation is set to manual, price for first domain is shown in admin product list
 		 */
 		$defaultPricingGroupId = $this->pricingGroupFacade->getDefaultPricingGroupByDomainId(1)->getId();
-		return $this->productListAdminRepository->getQueryBuilderByQuickSearchData($defaultPricingGroupId, $searchData);
+
+		return $this->productListAdminRepository->getProductListQueryBuilder($defaultPricingGroupId);
+	}
+
+	/**
+	 * @param array|null $searchData
+	 * @return \Doctrine\ORM\QueryBuilder
+	 */
+	public function getQueryBuilderByQuickSearchData(array $searchData = null) {
+		$queryBuilder = $this->getProductListQueryBuilder();
+		$this->productListAdminRepository->extendQueryBuilderByQuickSearchData($queryBuilder, $searchData);
+
+		return $queryBuilder;
 	}
 
 }
