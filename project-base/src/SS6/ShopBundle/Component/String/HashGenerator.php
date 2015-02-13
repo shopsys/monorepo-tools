8a77187dfd1f0257a6bdae9241baf68b7ae513a8
Miroslav Sustek <miroslav.sustek@shopsys.cz>
2015-02-13 14:28:10 +0100

    HashGenerator: evenly distributed random characters

--- project-base/src/SS6/ShopBundle/Component/String/HashGenerator.php
+++ project-base/src/SS6/ShopBundle/Component/String/HashGenerator.php
@@ -13,21 +13,42 @@ class HashGenerator {
 	public function generateHash($length) {
 		$numberOfChars = strlen($this->characters);
 
-		$iv = mcrypt_create_iv($length, MCRYPT_DEV_URANDOM);
-		if ($iv === false) {
-			throw new \SS6\ShopBundle\Component\String\Exception\HashGenerationFailedException();
-		}
-
-		$randomChars = unpack('C*', $iv);
-
 		$hash = '';
 		for ($i = 1; $i <= $length; $i++) {
-			$randomIndex = $randomChars[$i] % $numberOfChars;
-
+			$randomIndex = $this->randomUnsigned32(0, $numberOfChars - 1);
 			$hash .= $this->characters[$randomIndex];
 		}
 
 		return $hash;
+	}
+
+	/**
+	 * @param int $min
+	 * @param int $max
+	 * @throws \SS6\ShopBundle\Component\String\Exception\HashGenerationFailedException
+	 */
+	private function randomUnsigned32($min, $max) {
+		$iv = mcrypt_create_iv(4, MCRYPT_DEV_URANDOM);
+		if ($iv === false) {
+			throw new \SS6\ShopBundle\Component\String\Exception\HashGenerationFailedException();
+		}
+
+		$unpacked32 = unpack('V', $iv);
+		$randomUnsigned32 = $unpacked32[1];
+
+		$normalizedRandom = $randomUnsigned32 / 4294967295; // [0.0 - 1.0]
+
+		$result = $min + ($max - $min) * $normalizedRandom;
+		$roundedResult = (int)round($result);
+
+		// don't let rounding get number out of range
+		if ($roundedResult < $min) {
+			$roundedResult = $min;
+		} elseif ($roundedResult > $max) {
+			$roundedResult = $max;
+		}
+
+		return $roundedResult;
 	}
 
 }
