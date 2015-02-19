36f1cfb103ce5362f7dfddb346ba658ba85c65e7
Rostislav Vitek <vitek@netdevelo.cz>
2015-02-19 17:48:34 +0100

    title page: meta description and title set from setting

--- project-base/src/SS6/ShopBundle/Controller/Front/DefaultController.php
+++ project-base/src/SS6/ShopBundle/Controller/Front/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace SS6\ShopBundle\Controller\Front;
 
+use SS6\ShopBundle\Model\Seo\SeoSettingFacade;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller {
@@ -13,6 +14,8 @@ class DefaultController extends Controller {
 		/* @var $domain \SS6\ShopBundle\Model\Domain\Domain */
 		$topProductsFacade = $this->get('ss6.shop.product.top_product.top_product_facade');
 		/* @var $topProductsFacade \SS6\ShopBundle\Model\Product\TopProduct\TopProductFacade */
+		$seoSettingFacade = $this->get(SeoSettingFacade::class);
+		/* @var $seoSettingFacade \SS6\ShopBundle\Model\Seo\SeoSettingFacade */
 
 		$sliderItems = $sliderItemFacade->getAllOnCurrentDomain();
 		$topProductsDetails = $topProductsFacade->getAllProductDetailsByDomainId($domain->getId());
@@ -20,6 +23,8 @@ class DefaultController extends Controller {
 		return $this->render('@SS6Shop/Front/Content/Default/index.html.twig', [
 			'sliderItems' => $sliderItems,
 			'topProductsDetails' => $topProductsDetails,
+			'title' => $seoSettingFacade->getTitleMainPage($domain->getId()),
+			'metaDescription' => $seoSettingFacade->getDescriptionMainPage($domain->getId()),
 		]);
 	}
 
