829601fd59b7dd3bae89ee477fcf0cc237feda9c
Petr Kadlec <kadlec@shopsys.cz>
2015-02-24 13:04:17 +0100

    FriendlyUrl: tests

--- project-base/src/SS6/ShopBundle/Tests/Component/String/TransformStringTest.php
+++ project-base/src/SS6/ShopBundle/Tests/Component/String/TransformStringTest.php
@@ -7,14 +7,76 @@ use SS6\ShopBundle\Component\String\TransformString;
 
 class TransformStringTest extends PHPUnit_Framework_TestCase {
 
-	public function testSafeFilename() {
-		$this->assertEquals('escrzyaie.dat', TransformString::safeFilename('ěščřžýáíé.dat'));
-		$this->assertEquals('ESCRZYAIE.DAT', TransformString::safeFilename('ĚŠČŘŽÝÁÍÉ.DAT'));
-		$this->assertEquals('Foo_Bar.dat', TransformString::safeFilename('Foo     Bar.dat'));
-		$this->assertEquals('Foo-Bar.dat', TransformString::safeFilename('Foo-Bar.dat'));
-		$this->assertEquals('_._Foo.dat', TransformString::safeFilename('../../Foo.dat'));
-		$this->assertEquals('_._Foo.dat', TransformString::safeFilename('..\\..\\Foo.dat'));
-		$this->assertEquals('foo.dat', TransformString::safeFilename('.foo.dat'));
+	public function safeFilenameProvider() {
+		return [
+			[
+				'actual' => 'ěščřžýáíé.dat',
+				'expected' => 'escrzyaie.dat',
+			],
+			[
+				'actual' => 'ĚŠČŘŽÝÁÍÉ.DAT',
+				'expected' => 'ESCRZYAIE.DAT',
+			],
+			[
+				'actual' => 'Foo     Bar.dat',
+				'expected' => 'Foo_Bar.dat',
+			],
+			[
+				'actual' => 'Foo-Bar.dat',
+				'expected' => 'Foo-Bar.dat',
+			],
+			[
+				'actual' => '../../Foo.dat',
+				'expected' => '_._Foo.dat',
+			],
+			[
+				'actual' => '..\\..\\Foo.dat',
+				'expected' => '_._Foo.dat',
+			],
+			[
+				'actual' => '.foo.dat',
+				'expected' => 'foo.dat',
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider safeFilenameProvider
+	 */
+	public function testSafeFilename($actual, $expected) {
+		$this->assertEquals($expected, TransformString::safeFilename($actual));
+	}
+
+	public function stringToFriendlyUrlProvider() {
+		return [
+			[
+				'actual' => 'ěščřžýáíé foo',
+				'expected' => 'escrzyaie-foo',
+			],
+			[
+				'actual' => 'ĚŠČŘŽÝÁÍÉ   ',
+				'expected' => 'escrzyaie',
+			],
+			[
+				'actual' => 'Foo     Bar-Baz',
+				'expected' => 'foo-bar-baz',
+			],
+			[
+				'actual' => 'foo-bar_baz',
+				'expected' => 'foo-bar_baz',
+			],
+			[
+				'actual' => '$€@!?<>=;~%^&',
+				'expected' => '',
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider stringToFriendlyUrlProvider
+	 */
+	public function testStringToFriendlyUrl($actual, $expected) {
+		$this->assertEquals($expected, TransformString::stringToFriendlyUrl($actual));
 	}
 
 }
