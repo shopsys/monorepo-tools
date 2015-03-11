4b3ec5803b409b43430e6d18b4492a4de0300cf5
Petr Kadlec <kadlec@shopsys.cz>
2015-03-11 19:09:59 +0100

    collapsing of categories in category tree

---
+++ project-base/src/SS6/ShopBundle/Resources/scripts/admin/categoryTree.formItem.js
@@ -0,0 +1,83 @@
+(function ($){
+
+	SS6 = SS6 || {};
+	SS6.categoryTree = SS6.categoryTree || {};
+	SS6.categoryTree.FormItem = SS6.categoryTree.FormItem || {};
+
+	SS6.categoryTree.FormItem = function ($item, parent) {
+		var self = this;
+		var status = null;
+		var $statusIcon = $item.find('.js-category-tree-form-item-icon:first');
+		self.parent = parent;
+		self.children = [];
+
+		var $childrenContainer = $item.find('.js-category-tree-form-children-container:first');
+
+		this.init = function () {
+			initChildren();
+			initStatus();
+			updateStatusIcon();
+
+			$statusIcon.click(self.statusToggle);
+		};
+
+		var initChildren = function () {
+			$childrenContainer.find('> .js-category-tree-form-item').each(function () {
+				var childItem = new SS6.categoryTree.FormItem($(this), self);
+				childItem.init();
+				self.children.push(childItem);
+			});
+		};
+
+		var initStatus = function () {
+			if (self.children.length > 0) {
+				status = 'close';
+			} else {
+				status = 'none';
+			}
+		};
+
+		this.statusToggle = function () {
+			if (status === 'close') {
+				self.open();
+			} else if (status === 'open') {
+				self.close();
+			}
+		};
+
+		this.open = function () {
+			if (status === 'close') {
+				$childrenContainer.slideDown(function () {
+					status = 'open';
+					updateStatusIcon();
+				});
+			}
+		};
+
+		this.close = function () {
+			if (status === 'open') {
+				$childrenContainer.slideUp(function () {
+					status = 'close';
+					updateStatusIcon();
+				});
+			}
+		};
+
+		var updateStatusIcon = function () {
+			$statusIcon.removeClass('fa-plus-square-o fa-minus-square-o fa-square-o');
+			switch (status) {
+				case 'open':
+					$statusIcon.addClass('fa-minus-square-o');
+					break;
+				case 'close':
+					$statusIcon.addClass('fa-plus-square-o');
+					break;
+				case 'none':
+					$statusIcon.addClass('fa-square-o');
+					break;
+			}
+		};
+
+	};
+
+})(jQuery);
