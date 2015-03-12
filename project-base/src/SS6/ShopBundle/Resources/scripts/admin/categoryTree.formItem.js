c998e3854fa009f823f99f9d4f95b2cd33a2668c
Petr Kadlec <kadlec@shopsys.cz>
2015-03-12 10:35:11 +0100

    refactor work with JS SS6.categoryTree.FormItem.status

--- project-base/src/SS6/ShopBundle/Resources/scripts/admin/categoryTree.formItem.js
+++ project-base/src/SS6/ShopBundle/Resources/scripts/admin/categoryTree.formItem.js
@@ -6,6 +6,10 @@
 
 	SS6.categoryTree.FormItem = function ($item, parent) {
 		var self = this;
+		self.STATUS_OPENED = 'opened';
+		self.STATUS_CLOSED = 'closed';
+		self.STATUS_NONE = 'none';
+
 		var status = null;
 		var $statusIcon = $item.find('.js-category-tree-form-item-icon:first');
 		var $checkbox = $item.find('.js-category-tree-form-item-checkbox:first');
@@ -17,7 +21,6 @@
 		this.init = function () {
 			initChildren();
 			initStatus();
-			updateStatusIcon();
 
 			$statusIcon.click(self.statusToggle);
 		};
@@ -31,12 +34,12 @@
 		};
 
 		var initStatus = function () {
-			// status could be set to "open" by children
+			// status could be set to "opened" by children
 			if (status === null) {
 				if (self.children.length > 0) {
 					self.close(true);
 				} else {
-					status = 'none';
+					setStatus(self.STATUS_NONE);
 				}
 
 				if ($checkbox.is(':checked')) {
@@ -48,26 +51,24 @@
 		};
 
 		this.statusToggle = function () {
-			if (status === 'close') {
+			if (status === self.STATUS_CLOSED) {
 				self.open();
-			} else if (status === 'open') {
+			} else if (status === self.STATUS_OPENED) {
 				self.close();
 			}
 		};
 
 		this.open = function () {
-			if (status === 'close') {
+			if (status === self.STATUS_CLOSED) {
 				$childrenContainer.slideDown(function () {
-					status = 'open';
-					updateStatusIcon();
+					setStatus(self.STATUS_OPENED);
 				});
 			}
 		};
 
 		this.openForceWithParentCascade = function () {
 			$childrenContainer.show();
-			status = 'open';
-			updateStatusIcon();
+			setStatus(self.STATUS_OPENED);
 			if (self.parent instanceof SS6.categoryTree.FormItem) {
 				self.parent.openForceWithParentCascade();
 			}
@@ -76,25 +77,29 @@
 		this.close = function (force) {
 			if (force === true) {
 				$childrenContainer.hide();
-				status = 'close';
-			} else if (status === 'open') {
+				setStatus(self.STATUS_CLOSED);
+			} else if (status === self.STATUS_OPENED) {
 				$childrenContainer.slideUp(function () {
-					status = 'close';
-					updateStatusIcon();
+					setStatus(self.STATUS_CLOSED);
 				});
 			}
+		};
+
+		var setStatus = function (newStatus) {
+			status = newStatus;
+			updateStatusIcon();
 		};
 
 		var updateStatusIcon = function () {
 			$statusIcon.removeClass('fa-plus-square-o fa-minus-square-o fa-square-o');
 			switch (status) {
-				case 'open':
+				case self.STATUS_OPENED:
 					$statusIcon.addClass('fa-minus-square-o');
 					break;
-				case 'close':
+				case self.STATUS_CLOSED:
 					$statusIcon.addClass('fa-plus-square-o');
 					break;
-				case 'none':
+				case self.STATUS_NONE:
 					$statusIcon.addClass('fa-square-o');
 					break;
 			}
