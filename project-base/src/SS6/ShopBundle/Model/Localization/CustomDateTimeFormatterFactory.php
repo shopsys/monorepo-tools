55bfa245c3767c5ed75220def7a2af942c3e64c1
Miroslav Sustek <miroslav.sustek@shopsys.cz>
2015-04-08 17:05:40 +0200

    DateTimeFormatterExtension

---
+++ project-base/src/SS6/ShopBundle/Model/Localization/CustomDateTimeFormatterFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SS6\ShopBundle\Model\Localization;
+
+use IntlDateFormatter;
+use SS6\ShopBundle\Component\Localization\DateTimeFormatPattern;
+use SS6\ShopBundle\Component\Localization\DateTimeFormatPatternRepository;
+use SS6\ShopBundle\Component\Localization\DateTimeFormatter;
+
+class CustomDateTimeFormatterFactory {
+
+	/**
+	 * @return \SS6\ShopBundle\Component\Localization\DateTimeFormatter
+	 */
+	public function create() {
+		$customDateTimeFormatPatternRepository = new DateTimeFormatPatternRepository();
+		$customDateTimeFormatPatternRepository->add(new DateTimeFormatPattern('y-MM-dd', 'en', IntlDateFormatter::MEDIUM));
+
+		$dateTimeFormatter = new DateTimeFormatter($customDateTimeFormatPatternRepository);
+
+		return $dateTimeFormatter;
+	}
+
+}
