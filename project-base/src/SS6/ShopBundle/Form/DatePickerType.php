46d24bf04bba88a77f0020af6a4408186c1a6879
Rostislav Vitek <vitek@netdevelo.cz>
2015-04-30 13:45:24 +0200

    [BG-244] datepicker - date in valid format

--- project-base/src/SS6/ShopBundle/Form/DatePickerType.php
+++ project-base/src/SS6/ShopBundle/Form/DatePickerType.php
@@ -7,12 +7,13 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class DatePickerType extends AbstractType {
 
-	const FORMAT = 'dd.MM.yyyy';
+	const FORMAT_PHP = 'dd.MM.yyyy';
+	const FORMAT_JS = 'dd.mm.yy';
 
 	public function setDefaultOptions(OptionsResolverInterface $resolver) {
 		$resolver->setDefaults([
 			'widget' => 'single_text',
-			'format' => self::FORMAT,
+			'format' => self::FORMAT_PHP,
 		]);
 	}
 
--- project-base/src/SS6/ShopBundle/Resources/scripts/admin/components/datePicker.js
+++ project-base/src/SS6/ShopBundle/Resources/scripts/admin/components/datePicker.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
 	$('.js-datePicker').each(function() {
 		$(this).datepicker({
-			'dateFormat': SS6.constant('\\SS6\\ShopBundle\\Form\\DatePickerType::FORMAT')
+			'dateFormat': SS6.constant('\\SS6\\ShopBundle\\Form\\DatePickerType::FORMAT_JS')
 		});
 	});
 });
