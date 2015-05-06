393e0806c78fa0b311793e080fa249583ebe3597
Miroslav Sustek <miroslav.sustek@shopsys.cz>
2015-05-06 13:05:53 +0200

    IntlCurrencyRepository: add list of supported currency codes

--- project-base/src/SS6/ShopBundle/Model/Localization/IntlCurrencyRepository.php
+++ project-base/src/SS6/ShopBundle/Model/Localization/IntlCurrencyRepository.php
@@ -3,31 +3,178 @@
 namespace SS6\ShopBundle\Model\Localization;
 
 use CommerceGuys\Intl\Currency\CurrencyRepository as BaseCurrencyRepository;
-use SS6\ShopBundle\Model\Pricing\Currency\CurrencyRepository;
 
 class IntlCurrencyRepository extends BaseCurrencyRepository {
 
-	/**
-	 * @var \SS6\ShopBundle\Model\Pricing\Currency\Currency[code]
-	 */
-	private $currencyByCodeCache;
-
-	/**
-	 * @var \SS6\ShopBundle\Model\Pricing\Currency\CurrencyRepository
-	 */
-	private $currencyRepository;
-
-	public function __construct(CurrencyRepository $currencyRepository) {
-		parent::__construct();
-
-		$this->currencyByCodeCache = [];
-		$this->currencyRepository = $currencyRepository;
-	}
+	const SUPPORTED_CURRENCY_CODES = [
+		'AED',
+		'AFN',
+		'ALL',
+		'AMD',
+		'ANG',
+		'AOA',
+		'ARS',
+		'AUD',
+		'AWG',
+		'AZN',
+		'BAM',
+		'BBD',
+		'BDT',
+		'BGN',
+		'BHD',
+		'BIF',
+		'BMD',
+		'BND',
+		'BOB',
+		'BRL',
+		'BSD',
+		'BTN',
+		'BWP',
+		'BYR',
+		'BZD',
+		'CAD',
+		'CDF',
+		'CLP',
+		'CNY',
+		'COP',
+		'CRC',
+		'CUC',
+		'CUP',
+		'CVE',
+		'CZK',
+		'DJF',
+		'DKK',
+		'DOP',
+		'DZD',
+		'EGP',
+		'ERN',
+		'ETB',
+		'EUR',
+		'FJD',
+		'FKP',
+		'GBP',
+		'GEL',
+		'GHS',
+		'GIP',
+		'GMD',
+		'GNF',
+		'GTQ',
+		'GYD',
+		'HKD',
+		'HNL',
+		'HRK',
+		'HTG',
+		'HUF',
+		'CHF',
+		'IDR',
+		'ILS',
+		'INR',
+		'IQD',
+		'IRR',
+		'ISK',
+		'JMD',
+		'JOD',
+		'JPY',
+		'KES',
+		'KGS',
+		'KHR',
+		'KMF',
+		'KPW',
+		'KRW',
+		'KWD',
+		'KYD',
+		'KZT',
+		'LAK',
+		'LBP',
+		'LKR',
+		'LRD',
+		'LSL',
+		'LTL',
+		'LYD',
+		'MAD',
+		'MDL',
+		'MGA',
+		'MKD',
+		'MMK',
+		'MNT',
+		'MOP',
+		'MRO',
+		'MUR',
+		'MVR',
+		'MWK',
+		'MXN',
+		'MYR',
+		'MZN',
+		'NAD',
+		'NGN',
+		'NIO',
+		'NOK',
+		'NPR',
+		'NZD',
+		'OMR',
+		'PAB',
+		'PEN',
+		'PGK',
+		'PHP',
+		'PKR',
+		'PLN',
+		'PYG',
+		'QAR',
+		'RON',
+		'RSD',
+		'RUB',
+		'RWF',
+		'SAR',
+		'SBD',
+		'SCR',
+		'SDG',
+		'SEK',
+		'SGD',
+		'SHP',
+		'SLL',
+		'SOS',
+		'SRD',
+		'SSP',
+		'STD',
+		'SVC',
+		'SYP',
+		'SZL',
+		'THB',
+		'TJS',
+		'TMT',
+		'TND',
+		'TOP',
+		'TRY',
+		'TTD',
+		'TWD',
+		'TZS',
+		'UAH',
+		'UGX',
+		'USD',
+		'UYU',
+		'UZS',
+		'VEF',
+		'VND',
+		'VUV',
+		'WST',
+		'XAF',
+		'XCD',
+		'XOF',
+		'XPF',
+		'YER',
+		'ZAR',
+		'ZMW',
+		'ZWL',
+	];
 
 	/**
 	 * {@inheritDoc}
 	 */
 	public function get($currencyCode, $locale = null, $fallbackLocale = null) {
+		if (!$this->isSupportedCurrency($currencyCode)) {
+			throw new \SS6\ShopBundle\Model\Localization\Exception\UnsupportedCurrencyException($currencyCode);
+		}
+
 		$intlCurrency = parent::get($currencyCode, $locale, $fallbackLocale);
 
 		return $intlCurrency;
@@ -39,8 +186,24 @@ class IntlCurrencyRepository extends BaseCurrencyRepository {
 	 */
 	public function getAll($locale = null, $fallbackLocale = null) {
 		$intlCurrencies = parent::getAll($locale, $fallbackLocale);
+		/* @var $intlCurrencies \CommerceGuys\Intl\Currency\CurrencyInterface[] */
 
-		return $intlCurrencies;
+		$supportedCurrencies = [];
+		foreach ($intlCurrencies as $intlCurrency) {
+			if ($this->isSupportedCurrency($intlCurrency->getCurrencyCode())) {
+				$supportedCurrencies[] = $intlCurrency;
+			}
+		}
+
+		return $supportedCurrencies;
+	}
+
+	/**
+	 * @param string $currencyCode
+	 * @return boolean
+	 */
+	public function isSupportedCurrency($currencyCode) {
+		return in_array($currencyCode, self::SUPPORTED_CURRENCY_CODES, true);
 	}
 
 }
