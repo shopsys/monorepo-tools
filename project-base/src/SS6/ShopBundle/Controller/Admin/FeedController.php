87d722153c1d39e9fac06b6dc1b5d52125189d60
Petr Kadlec <kadlec@shopsys.cz>
2015-05-11 10:14:30 +0200

    refactoring feed grid data source

--- project-base/src/SS6/ShopBundle/Controller/Admin/FeedController.php
+++ project-base/src/SS6/ShopBundle/Controller/Admin/FeedController.php
@@ -5,6 +5,7 @@ namespace SS6\ShopBundle\Controller\Admin;
 use DateTime;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use SS6\ShopBundle\Controller\Admin\BaseController;
+use SS6\ShopBundle\Model\Domain\Domain;
 use SS6\ShopBundle\Model\Feed\FeedFacade;
 use SS6\ShopBundle\Model\Grid\ArrayDataSource;
 use SS6\ShopBundle\Model\Grid\GridFactory;
@@ -21,9 +22,15 @@ class FeedController extends BaseController {
 	 */
 	private $gridFactory;
 
-	public function __construct(FeedFacade $feedFacade, GridFactory $gridFactory) {
+	/**
+	 * @var \SS6\ShopBundle\Model\Domain\Domain
+	 */
+	private $domain;
+
+	public function __construct(FeedFacade $feedFacade, GridFactory $gridFactory, Domain $domain) {
 		$this->feedFacade = $feedFacade;
 		$this->gridFactory = $gridFactory;
+		$this->domain = $domain;
 	}
 
 	/**
@@ -41,21 +48,16 @@ class FeedController extends BaseController {
 	 * @Route("/feed/list/")
 	 */
 	public function listAction() {
-		$heureka1Filepath = $this->container->getParameter('ss6.feed_dir') . '/heureka_1.xml';
-		$heureka2Filepath = $this->container->getParameter('ss6.feed_dir') . '/heureka_2.xml';
-
-		$feeds = [
-			[
-				'name' => 'Heureka 1',
-				'url' => $this->container->getParameter('ss6.feed_url_prefix') . 'heureka_1.xml',
-				'created' => file_exists($heureka1Filepath) ? new DateTime('@' . filemtime($heureka1Filepath)) : null,
-			],
-			[
-				'name' => 'Heureka 2',
-				'url' => $this->container->getParameter('ss6.feed_url_prefix') . 'heureka_2.xml',
-				'created' => file_exists($heureka2Filepath) ? new DateTime('@' . filemtime($heureka2Filepath)) : null,
-			],
-		];
+		$feeds = [];
+		foreach ($this->domain->getAll() as $domainConfig) {
+			$filename = 'heureka_' . $domainConfig->getId() . '.xml';
+			$heurekaFilepath = $this->container->getParameter('ss6.feed_dir') . '/' . $filename;
+			$feeds[] = [
+				'name' => $domainConfig->getName() . ' - Heureka',
+				'url' => $domainConfig->getUrl() . $this->container->getParameter('ss6.feed_url_prefix') . $filename,
+				'created' => file_exists($heurekaFilepath) ? new DateTime('@' . filemtime($heurekaFilepath)) : null,
+			];
+		}
 
 		$dataSource = new ArrayDataSource($feeds, 'name');
 
