8364792ca3fe46c2b3d88545f22b6c5eb451cc5f
Miroslav Sustek <miroslav.sustek@shopsys.cz>
2015-05-12 13:32:15 +0200

    remove unused exceptions

--- project-base/src/SS6/ShopBundle/Model/Product/BestsellingProduct/Exception/BestsellingProductException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SS6\ShopBundle\Model\Product\BestsellingProduct\Exception;
-
-use SS6\ShopBundle\Model\Product\Exception\ProductException;
-
-interface BestsellingProductException extends ProductException {
-
-}
