1378ef7c3233c7987dab2f01290d0d955c173537
Petr Kadlec <kadlec@shopsys.cz>
2015-05-15 12:31:42 +0200

    Cron command can run only one at same time

--- project-base/src/SS6/ShopBundle/Command/CronCommand.php
+++ project-base/src/SS6/ShopBundle/Command/CronCommand.php
@@ -3,6 +3,7 @@
 namespace SS6\ShopBundle\Command;
 
 use SS6\ShopBundle\Component\Cron\CronFacade;
+use SS6\ShopBundle\Component\Mutex\MutexFactory;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -22,10 +23,22 @@ class CronCommand extends ContainerAwareCommand {
 	protected function execute(InputInterface $input, OutputInterface $output) {
 		$cronFacade = $this->getContainer()->get(CronFacade::class);
 		/* @var $cronFacade \SS6\ShopBundle\Component\Cron\CronFacade */
+		$mutexFactory = $this->getContainer()->get(MutexFactory::class);
+		/* @var $mutexFactory \SS6\ShopBundle\Component\Mutex\MutexFactory */
 
-		$cronFacade->runServicesForTime($this->getActualRoundedTime());
+		$mutex = $mutexFactory->getCronMutex();
+		if ($mutex->acquireLock(0)) {
+			$cronFacade->runServicesForTime($this->getActualRoundedTime());
+			$mutex->releaseLock();
+		} else {
+			throw new \SS6\ShopBundle\Command\Exception\CronCommandException('Cron can run only one at this time');
+		}
+
 	}
 
+	/**
+	 * @return \DateTime
+	 */
 	private function getActualRoundedTime() {
 		$time = new \DateTime(null);
 		$time->modify('-' . $time->format('s') . ' sec');
