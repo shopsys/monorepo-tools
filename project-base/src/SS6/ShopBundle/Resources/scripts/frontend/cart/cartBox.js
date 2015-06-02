3c950ea2379b1cef2d61a839f2e817795e3851bb
Rostislav Vitek <vitek@netdevelo.cz>
2015-06-02 15:38:45 +0200

    cart box reload binding

--- project-base/src/SS6/ShopBundle/Resources/scripts/frontend/cart/cartBox.js
+++ project-base/src/SS6/ShopBundle/Resources/scripts/frontend/cart/cartBox.js
@@ -3,30 +3,26 @@
 	SS6 = window.SS6 || {};
 	SS6.cartBox = SS6.cartBox || {};
 
-	SS6.cartBox.init = function () {
-		$('#cart-box').bind('reload', SS6.cartBox.reload);
+	SS6.cartBox.init = function ($container) {
+		$container.find('#cart-box').bind('reload', SS6.cartBox.reload);
 	};
 
 	SS6.cartBox.reload = function (event) {
-		var self = $(this);
 
 		$.ajax({
-			url: self.data('reload-url'),
+			url: $(this).data('reload-url'),
 			type: 'get',
 			success: function (data) {
 				$('#cart-box').replaceWith(data);
 
-				// TODO: temporal solution, should be fixed in US-537
-				$('#cart-box').bind('reload', SS6.cartBox.reload);
+				SS6.eventBinder.init($('#cart-box').parent());
 			}
 		});
 
 		event.preventDefault();
 	};
 
-	$(document).ready(function () {
-		SS6.cartBox.init();
-	});
+	SS6.eventBinder.registerCallback(SS6.cartBox.init);
 
 })(jQuery);
 
