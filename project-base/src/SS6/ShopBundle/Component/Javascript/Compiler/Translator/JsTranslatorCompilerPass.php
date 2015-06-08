4cf96fa9dbdc2378a603dfb8b353f0037e4a0e0a
Rostislav Vitek <vitek@netdevelo.cz>
2015-06-08 13:58:19 +0200

    custom processing of pluralization string in js translations

--- project-base/src/SS6/ShopBundle/Component/Javascript/Compiler/Translator/JsTranslatorCompilerPass.php
+++ project-base/src/SS6/ShopBundle/Component/Javascript/Compiler/Translator/JsTranslatorCompilerPass.php
@@ -7,6 +7,7 @@ import('PLUG.JavaScript.JNodes.nonterminal.JProgramNode');
 use JProgramNode;
 use SS6\ShopBundle\Component\Javascript\Compiler\JsCompilerPassInterface;
 use SS6\ShopBundle\Component\Javascript\Parser\Translator\JsTranslatorCallParser;
+use SS6\ShopBundle\Component\Javascript\Parser\Translator\JsTranslatorCallParserFactory;
 use SS6\ShopBundle\Component\Translation\Translator;
 
 class JsTranslatorCompilerPass implements JsCompilerPassInterface {
@@ -38,10 +39,58 @@ class JsTranslatorCompilerPass implements JsCompilerPassInterface {
 		foreach ($jsTranslatorsCalls as $jsTranslatorsCall) {
 			$messageIdArgumentNode = $jsTranslatorsCall->getMessageIdArgumentNode();
 
-			$translatedMessage = $this->translator->trans($jsTranslatorsCall->getMessageId());
+			// It is necessary to mark each part of pluralization with two hashes when the message is not translated.
+			// Therefore custom method is used instead of using $this->translaor->trans method for transChoice calls.
+			if ($jsTranslatorsCall->getFunctionName() === JsTranslatorCallParserFactory::METHOD_NAME_TRANS_CHOICE) {
+				$translatedMessage = $this->translate($jsTranslatorsCall);
+			} else {
+				$translatedMessage = $this->translator->trans($jsTranslatorsCall->getMessageId());
+			}
 
 			$messageIdArgumentNode->terminate(json_encode($translatedMessage));
 		}
+	}
+
+	/**
+	 * @param \SS6\ShopBundle\Component\Javascript\Parser\Translator\JsTranslatorCall $jsTranslatorsCall
+	 * @return string
+	 */
+	private function translate($jsTranslatorsCall) {
+		$locale = $this->translator->getLocale();
+		$catalogue = $this->translator->getCatalogue($locale);
+		$messageId = $jsTranslatorsCall->getMessageId();
+		$domain = $jsTranslatorsCall->getDomain();
+
+		if ($catalogue->defines($messageId, $domain)) {
+			return $catalogue->get((string)$messageId, $domain);
+		} elseif ($locale === Translator::SOURCE_LOCALE) {
+			return $messageId;
+		} else {
+			return $this->markAsNotTranslated((string)$messageId);
+		}
+	}
+
+	/**
+	 * @param string $messageId
+	 * @return string
+	 */
+	private function markAsNotTranslated($messageId) {
+		$pluralizationParts = explode('|', $messageId);
+		$markedMessages = [];
+		foreach ($pluralizationParts as $part) {
+			$endBracePosition = strpos($part, '}');
+			$endBracketPosition =  strpos($part, ']');
+			if ($endBracePosition !== false) {
+				$notTranslatedPrefixPosition = $endBracePosition + 1;
+			} elseif ($endBracketPosition !== false) {
+				$notTranslatedPrefixPosition = $endBracketPosition + 1;
+			} else {
+				$notTranslatedPrefixPosition = 0;
+			}
+			$markedMessages[] = substr_replace($part, Translator::NOT_TRANSLATED_PREFIX, $notTranslatedPrefixPosition, 0);
+		}
+
+		return implode('|', $markedMessages);
 	}
 
 }
