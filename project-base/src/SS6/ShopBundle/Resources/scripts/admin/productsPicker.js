62efe8e34e2531d124063fd7e0401bd41b42d671
Rostislav Vitek <vitek@netdevelo.cz>
2015-06-22 11:07:14 +0200

    sortable products picker + light refactoring

--- project-base/src/SS6/ShopBundle/Resources/scripts/admin/productsPicker.js
+++ project-base/src/SS6/ShopBundle/Resources/scripts/admin/productsPicker.js
@@ -24,7 +24,6 @@
 		var $addButton = $productsPicker.find('.js-products-picker-button-add');
 		var $itemsContainer = $productsPicker.find('.js-products-picker-items');
 		var productItems = [];
-		var nextIndex = 0;
 		var mainProductId = $productsPicker.data('products-picker-main-product-id');
 
 		this.init = function () {
@@ -32,11 +31,15 @@
 			$itemsContainer.find('.js-products-picker-item').each(function () {
 				initItem($(this));
 			});
+			$itemsContainer.sortable({
+				handle: '.cursor-move',
+				update: updateOrdering
+			});
 		};
 
 		this.addProduct = function (productId, productName) {
-			var itemHtml = $productsPicker.data('products-picker-prototype').replace(/__name__/g, 'new_' + nextIndex);
-			nextIndex++;
+			var nextIndex = $itemsContainer.find('.js-products-picker-item').size();
+			var itemHtml = $productsPicker.data('products-picker-prototype').replace(/__name__/g, nextIndex);
 			var $item = $($.parseHTML(itemHtml));
 			$item.find('.js-products-picker-item-product-name').text(productName);
 			$item.find('.js-products-picker-item-input').val(productId);
@@ -46,13 +49,7 @@
 		};
 
 		this.hasProduct = function (productId) {
-			for (var key in productItems) {
-				if (productItems[key].find('.js-products-picker-item-input:first').val() === productId.toString()) {
-					return true;
-				}
-			}
-
-			return false;
+			return findProductItemIndex(productId) !== null;
 		};
 
 		this.isMainProduct = function (productId) {
@@ -60,13 +57,10 @@
 		};
 
 		var initItem = function ($item) {
-			var index = productItems.length;
-			productItems[index] = $item;
+			productItems.push($item);
 
 			$item.find('.js-products-picker-item-button-delete').click(function () {
-				delete productItems[index];
-				$item.remove();
-				SS6.formChangeInfo.showInfo();
+				removeItem($item);
 			});
 		};
 
@@ -78,6 +72,37 @@
 			});
 
 			return false;
+		};
+
+		var reIndex = function () {
+			$itemsContainer.find('.js-products-picker-item-input').each(function (index) {
+				var name = $(this).attr("name");
+				var newName = name.substr(0, name.lastIndexOf('[') + 1) + index + ']';
+				$(this).attr("name", newName);
+			});
+		};
+
+		var removeItem = function ($item) {
+			var productId = $item.find('.js-products-picker-item-input:first').val();
+			delete productItems[findProductItemIndex(productId)];
+			$item.remove();
+			reIndex();
+			SS6.formChangeInfo.showInfo();
+		};
+
+		var findProductItemIndex = function (productId) {
+			for (var key in productItems) {
+				if (productItems[key].find('.js-products-picker-item-input:first').val() === productId.toString()) {
+					return key;
+				}
+			}
+
+			return null;
+		};
+
+		var updateOrdering = function () {
+			reIndex();
+			SS6.formChangeInfo.showInfo();
 		};
 	};
 
