9b1503c1a3194a39c72404c79072625e5fd0e5db
Rostislav Vitek <vitek@netdevelo.cz>
2015-06-29 08:52:29 +0200

    refactoring

--- project-base/src/SS6/ShopBundle/Command/GenerateGruntfileCommand.php
+++ project-base/src/SS6/ShopBundle/Command/GenerateGruntfileCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Twig_Environment;
 
-class GenerateGruntCommand extends ContainerAwareCommand {
+class GenerateGruntfileCommand extends ContainerAwareCommand {
 
 	protected function configure() {
 		$this
