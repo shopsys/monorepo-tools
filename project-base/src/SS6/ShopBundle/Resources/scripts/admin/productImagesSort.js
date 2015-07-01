ab278ca3b5ed9e4401825ff8cda69c50b369e909
Petr Kadlec <kadlec@shopsys.cz>
2015-07-01 16:08:23 +0200

    styling

--- project-base/src/SS6/ShopBundle/Resources/scripts/admin/productImagesSort.js
+++ project-base/src/SS6/ShopBundle/Resources/scripts/admin/productImagesSort.js
@@ -5,15 +5,8 @@
 
 	SS6.productImagesSort.init = function () {
 		$('#js-product-images').sortable({
-			handle: '.js-product-images-image-handle',
-			update: SS6.productImagesSort.highlightMainImage
+			handle: '.js-product-images-image-handle'
 		});
-		SS6.productImagesSort.highlightMainImage();
-	};
-
-	SS6.productImagesSort.highlightMainImage = function () {
-		$('#js-product-images .list-image__item--main').removeClass('list-image__item--main');
-		$('#js-product-images .js-product-images-image:first').addClass('list-image__item--main');
 	};
 
 	$(document).ready(function () {
