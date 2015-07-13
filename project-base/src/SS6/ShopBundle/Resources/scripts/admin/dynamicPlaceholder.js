c65c9f6424f250ca7e1f17863565ac5351710488
Miroslav Sustek <miroslav.sustek@shopsys.cz>
2015-07-13 18:21:33 +0200

    admin: article: placeholder for seoTitle

---
+++ project-base/src/SS6/ShopBundle/Resources/scripts/admin/dynamicPlaceholder.js
@@ -0,0 +1,30 @@
+(function ($) {
+
+	SS6 = window.SS6 || {};
+	SS6.dynamicPlaceholder = SS6.dynamicPlaceholder || {};
+
+	SS6.dynamicPlaceholder.DynamicPlaceholder = function ($input) {
+		var self = this;
+		var $sourceInput = $('#' + $input.data('placeholder-source-input-id'));
+
+		this.init = function() {
+			$sourceInput.change(function () {
+				updatePlaceholder();
+			});
+
+			updatePlaceholder();
+		};
+
+		var updatePlaceholder = function () {
+			$input.attr('placeholder', $sourceInput.val());
+		};
+	};
+
+	$(document).ready(function () {
+		$('.js-dynamic-placeholder').each(function () {
+			var dynamicPlaceholder = new SS6.dynamicPlaceholder.DynamicPlaceholder($(this));
+			dynamicPlaceholder.init();
+		});
+	});
+
+})(jQuery);
