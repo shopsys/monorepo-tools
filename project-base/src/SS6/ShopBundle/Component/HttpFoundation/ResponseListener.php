26b4111c8c68af91df45754fa5aef41fea412b76
Rostislav Vitek <vitek@netdevelo.cz>
2015-07-14 14:33:13 +0200

    [BG-298] set X-Frame-Options header to 'sameorigin'

--- project-base/src/SS6/ShopBundle/Component/HttpFoundation/ResponseListener.php
+++ project-base/src/SS6/ShopBundle/Component/HttpFoundation/ResponseListener.php
@@ -10,7 +10,7 @@ class ResponseListener {
 	 * @param \Symfony\Component\HttpKernel\Event\FilterResponseEvent $event
 	 */
 	public function onKernelResponse(FilterResponseEvent $event) {
-		$event->getResponse()->headers->set('X-Frame-Options', 'deny');
+		$event->getResponse()->headers->set('X-Frame-Options', 'sameorigin');
 		$event->getResponse()->headers->set('X-XSS-Protection', '1; mode=block');
 		$event->getResponse()->headers->set('X-Content-Type-Options', 'nosniff');
 	}
