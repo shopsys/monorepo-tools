ff706931a708d9c327cde38ada45e5d3cd59dfb8
Rostislav Vitek <vitek@netdevelo.cz>
2015-07-15 14:35:44 +0200

    change route admin_overview_list to admin_domain_list

--- project-base/src/SS6/ShopBundle/Controller/Admin/DomainController.php
+++ project-base/src/SS6/ShopBundle/Controller/Admin/DomainController.php
@@ -5,6 +5,8 @@ namespace SS6\ShopBundle\Controller\Admin;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use SS6\ShopBundle\Model\Domain\Domain;
 use SS6\ShopBundle\Model\Domain\SelectedDomain;
+use SS6\ShopBundle\Model\Grid\ArrayDataSource;
+use SS6\ShopBundle\Model\Grid\GridFactory;
 use SS6\ShopBundle\Model\Localization\Localization;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,14 +28,21 @@ class DomainController extends Controller {
 	 */
 	private $localization;
 
+	/**
+	 * @var \SS6\ShopBundle\Model\Grid\GridFactory
+	 */
+	private $gridFactory;
+
 	public function __construct(
 		Domain $domain,
 		SelectedDomain $selectedDomain,
-		Localization $localization
+		Localization $localization,
+		GridFactory $gridFactory
 	) {
 		$this->domain = $domain;
 		$this->selectedDomain = $selectedDomain;
 		$this->localization = $localization;
+		$this->gridFactory = $gridFactory;
 	}
 
 	public function domainTabsAction() {
@@ -59,6 +68,37 @@ class DomainController extends Controller {
 		} else {
 			return $this->redirect($referer);
 		}
+	}
+
+	/**
+	 * @Route("/domain/list")
+	 */
+	public function listAction() {
+		$dataSource = new ArrayDataSource($this->loadData(), 'id');
+
+		$grid = $this->gridFactory->create('domainsList', $dataSource);
+
+		$grid->addColumn('name', 'name', 'Název domény');
+		$grid->addColumn('locale', 'locale', 'Jazyk');
+
+		$grid->setTheme('@SS6Shop/Admin/Content/Domain/listGrid.html.twig');
+
+		return $this->render('@SS6Shop/Admin/Content/Domain/list.html.twig', [
+			'gridView' => $grid->createView(),
+		]);
+	}
+
+	private function loadData() {
+		$data = [];
+		foreach ($this->domain->getAll() as $domainConfig) {
+			$data[] = [
+				'id' => $domainConfig->getId(),
+				'name' => $domainConfig->getName(),
+				'locale' => $domainConfig->getLocale(),
+			];
+		}
+
+		return $data;
 	}
 
 }
