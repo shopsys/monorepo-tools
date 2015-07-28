eb33ad750ddcca2536ce5ad4261b56f6d5d07273
Petr Kadlec <kadlec@shopsys.cz>
2015-07-28 14:57:25 +0200

    [US-670] Security: logout only from current context (firewall) -> session cannot by invalidate, must be migrate

--- project-base/src/SS6/ShopBundle/Model/Security/AdminLogoutHandler.php
+++ project-base/src/SS6/ShopBundle/Model/Security/AdminLogoutHandler.php
@@ -2,18 +2,12 @@
 
 namespace SS6\ShopBundle\Model\Security;
 
-use SS6\ShopBundle\Model\Order\OrderFlowFacade;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Router;
 use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
 
-class LogoutHandler implements LogoutSuccessHandlerInterface {
-
-	/**
-	 * @var \SS6\ShopBundle\Model\Order\OrderFlowFacade
-	 */
-	private $orderFlowFacade;
+class AdminLogoutHandler implements LogoutSuccessHandlerInterface {
 
 	/**
 	 * @var \Symfony\Component\Routing\Router
@@ -22,11 +16,9 @@ class LogoutHandler implements LogoutSuccessHandlerInterface {
 
 	/**
 	 * @param \Symfony\Component\Routing\Router $router
-	 * @param \SS6\ShopBundle\Model\Order\OrderFlowFacade $orderFlowFacade
 	 */
-	public function __construct(Router $router, OrderFlowFacade $orderFlowFacade) {
+	public function __construct(Router $router) {
 		$this->router = $router;
-		$this->orderFlowFacade = $orderFlowFacade;
 	}
 
 	/**
@@ -34,8 +26,8 @@ class LogoutHandler implements LogoutSuccessHandlerInterface {
 	 * @return \Symfony\Component\HttpFoundation\RedirectResponse
 	 */
 	public function onLogoutSuccess(Request $request) {
-		$this->orderFlowFacade->resetOrderForm();
-		$url = $this->router->generate('front_homepage');
+		$url = $this->router->generate('admin_login');
+		$request->getSession()->migrate();
 
 		return new RedirectResponse($url);
 	}
