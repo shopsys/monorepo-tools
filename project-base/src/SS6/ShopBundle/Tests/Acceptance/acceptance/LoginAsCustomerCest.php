8c0c8a81aff2ccfa9ae9c7dce7daa2111ca8ce1d
Miroslav Sustek <miroslav.sustek@shopsys.cz>
2015-08-07 14:30:06 +0200

    tests: acceptance: fix testLoginAsCustomer

--- project-base/src/SS6/ShopBundle/Tests/Acceptance/acceptance/LoginAsCustomerCest.php
+++ project-base/src/SS6/ShopBundle/Tests/Acceptance/acceptance/LoginAsCustomerCest.php
@@ -7,14 +7,13 @@ use SS6\ShopBundle\Tests\Test\Codeception\AcceptanceTester;
 class LoginAsCustomerCest {
 
 	public function testLoginAsCustomer(AcceptanceTester $me) {
-		$me->wantTo('login on admin');
+		$me->wantTo('login as a customer from admin');
 		$me->amOnPage('/admin/');
 		$me->see('Administrace');
 		$me->fillField('admin_login_form[username]', 'admin');
 		$me->fillField('admin_login_form[password]', 'admin123');
 		$me->click('Přihlásit');
-		$me->click('Zákazníci');
-		$me->click('Anpilogov Igor');
+		$me->amOnPage('/admin/customer/edit/3');
 		$me->click('Přihlásit za uživatele');
 		$me->switchToLastOpenedWindow();
 		$me->see('Internetový obchod');
