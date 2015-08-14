953988bf7c734a264059ad449d734d3620d3b884
Rostislav Vitek <vitek@netdevelo.cz>
2015-08-14 12:02:00 +0200

    translations in grid.dragAndDrop

--- project-base/src/SS6/ShopBundle/Resources/scripts/admin/components/grid.dragAndDrop.js
+++ project-base/src/SS6/ShopBundle/Resources/scripts/admin/components/grid.dragAndDrop.js
@@ -62,10 +62,10 @@
 				$grid.data('positionsChanged', false);
 				SS6.grid.dragAndDrop.highlightChanges($grid, false);
 
-				SS6.window({content: 'Pořadí bylo uloženo'});
+				SS6.window({content: SS6.translator.trans('Pořadí bylo uloženo')});
 			},
 			error: function () {
-				SS6.window({content: 'Pořadí se nepodařilo uložit'});
+				SS6.window({content: SS6.translator.trans('Pořadí se nepodařilo uložit')});
 			}
 		});
 		$grid.trigger('save');
