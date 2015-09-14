cec2532ff957f2de1aebdbdc7e1745272bce4b78
Miroslav Sustek <miroslav.sustek@shopsys.cz>
2015-09-14 13:38:07 +0200

    sitemap: add homepage sitemap

--- project-base/src/SS6/ShopBundle/Component/Sitemap/SitemapListener.php
+++ project-base/src/SS6/ShopBundle/Component/Sitemap/SitemapListener.php
@@ -6,10 +6,12 @@ use Presta\SitemapBundle\Event\SitemapPopulateEvent;
 use Presta\SitemapBundle\Service\AbstractGenerator;
 use Presta\SitemapBundle\Service\SitemapListenerInterface;
 use Presta\SitemapBundle\Sitemap\Url\UrlConcrete;
+use SS6\ShopBundle\Component\Router\DomainRouterFactory;
 use SS6\ShopBundle\Component\Router\FriendlyUrl\FriendlyUrlService;
 use SS6\ShopBundle\Component\Sitemap\SitemapFacade;
 use SS6\ShopBundle\Model\Domain\Config\DomainConfig;
 use SS6\ShopBundle\Model\Domain\Domain;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class SitemapListener implements SitemapListenerInterface {
 
@@ -28,14 +30,21 @@ class SitemapListener implements SitemapListenerInterface {
 	 */
 	private $friendlyUrlService;
 
+	/**
+	 * @var \SS6\ShopBundle\Component\Router\DomainRouterFactory
+	 */
+	private $domainRouterFactory;
+
 	public function __construct(
 		SitemapFacade $sitemapFacade,
 		Domain $domain,
-		FriendlyUrlService $friendlyUrlService
+		FriendlyUrlService $friendlyUrlService,
+		DomainRouterFactory $domainRouterFactory
 	) {
 		$this->sitemapFacade = $sitemapFacade;
 		$this->domain = $domain;
 		$this->friendlyUrlService = $friendlyUrlService;
+		$this->domainRouterFactory = $domainRouterFactory;
 	}
 
 	/**
@@ -47,6 +56,8 @@ class SitemapListener implements SitemapListenerInterface {
 
 		$generator = $event->getGenerator();
 		$domainConfig = $this->domain->getDomainConfigById($domainId);
+
+		$this->addHomepageUrl($generator, $domainConfig, $section);
 
 		$productSitemapItems = $this->sitemapFacade->getSitemapItemsForVisibleProducts($domainConfig);
 		$this->addUrlsBySitemapItems($productSitemapItems, $generator, $domainConfig, $section);
@@ -75,6 +86,22 @@ class SitemapListener implements SitemapListenerInterface {
 			$urlConcrete = new UrlConcrete($absoluteUrl);
 			$generator->addUrl($urlConcrete, $section);
 		}
+	}
+
+	/**
+	 * @param \Presta\SitemapBundle\Service\AbstractGenerator $generator
+	 * @param \SS6\ShopBundle\Model\Domain\Config\DomainConfig $domainConfig
+	 * @param string $section
+	 */
+	private function addHomepageUrl(
+		AbstractGenerator $generator,
+		DomainConfig $domainConfig,
+		$section
+	) {
+		$domainRouter = $this->domainRouterFactory->getRouter($domainConfig->getId());
+		$absoluteUrl = $domainRouter->generate('front_homepage', [], UrlGeneratorInterface::ABSOLUTE_URL);
+		$urlConcrete = new UrlConcrete($absoluteUrl);
+		$generator->addUrl($urlConcrete, $section);
 	}
 
 }
