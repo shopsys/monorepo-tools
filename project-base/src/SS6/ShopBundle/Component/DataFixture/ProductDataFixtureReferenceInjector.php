d2757b2ab270c92490437288e89ab3c26b04c9e7
Pavel Jurecka <paveljurecka42@gmail.com>
2015-09-30 13:31:07 +0200

    New brands in demo data CSV, BrandDataFixture and ProductDataFixtureReferenceInjector.

--- project-base/src/SS6/ShopBundle/Component/DataFixture/ProductDataFixtureReferenceInjector.php
+++ project-base/src/SS6/ShopBundle/Component/DataFixture/ProductDataFixtureReferenceInjector.php
@@ -24,18 +24,56 @@ class ProductDataFixtureReferenceInjector {
 		ProductDataFixtureLoader $productDataFixtureLoader,
 		ReferenceRepository $referenceRepository
 	) {
+		$vats = $this->getVatReferences($referenceRepository);
+		$availabilities = $this->getAvailabilityReferences($referenceRepository);
+		$categories = $this->getCategoryReferences($referenceRepository);
+		$flags = $this->getFlagReferences($referenceRepository);
+		$brands = $this->getBrandReferences($referenceRepository);
+		$units = $this->getUnitReferences($referenceRepository);
+
+		$productDataFixtureLoader->injectReferences(
+			$vats,
+			$availabilities,
+			$categories,
+			$flags,
+			$brands,
+			$units
+		);
+	}
+
+	/**
+	 * @param \Doctrine\Common\DataFixtures\ReferenceRepository $referenceRepository
+	 * @return string[]
+	 */
+	private function getVatReferences(ReferenceRepository $referenceRepository) {
 		$vats = [
 			'high' => $referenceRepository->getReference(VatDataFixture::VAT_HIGH),
 			'low' => $referenceRepository->getReference(VatDataFixture::VAT_LOW),
 			'zero' => $referenceRepository->getReference(VatDataFixture::VAT_ZERO),
 		];
 
+		return $vats;
+	}
+
+	/**
+	 * @param \Doctrine\Common\DataFixtures\ReferenceRepository $referenceRepository
+	 * @return string[]
+	 */
+	private function getAvailabilityReferences(ReferenceRepository $referenceRepository) {
 		$availabilities = [
 			'in-stock' => $referenceRepository->getReference(AvailabilityDataFixture::IN_STOCK),
 			'out-of-stock' => $referenceRepository->getReference(AvailabilityDataFixture::OUT_OF_STOCK),
 			'on-request' => $referenceRepository->getReference(AvailabilityDataFixture::ON_REQUEST),
 		];
 
+		return $availabilities;
+	}
+
+	/**
+	 * @param \Doctrine\Common\DataFixtures\ReferenceRepository $referenceRepository
+	 * @return string[]
+	 */
+	private function getCategoryReferences(ReferenceRepository $referenceRepository) {
 		$categories = [
 			CategoryDataFixture::TV => $referenceRepository->getReference(
 				CategoryDataFixture::PREFIX . CategoryDataFixture::TV
@@ -63,32 +101,67 @@ class ProductDataFixtureReferenceInjector {
 			),
 		];
 
+		return $categories;
+	}
+
+	/**
+	 * @param \Doctrine\Common\DataFixtures\ReferenceRepository $referenceRepository
+	 * @return string[]
+	 */
+	private function getFlagReferences(ReferenceRepository $referenceRepository) {
 		$flags = [
 			'action' => $referenceRepository->getReference(FlagDataFixture::ACTION_PRODUCT),
 			'new' => $referenceRepository->getReference(FlagDataFixture::NEW_PRODUCT),
 			'top' => $referenceRepository->getReference(FlagDataFixture::TOP_PRODUCT),
 		];
 
+		return $flags;
+	}
+
+	/**
+	 * @param \Doctrine\Common\DataFixtures\ReferenceRepository $referenceRepository
+	 * @return string[]
+	 */
+	private function getBrandReferences(ReferenceRepository $referenceRepository) {
 		$brands = [
 			'apple' => $referenceRepository->getReference(BrandDataFixture::APPLE),
 			'canon' => $referenceRepository->getReference(BrandDataFixture::CANON),
 			'lg' => $referenceRepository->getReference(BrandDataFixture::LG),
 			'philips' => $referenceRepository->getReference(BrandDataFixture::PHILIPS),
+			'sencor' => $referenceRepository->getReference(BrandDataFixture::SENCOR),
+			'a4tech' => $referenceRepository->getReference(BrandDataFixture::A4TECH),
+			'brother' => $referenceRepository->getReference(BrandDataFixture::BROTHER),
+			'verbatim' => $referenceRepository->getReference(BrandDataFixture::VERBATIM),
+			'dlink' => $referenceRepository->getReference(BrandDataFixture::DLINK),
+			'defender' => $referenceRepository->getReference(BrandDataFixture::DEFENDER),
+			'delonghi' => $referenceRepository->getReference(BrandDataFixture::DELONGHI),
+			'genius' => $referenceRepository->getReference(BrandDataFixture::GENIUS),
+			'gigabyte' => $referenceRepository->getReference(BrandDataFixture::GIGABYTE),
+			'hp' => $referenceRepository->getReference(BrandDataFixture::HP),
+			'htc' => $referenceRepository->getReference(BrandDataFixture::HTC),
+			'jura' => $referenceRepository->getReference(BrandDataFixture::JURA),
+			'logitech' => $referenceRepository->getReference(BrandDataFixture::LOGITECH),
+			'microsoft' => $referenceRepository->getReference(BrandDataFixture::MICROSOFT),
+			'samsung' => $referenceRepository->getReference(BrandDataFixture::SAMSUNG),
+			'sony' => $referenceRepository->getReference(BrandDataFixture::SONY),
+			'orava' => $referenceRepository->getReference(BrandDataFixture::ORAVA),
+			'olympus' => $referenceRepository->getReference(BrandDataFixture::OLYMPUS),
 		];
 
+		return $brands;
+	}
+
+	/**
+	 * @param \Doctrine\Common\DataFixtures\ReferenceRepository $referenceRepository
+	 * @return string[]
+	 */
+	private function getUnitReferences(ReferenceRepository $referenceRepository) {
 		$units = [
 			'pcs' => $referenceRepository->getReference(BaseUnitDataFixture::PCS),
 			'm3' => $referenceRepository->getReference(DemoUnitDataFixture::M3),
 		];
-
-		$productDataFixtureLoader->injectReferences(
-			$vats,
-			$availabilities,
-			$categories,
-			$flags,
-			$brands,
-			$units
-		);
+		
+		return $units;
 	}
 
 	/**
@@ -105,5 +178,5 @@ class ProductDataFixtureReferenceInjector {
 			DemoUnitDataFixture::class,
 		];
 	}
-
+	
 }
--- project-base/src/SS6/ShopBundle/DataFixtures/Demo/BrandDataFixture.php
+++ project-base/src/SS6/ShopBundle/DataFixtures/Demo/BrandDataFixture.php
@@ -13,12 +13,31 @@ class BrandDataFixture extends AbstractReferenceFixture {
 	const CANON = 'brand_canon';
 	const LG = 'brand_lg';
 	const PHILIPS = 'brand_philips';
+	const SENCOR = 'brand_sencor';
+	const A4TECH = 'brand_a4tech';
+	const BROTHER = 'brand_brother';
+	const VERBATIM = 'brand_verbatim';
+	const DLINK = 'brand_dlink';
+	const DEFENDER = 'brand_defender';
+	const DELONGHI = 'brand_delonghi';
+	const GENIUS = 'brand_genius';
+	const GIGABYTE = 'brand_gigabyte';
+	const HP = 'brand_hp';
+	const HTC = 'brand_htc';
+	const JURA = 'brand_jura';
+	const LOGITECH = 'brand_logitech';
+	const MICROSOFT = 'brand_microsoft';
+	const SAMSUNG = 'brand_samsung';
+	const SONY = 'brand_sony';
+	const ORAVA = 'brand_orava';
+	const OLYMPUS = 'brand_olympus';
 
 	/**
 	 * @param \Doctrine\Common\Persistence\ObjectManager $manager
 	 */
 	public function load(ObjectManager $manager) {
 		$brandData = new BrandData();
+
 		$brandData->name = 'Apple';
 		$this->createBrand(self::APPLE, $brandData);
 
@@ -30,6 +49,60 @@ class BrandDataFixture extends AbstractReferenceFixture {
 
 		$brandData->name = 'Philips';
 		$this->createBrand(self::PHILIPS, $brandData);
+
+		$brandData->name = 'Sencor';
+		$this->createBrand(self::SENCOR, $brandData);
+
+		$brandData->name = 'A4tech';
+		$this->createBrand(self::A4TECH, $brandData);
+
+		$brandData->name = 'Brother';
+		$this->createBrand(self::BROTHER, $brandData);
+
+		$brandData->name = 'Verbatim';
+		$this->createBrand(self::VERBATIM, $brandData);
+
+		$brandData->name = 'Dlink';
+		$this->createBrand(self::DLINK, $brandData);
+
+		$brandData->name = 'Defender';
+		$this->createBrand(self::DEFENDER, $brandData);
+
+		$brandData->name = 'DeLonghi';
+		$this->createBrand(self::DELONGHI, $brandData);
+
+		$brandData->name = 'Genius';
+		$this->createBrand(self::GENIUS, $brandData);
+
+		$brandData->name = 'Gigabyte';
+		$this->createBrand(self::GIGABYTE, $brandData);
+
+		$brandData->name = 'HP';
+		$this->createBrand(self::HP, $brandData);
+
+		$brandData->name = 'HTC';
+		$this->createBrand(self::HTC, $brandData);
+
+		$brandData->name = 'JURA';
+		$this->createBrand(self::JURA, $brandData);
+
+		$brandData->name = 'Logitech';
+		$this->createBrand(self::LOGITECH, $brandData);
+
+		$brandData->name = 'Microsoft';
+		$this->createBrand(self::MICROSOFT, $brandData);
+
+		$brandData->name = 'Samsung';
+		$this->createBrand(self::SAMSUNG, $brandData);
+
+		$brandData->name = 'SONY';
+		$this->createBrand(self::SONY, $brandData);
+
+		$brandData->name = 'Orava';
+		$this->createBrand(self::ORAVA, $brandData);
+
+		$brandData->name = 'Olympus';
+		$this->createBrand(self::OLYMPUS, $brandData);
 	}
 
 	/**
