508f5789bce86572f5ee9f59fa257bf6b2078827
Miroslav Sustek <miroslav.sustek@shopsys.cz>
2015-11-03 10:49:15 +0100

    order: more tests for remembering form data

--- project-base/src/SS6/ShopBundle/Tests/Acceptance/acceptance/OrderCest.php
+++ project-base/src/SS6/ShopBundle/Tests/Acceptance/acceptance/OrderCest.php
@@ -6,7 +6,7 @@ use SS6\ShopBundle\Tests\Test\Codeception\AcceptanceTester;
 
 class OrderCest {
 
-	public function testFormRemembersPaymentAndTransport(AcceptanceTester $me) {
+	public function testFormRemembersPaymentAndTransportWhenClickingBack(AcceptanceTester $me) {
 		$me->wantTo('have my payment and transport remebered by order');
 
 		$me->amOnPage('/televize-audio/');
@@ -26,6 +26,25 @@ class OrderCest {
 		$me->seeCheckboxIsChecked('input[name="transportAndPayment_form[payment]"][value="2"]');
 	}
 
+	public function testFormRemembersPaymentAndTransportWhenGoingDirectlyToUrl(AcceptanceTester $me) {
+		$me->wantTo('have my payment and transport remebered by order');
+
+		$me->amOnPage('/televize-audio/');
+		$me->click('Vložit do košíku');
+		$me->waitForAjax();
+		$me->click('Přejít do košíku');
+		$me->click('Objednat');
+
+		$me->dontSeeCheckboxIsChecked('input[name="transportAndPayment_form[transport]"][value="1"]');
+		$me->checkOption('input[name="transportAndPayment_form[transport]"][value="1"]');
+		$me->dontSeeCheckboxIsChecked('input[name="transportAndPayment_form[payment]"][value="2"]');
+		$me->checkOption('input[name="transportAndPayment_form[payment]"][value="2"]');
+		$me->amOnPage('/objednavka/');
+
+		$me->seeCheckboxIsChecked('input[name="transportAndPayment_form[transport]"][value="1"]');
+		$me->seeCheckboxIsChecked('input[name="transportAndPayment_form[payment]"][value="2"]');
+	}
+
 	public function testFormRemembersFirstName(AcceptanceTester $me) {
 		$me->wantTo('have my first name remebered by order');
 
@@ -40,6 +59,7 @@ class OrderCest {
 
 		$me->fillField('input[name="orderPersonalInfo_form[firstName]"]', 'Jan');
 		$me->click('Zpět na výběr dopravy a platby');
+		$me->amOnPage('/objednavka/');
 		$me->click('Pokračovat v objednávce');
 
 		$me->seeInField('input[name="orderPersonalInfo_form[firstName]"]', 'Jan');
