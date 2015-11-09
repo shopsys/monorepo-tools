8988190167991abbf0aff4a4bea405ad53da6cd9
Rostislav Vitek <vitek@netdevelo.cz>
2015-11-09 18:27:57 +0100

    [US-691] Browser alert when admin is leaving page with unsaved form

--- project-base/src/SS6/ShopBundle/Resources/scripts/admin/formChangeInfo.js
+++ project-base/src/SS6/ShopBundle/Resources/scripts/admin/formChangeInfo.js
@@ -3,6 +3,9 @@
 	SS6 = window.SS6 || {};
 	SS6.formChangeInfo = SS6.formChangeInfo || {};
 
+	var isFormSubmitted = false;
+	var isInfoShown = false;
+
 	SS6.formChangeInfo.init = function () {
 		$('.web__content form')
 			.change(function(){
@@ -14,6 +17,13 @@
 				}
 			});
 
+		$(document).on('submit', '.web__content form', function (event) {
+			if (event.isDefaultPrevented() === false) {
+				isFormSubmitted = true;
+			}
+		});
+
+
 		if (typeof CKEDITOR !== 'undefined') {
 			for (var i in CKEDITOR.instances) {
 				CKEDITOR.instances[i].on('change', function () {
@@ -21,13 +31,19 @@
 				});
 			}
 		}
+
+		$(window).on('beforeunload', function() {
+			if (isInfoShown && !isFormSubmitted) {
+				return SS6.translator.trans('Máte neuložené změny!');
+			}
+		});
 	};
 
 	SS6.formChangeInfo.showInfo = function () {
 		var textToShow = SS6.translator.trans('Provedli jste změny, nezapomeňte je uložit!');
 		var $fixedBarIn = $('.web__content .window-fixed-bar .window-fixed-bar__in');
 		var $infoDiv = $fixedBarIn.find('#js-form-change-info');
-		if ($infoDiv.length === 0) {
+		if (!isInfoShown) {
 			$fixedBarIn.prepend(
 				'<div class="window-fixed-bar__item">\
 					<div id="js-form-change-info" class="window-fixed-bar__item__cell text-center">\
@@ -37,10 +53,14 @@
 		} else {
 			$infoDiv.text = textToShow;
 		}
+		if ($fixedBarIn.size() > 0) {
+			isInfoShown = true;
+		}
 	};
 
 	SS6.formChangeInfo.removeInfo = function () {
 		$('#js-form-change-info').remove();
+		isInfoShown = false;
 	};
 
 	$(document).ready(function () {
