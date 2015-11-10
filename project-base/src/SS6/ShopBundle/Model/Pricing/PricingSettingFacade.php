14f4d61a059849c1264c2c694b5cd72f9968487a
Rostislav Vitek <vitek@netdevelo.cz>
2015-11-10 09:27:39 +0100

    remove useless PricingSettingFacade::setRoundingType()

--- project-base/src/SS6/ShopBundle/Model/Pricing/PricingSettingFacade.php
+++ project-base/src/SS6/ShopBundle/Model/Pricing/PricingSettingFacade.php
@@ -5,24 +5,16 @@ namespace SS6\ShopBundle\Model\Pricing;
 class PricingSettingFacade {
 
 	/**
-	 * @var \SS6\ShopBundle\Model\Pricing\PricingSetting
-	 */
-	private $pricingSetting;
-
-	/**
 	 * @var \SS6\ShopBundle\Model\Pricing\DelayedPricingSetting
 	 */
 	private $delayedPricingSetting;
 
 	/**
-	 * @param \SS6\ShopBundle\Model\Pricing\PricingSetting $pricingSetting
 	 * @param \SS6\ShopBundle\Model\Pricing\DelayedPricingSetting $delayedPricingSetting
 	 */
 	public function __construct(
-		PricingSetting $pricingSetting,
 		DelayedPricingSetting $delayedPricingSetting
 	) {
-		$this->pricingSetting = $pricingSetting;
 		$this->delayedPricingSetting = $delayedPricingSetting;
 	}
 
@@ -31,13 +23,6 @@ class PricingSettingFacade {
 	 */
 	public function setInputPriceType($inputPriceType) {
 		$this->delayedPricingSetting->setInputPriceType($inputPriceType);
-	}
-
-	/**
-	 * @param int $roundingType
-	 */
-	public function setRoundingType($roundingType) {
-		$this->pricingSetting->setRoundingType($roundingType);
 	}
 
 }
