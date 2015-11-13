5c045c5eedac81e1e4fc15710612f5a01fc33b1f
Pavel Jurecka <jurecka@shopsys.cz>
2015-11-13 15:33:17 +0100

    ZboziItemFactory:: createItems(), getProductCategoryText()

--- project-base/src/SS6/ShopBundle/Model/Feed/Zbozi/ZboziItemFactory.php
+++ project-base/src/SS6/ShopBundle/Model/Feed/Zbozi/ZboziItemFactory.php
@@ -2,12 +2,14 @@
 
 namespace SS6\ShopBundle\Model\Feed\Zbozi;
 
+use SS6\ShopBundle\Component\Domain\Config\DomainConfig;
 use SS6\ShopBundle\Model\Category\CategoryFacade;
 use SS6\ShopBundle\Model\Feed\FeedItemFactoryInterface;
 use SS6\ShopBundle\Model\Product\Collection\ProductCollectionFacade;
 use SS6\ShopBundle\Model\Product\Pricing\ProductPriceCalculationForUser;
+use SS6\ShopBundle\Model\Product\Product;
 
-abstract class ZboziItemFactory implements FeedItemFactoryInterface {
+class ZboziItemFactory implements FeedItemFactoryInterface {
 
 	/**
 	 * @var \SS6\ShopBundle\Model\Product\Pricing\ProductPriceCalculationForUser
@@ -37,6 +39,78 @@ abstract class ZboziItemFactory implements FeedItemFactoryInterface {
 		$this->productPriceCalculationForUser = $productPriceCalculationForUser;
 		$this->productCollectionFacade = $productCollectionFacade;
 		$this->categoryFacade = $categoryFacade;
+	}
+
+	/**
+	 * @param \SS6\ShopBundle\Model\Product\Product[] $products
+	 * @param \SS6\ShopBundle\Component\Domain\Config\DomainConfig $domainConfig
+	 * @return \SS6\ShopBundle\Model\Feed\Heureka\HeurekaItem[]
+	 */
+	public function createItems(array $products, DomainConfig $domainConfig) {
+		$productDomainsByProductId = $this->productCollectionFacade->getProductDomainsIndexedByProductId(
+			$products,
+			$domainConfig
+		);
+		$imagesByProductId = $this->productCollectionFacade->getImagesUrlsIndexedByProductId($products, $domainConfig);
+		$urlsByProductId = $this->productCollectionFacade->getAbsoluteUrlsIndexedByProductId($products, $domainConfig);
+		$paramsByProductId = $this->productCollectionFacade->getProductParameterValuesIndexedByProductIdAndParameterName(
+			$products,
+			$domainConfig
+		);
+
+		$items = [];
+		foreach ($products as $product) {
+			$productPrice = $this->productPriceCalculationForUser->calculatePriceForUserAndDomainId(
+				$product,
+				$domainConfig->getId(),
+				null
+			);
+			$manufacturer = null;
+			if ($product->getBrand() !== null) {
+				$manufacturer = $product->getBrand()->getName();
+			}
+			if (array_key_exists($product->getId(), $paramsByProductId)) {
+				$params = $paramsByProductId[$product->getId()];
+			} else {
+				$params = [];
+			}
+			if ($product->getCalculatedAvailability()->getDispatchTime() === null) {
+				$deliveryDate = -1;
+			} else {
+				$deliveryDate = $product->getCalculatedAvailability()->getDispatchTime();
+			}
+
+			$items[] = new ZboziItem(
+				$product->getId(),
+				$product->getName($domainConfig->getLocale()),
+				$productDomainsByProductId[$product->getId()]->getDescription(),
+				$urlsByProductId[$product->getId()],
+				$imagesByProductId[$product->getId()],
+				$productPrice->getPriceWithVat(),
+				$product->getEan(),
+				$deliveryDate,
+				$manufacturer,
+				$this->getProductCategoryText($product, $domainConfig),
+				$params,
+				$product->getPartno()
+			);
+		}
+
+		return $items;
+	}
+
+	/**
+	 * @param \SS6\ShopBundle\Model\Product\Product $product
+	 * @param \SS6\ShopBundle\Component\Domain\Config\DomainConfig $domainConfig
+	 * @return string|null
+	 */
+	private function getProductCategoryText(Product $product, DomainConfig $domainConfig) {
+		$pathFromRootCategoryToMainCategory = $this->categoryFacade->getCategoryNamesInPathFromRootToProductMainCategoryOnDomain(
+			$product,
+			$domainConfig
+		);
+
+		return implode(' | ', $pathFromRootCategoryToMainCategory);
 	}
 
 }
