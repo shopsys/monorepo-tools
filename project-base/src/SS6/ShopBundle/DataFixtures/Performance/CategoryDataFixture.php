72c59c672c7d9b81aec1654bcab569d421df7403
Rostislav Vitek <vitek@netdevelo.cz>
2015-11-26 14:10:35 +0100

    performance data fixtures are regular services with autowired dependencies

--- project-base/src/SS6/ShopBundle/DataFixtures/Performance/CategoryDataFixture.php
+++ project-base/src/SS6/ShopBundle/DataFixtures/Performance/CategoryDataFixture.php
@@ -2,18 +2,29 @@
 
 namespace SS6\ShopBundle\DataFixtures\Performance;
 
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory as FakerFactory;
-use SS6\ShopBundle\Component\DataFixture\AbstractReferenceFixture;
 use SS6\ShopBundle\Component\Doctrine\SqlLoggerFacade;
-use SS6\ShopBundle\DataFixtures\Base\PricingGroupDataFixture;
 use SS6\ShopBundle\Model\Category\Category;
 use SS6\ShopBundle\Model\Category\CategoryData;
 use SS6\ShopBundle\Model\Category\CategoryFacade;
 use SS6\ShopBundle\Model\Category\CategoryRepository;
 
-class CategoryDataFixture extends AbstractReferenceFixture implements DependentFixtureInterface {
+class CategoryDataFixture {
+
+	/**
+	 * @var \SS6\ShopBundle\Model\Category\CategoryRepository
+	 */
+	private $categoryRepository;
+
+	/**
+	 * @var \SS6\ShopBundle\Model\Category\CategoryFacade
+	 */
+	private $categoryFacade;
+
+	/**
+	 * @var \SS6\ShopBundle\Component\Doctrine\SqlLoggerFacade
+	 */
+	private $sqlLoggerFacade;
 
 	/**
 	 * @var \Faker\Generator
@@ -30,25 +41,24 @@ class CategoryDataFixture extends AbstractReferenceFixture implements DependentF
 	 */
 	private $categoriesCreated;
 
-	public function __construct() {
+	public function __construct(
+		CategoryRepository $categoryRepository,
+		SqlLoggerFacade $sqlLoggerFacade,
+		CategoryFacade $categoryFacade
+	) {
+		$this->categoryRepository = $categoryRepository;
+		$this->categoryFacade = $categoryFacade;
+		$this->sqlLoggerFacade = $sqlLoggerFacade;
 		$this->faker = FakerFactory::create();
 		$this->categoriesCountsByLevel = [2, 4, 6];
 		$this->categoriesCreated = 0;
 	}
 
-	/**
-	 * @param \Doctrine\Common\Persistence\ObjectManager $objectManager
-	 */
-	public function load(ObjectManager $objectManager) {
-		$categoryRepository = $this->get(CategoryRepository::class);
-		/* @var $categoryRepository \SS6\ShopBundle\Model\Category\CategoryRepository */
-		$sqlLoggerFacade = $this->get(SqlLoggerFacade::class);
-		/* @var $sqlLoggerFacade \SS6\ShopBundle\Component\Doctrine\SqlLoggerFacade */
-		$rootCategory = $categoryRepository->getRootCategory();
-
-		$sqlLoggerFacade->temporarilyDisableLogging();
+	public function load() {
+		$rootCategory = $this->categoryRepository->getRootCategory();
+		$this->sqlLoggerFacade->temporarilyDisableLogging();
 		$this->recursivelyCreateCategoryTree($rootCategory);
-		$sqlLoggerFacade->reenableLogging();
+		$this->sqlLoggerFacade->reenableLogging();
 	}
 
 	/**
@@ -56,11 +66,9 @@ class CategoryDataFixture extends AbstractReferenceFixture implements DependentF
 	 * @param int $categoryLevel
 	 */
 	private function recursivelyCreateCategoryTree($parentCategory, $categoryLevel = 0) {
-		$categoryFacade = $this->get(CategoryFacade::class);
-		/* @var $categoryFacade \SS6\ShopBundle\Model\Category\CategoryFacade */
 		for ($i = 0; $i < $this->categoriesCountsByLevel[$categoryLevel]; $i++) {
 			$categoryData = $this->getRandomCategoryDataByParentCategory($parentCategory);
-			$newCategory = $categoryFacade->create($categoryData);
+			$newCategory = $this->categoryFacade->create($categoryData);
 			$this->categoriesCreated++;
 			if (array_key_exists($categoryLevel + 1, $this->categoriesCountsByLevel)) {
 				$this->recursivelyCreateCategoryTree($newCategory, $categoryLevel + 1);
@@ -79,13 +87,6 @@ class CategoryDataFixture extends AbstractReferenceFixture implements DependentF
 		$categoryData->parent = $parentCategory;
 
 		return $categoryData;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public function getDependencies() {
-		return [PricingGroupDataFixture::class];
 	}
 
 }
