dd5efda4003421e49c6479c9a7cdb3f5c740bef2
Rostislav Vitek <vitek@netdevelo.cz>
2015-12-08 10:41:12 +0100

    FrontBaseController::transactional() function

--- project-base/src/SS6/ShopBundle/Component/Controller/FrontBaseController.php
+++ project-base/src/SS6/ShopBundle/Component/Controller/FrontBaseController.php
@@ -2,6 +2,8 @@
 
 namespace SS6\ShopBundle\Component\Controller;
 
+use Closure;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class FrontBaseController extends Controller {
@@ -11,5 +13,15 @@ class FrontBaseController extends Controller {
 	 */
 	public function getFlashMessageSender() {
 		return $this->get('ss6.shop.component.flash_message.sender.front');
+	}
+
+	/**
+	 * @param \Closure
+	 */
+	public function transactional(Closure $closure) {
+		$em = $this->get(EntityManager::class);
+		/* @var $em \Doctrine\ORM\EntityManager */
+
+		return $em->transactional($closure);
 	}
 }
