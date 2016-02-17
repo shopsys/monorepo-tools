00a94382710c8adb501089ae40a59eb6b05d543c
Pavel Jurecka <jurecka@shopsys.cz>
2016-02-17 18:11:34 +0100

    [US-1173] Acceptance tests for administrator login with invalid data

--- project-base/src/SS6/ShopBundle/Tests/Acceptance/acceptance/LoginCest.php
+++ project-base/src/SS6/ShopBundle/Tests/Acceptance/acceptance/LoginCest.php
@@ -6,14 +6,36 @@ use SS6\ShopBundle\Tests\Test\Codeception\AcceptanceTester;
 
 class LoginCest {
 
-	public function testLoginAdmin(AcceptanceTester $me) {
-		$me->wantTo('login on admin');
+	public function testSuccessfulLoginAdmin(AcceptanceTester $me) {
+		$me->wantTo('login on admin with valid data');
 		$me->amOnPage('/admin/');
 		$me->see('Administrace');
 		$me->fillFieldByName('admin_login_form[username]', 'admin');
 		$me->fillFieldByName('admin_login_form[password]', 'admin123');
 		$me->clickByText('Přihlásit');
 		$me->see('Nástěnka');
+	}
+
+	public function testLoginWithInvalidUsernameAdmin(AcceptanceTester $me) {
+		$me->wantTo('login on admin with nonexistent username');
+		$me->amOnPage('/admin/');
+		$me->see('Administrace');
+		$me->fillFieldByName('admin_login_form[username]', 'nonexistent username');
+		$me->fillFieldByName('admin_login_form[password]', 'password');
+		$me->clickByText('Přihlásit');
+		$me->see('Přihlášení se nepodařilo.');
+		$me->seeCurrentPageEquals('/admin/');
+	}
+
+	public function testLoginWithInvalidPasswordAdmin(AcceptanceTester $me) {
+		$me->wantTo('login on admin with invalid password');
+		$me->amOnPage('/admin/');
+		$me->see('Administrace');
+		$me->fillFieldByName('admin_login_form[username]', 'admin');
+		$me->fillFieldByName('admin_login_form[password]', 'invalid password');
+		$me->clickByText('Přihlásit');
+		$me->see('Přihlášení se nepodařilo.');
+		$me->seeCurrentPageEquals('/admin/');
 	}
 
 	public function testLoginFront(AcceptanceTester $me) {
