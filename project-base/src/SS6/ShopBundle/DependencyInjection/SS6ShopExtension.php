49554e0c081a458db38bfded1b58f5241058c258
Pavel Jurecka <jurecka@shopsys.cz>
2016-02-22 09:16:11 +0100

    SS6ShopExtension: deleted default annotation

--- project-base/src/SS6/ShopBundle/DependencyInjection/SS6ShopExtension.php
+++ project-base/src/SS6/ShopBundle/DependencyInjection/SS6ShopExtension.php
@@ -7,11 +7,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
 class SS6ShopExtension extends ConfigurableExtension {
 
 	/**
