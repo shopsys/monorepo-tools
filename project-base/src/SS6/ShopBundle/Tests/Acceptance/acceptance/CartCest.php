01c2912172eb5e8068f71717371b06d66ad34a4e
Rostislav Vitek <vitek@netdevelo.cz>
2016-02-29 16:46:11 +0100

    CartCest uses CartBoxPage page object

--- project-base/src/SS6/ShopBundle/Tests/Acceptance/acceptance/CartCest.php
+++ project-base/src/SS6/ShopBundle/Tests/Acceptance/acceptance/CartCest.php
@@ -2,7 +2,7 @@
 
 namespace SS6\ShopBundle\Tests\Acceptance\acceptance;
 
-use Facebook\WebDriver\WebDriverBy;
+use SS6\ShopBundle\Tests\Acceptance\acceptance\PageObject\Front\CartBoxPage;
 use SS6\ShopBundle\Tests\Acceptance\acceptance\PageObject\Front\CartPage;
 use SS6\ShopBundle\Tests\Acceptance\acceptance\PageObject\Front\HomepagePage;
 use SS6\ShopBundle\Tests\Acceptance\acceptance\PageObject\Front\ProductDetailPage;
@@ -14,16 +14,17 @@ class CartCest {
 	public function testAddingSameProductToCartMakesSum(
 		CartPage $cartPage,
 		ProductDetailPage $productDetailPage,
+		CartBoxPage $cartBoxPage,
 		AcceptanceTester $me
 	) {
 		$me->wantTo('have more pieces of the same product as one item in cart');
 		$me->amOnPage('/22-sencor-sle-22f46dm4-hello-kitty/');
 
 		$productDetailPage->addProductIntoCart(3);
-		$me->see('1 položka za 10 497,00 Kč', WebDriverBy::cssSelector('#cart-box'));
+		$cartBoxPage->seeInCartBox('1 položka za 10 497,00 Kč');
 
 		$productDetailPage->addProductIntoCart(3);
-		$me->see('1 položka za 20 994,00 Kč', WebDriverBy::cssSelector('#cart-box'));
+		$cartBoxPage->seeInCartBox('1 položka za 20 994,00 Kč');
 
 		$me->amOnPage('/kosik/');
 
@@ -31,36 +32,50 @@ class CartCest {
 		$me->seeInFieldByElement(6, $quantityField);
 	}
 
-	public function testAddToCartFromProductListPage(CartPage $cartPage, ProductListPage $productListPage, AcceptanceTester $me) {
+	public function testAddToCartFromProductListPage(
+		CartPage $cartPage,
+		ProductListPage $productListPage,
+		CartBoxPage $cartBoxPage,
+		AcceptanceTester $me
+	) {
 		$me->wantTo('add product to cart from product list');
 		$me->amOnPage('/televize-audio/');
 		$productListPage->addProductToCartByName('Defender 2.0 SPK-480', 1);
 		$me->see('Do košíku bylo vloženo zboží');
-		$me->seeInCss('1 položka', '.cart-box__info');
+		$cartBoxPage->seeInCartBox('1 položka');
 		$me->amOnPage('/kosik/');
 		$productPriceColumn = $cartPage->getProductPriceColumnByName('Defender 2.0 SPK-480');
 		$me->seeInElement('119,00 Kč', $productPriceColumn);
 	}
 
-	public function testAddToCartFromHomepage(CartPage $cartPage, HomepagePage $homepagePage, AcceptanceTester $me) {
+	public function testAddToCartFromHomepage(
+		CartPage $cartPage,
+		HomepagePage $homepagePage,
+		CartBoxPage $cartBoxPage,
+		AcceptanceTester $me
+	) {
 		$me->wantTo('add product to cart from homepage');
 		$me->amOnPage('/');
 		$homepagePage->addProductToCartByName('22" Sencor SLE 22F46DM4 HELLO KITTY', 1);
 		$me->see('Do košíku bylo vloženo zboží');
-		$me->seeInCss('1 položka', '.cart-box__info');
+		$cartBoxPage->seeInCartBox('1 položka');
 		$me->amOnPage('/kosik/');
 		$productPrice = $cartPage->getProductPriceColumnByName('22" Sencor SLE 22F46DM4 HELLO KITTY');
 		$me->seeInElement('3 499,00 Kč', $productPrice);
 	}
 
-	public function testAddToCartFromProductDetail(ProductDetailPage $productDetailPage, AcceptanceTester $me) {
+	public function testAddToCartFromProductDetail(
+		ProductDetailPage $productDetailPage,
+		CartBoxPage $cartBoxPage,
+		AcceptanceTester $me
+	) {
 		$me->wantTo('add product to cart from product detail');
 		$me->amOnPage('/22-sencor-sle-22f46dm4-hello-kitty/');
 		$me->see('Vložit do košíku');
 		$productDetailPage->addProductIntoCart(3);
 		$me->see('Do košíku bylo vloženo zboží');
 		$me->clickByCss('.window-button-close');
-		$me->seeInCss('1 položka za 10 497,00 Kč', '.cart-box__info');
+		$cartBoxPage->seeInCartBox('1 položka za 10 497,00 Kč');
 		$me->amOnPage('/kosik/');
 		$me->see('22" Sencor SLE 22F46DM4 HELLO KITTY');
 	}
