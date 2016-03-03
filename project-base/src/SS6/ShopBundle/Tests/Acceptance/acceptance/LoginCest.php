db371e895dfc9e3065606e5169f85616aa62a972
Rostislav Vitek <vitek@netdevelo.cz>
2016-03-03 13:46:46 +0100

    LoginAsCustomerCest and LoginCest use Admin/LoginPage

--- project-base/src/SS6/ShopBundle/Tests/Acceptance/acceptance/LoginCest.php
+++ project-base/src/SS6/ShopBundle/Tests/Acceptance/acceptance/LoginCest.php
@@ -2,38 +2,33 @@
 
 namespace SS6\ShopBundle\Tests\Acceptance\acceptance;
 
+use SS6\ShopBundle\Tests\Acceptance\acceptance\PageObject\Admin\LoginPage;
 use SS6\ShopBundle\Tests\Test\Codeception\AcceptanceTester;
 
 class LoginCest {
 
-	public function testSuccessfulLoginAdmin(AcceptanceTester $me) {
+	public function testSuccessfulLoginAdmin(LoginPage $loginPage, AcceptanceTester $me) {
 		$me->wantTo('login on admin with valid data');
 		$me->amOnPage('/admin/');
 		$me->see('Administrace');
-		$me->fillFieldByName('admin_login_form[username]', 'admin');
-		$me->fillFieldByName('admin_login_form[password]', 'admin123');
-		$me->clickByText('Přihlásit');
+		$loginPage->login('admin', 'admin123');
 		$me->see('Nástěnka');
 	}
 
-	public function testLoginWithInvalidUsernameAdmin(AcceptanceTester $me) {
+	public function testLoginWithInvalidUsernameAdmin(LoginPage $loginPage, AcceptanceTester $me) {
 		$me->wantTo('login on admin with nonexistent username');
 		$me->amOnPage('/admin/');
 		$me->see('Administrace');
-		$me->fillFieldByName('admin_login_form[username]', 'nonexistent username');
-		$me->fillFieldByName('admin_login_form[password]', 'password');
-		$me->clickByText('Přihlásit');
+		$loginPage->login('nonexistent username', 'admin123');
 		$me->see('Přihlášení se nepodařilo.');
 		$me->seeCurrentPageEquals('/admin/');
 	}
 
-	public function testLoginWithInvalidPasswordAdmin(AcceptanceTester $me) {
+	public function testLoginWithInvalidPasswordAdmin(LoginPage $loginPage, AcceptanceTester $me) {
 		$me->wantTo('login on admin with invalid password');
 		$me->amOnPage('/admin/');
 		$me->see('Administrace');
-		$me->fillFieldByName('admin_login_form[username]', 'admin');
-		$me->fillFieldByName('admin_login_form[password]', 'invalid password');
-		$me->clickByText('Přihlásit');
+		$loginPage->login('admin', 'invalid password');
 		$me->see('Přihlášení se nepodařilo.');
 		$me->seeCurrentPageEquals('/admin/');
 	}
