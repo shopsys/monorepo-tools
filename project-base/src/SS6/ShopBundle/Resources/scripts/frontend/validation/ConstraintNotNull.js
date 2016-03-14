df18aeec5e73c6ee7a2af23d5f5a05b40c5c9fa2
Petr Kadlec <kadlec@shopsys.cz>
2016-03-14 18:34:45 +0100

    JS ConstraintNotNull: fix on CheckboxType

--- project-base/src/SS6/ShopBundle/Resources/scripts/frontend/validation/ConstraintNotNull.js
+++ project-base/src/SS6/ShopBundle/Resources/scripts/frontend/validation/ConstraintNotNull.js
@@ -3,11 +3,19 @@
 	SymfonyComponentValidatorConstraintsNotNull = function () {
 		this.message = '';
 
-		this.validate = function (value) {
+		this.validate = function (value, element) {
 			var errors = [];
 			var f = FpJsFormValidator;
 
-			if (f.isValueEmty(value)) {
+			var isValueNull;
+
+			if (element.type === SS6.constant('SS6\\ShopBundle\\Form\\FormType::CHECKBOX')) {
+				isValueNull = value === null;
+			} else {
+				isValueNull = f.isValueEmty(value);
+			}
+
+			if (isValueNull) {
 				errors.push(this.message.replace('{{ value }}', String(value)));
 			}
 
