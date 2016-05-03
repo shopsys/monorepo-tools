8128f9ab83a02b2e675b45da6eac56938d9366cf
Pavel Jurecka <jurecka@shopsys.cz>
2016-05-03 14:13:27 +0200

    Flag: attribute visible

---
+++ project-base/src/SS6/ShopBundle/Migrations/Version20160503132106.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SS6\ShopBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use ShopSys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20160503132106 extends AbstractMigration {
+
+	/**
+	 * @param \Doctrine\DBAL\Schema\Schema $schema
+	 */
+	public function up(Schema $schema) {
+		$this->sql('ALTER TABLE flags ADD visible BOOLEAN NOT NULL DEFAULT TRUE;');
+		$this->sql('ALTER TABLE flags ALTER visible DROP DEFAULT;');
+	}
+
+	/**
+	 * @param \Doctrine\DBAL\Schema\Schema $schema
+	 */
+	public function down(Schema $schema) {
+
+	}
+
+}
