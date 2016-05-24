cf9f1153e75537c77587844c691ef5a4788847e7
Pavel Jurecka <jurecka@shopsys.cz>
2016-05-24 10:52:50 +0200

    CurrentCustomerTest: User, tokenStorageMock and pricingGroupSettingFacadeMock created in private methods

--- project-base/src/SS6/ShopBundle/Tests/Unit/Model/Customer/CurrentCustomerTest.php
+++ project-base/src/SS6/ShopBundle/Tests/Unit/Model/Customer/CurrentCustomerTest.php
@@ -19,16 +19,7 @@ class CurrentCustomerTest extends PHPUnit_Framework_TestCase {
 		$expectedPricingGroup = new PricingGroup(new PricingGroupData('name', 1), 1);
 
 		$tokenStorageMock = $this->getMock(TokenStorage::class, [], [], '', false);
-		$pricingGroupSettingFacadeMock = $this->getMock(
-			PricingGroupSettingFacade::class,
-			['getDefaultPricingGroupByCurrentDomain'],
-			[],
-			'',
-			false
-		);
-		$pricingGroupSettingFacadeMock
-			->method('getDefaultPricingGroupByCurrentDomain')
-			->willReturn($expectedPricingGroup);
+		$pricingGroupSettingFacadeMock = $this->getPricingGroupSettingFacadeMockReturningDefaultPricingGroup($expectedPricingGroup);
 
 		$currentCustomer = new CurrentCustomer($tokenStorageMock, $pricingGroupSettingFacadeMock);
 
@@ -38,19 +29,9 @@ class CurrentCustomerTest extends PHPUnit_Framework_TestCase {
 
 	public function testGetPricingGroupForRegisteredCustomerReturnsHisPricingGroup() {
 		$expectedPricingGroup = new PricingGroup(new PricingGroupData('name', 1), 1);
-		$billingAddress = $this->getMock(BillingAddress::class, [], [], '', false);
-		$userData = new UserData();
-		$userData->pricingGroup = $expectedPricingGroup;
-		$user = new User($userData, $billingAddress, null);
+		$user = $this->getUserWithPricingGroup($expectedPricingGroup);
 
-		$tokenMock = $this->getMockBuilder(TokenInterface::class)
-			->setMethods(['getUser'])
-			->getMockForAbstractClass();
-		$tokenMock->method('getUser')->willReturn($user);
-
-		$tokenStorageMock = $this->getMock(TokenStorage::class, ['getToken'], [], '', false);
-		$tokenStorageMock->method('getToken')->willReturn($tokenMock);
-
+		$tokenStorageMock = $this->getTokenStorageMockForUser($user);
 		$pricingGroupFacadeMock = $this->getMock(PricingGroupSettingFacade::class, [], [], '', false);
 
 		$currentCustomer = new CurrentCustomer($tokenStorageMock, $pricingGroupFacadeMock);
@@ -59,4 +40,48 @@ class CurrentCustomerTest extends PHPUnit_Framework_TestCase {
 		$this->assertSame($expectedPricingGroup, $pricingGroup);
 	}
 
+	/**
+	 * @param \SS6\ShopBundle\Model\Pricing\Group\PricingGroup $defaultPricingGroup
+	 * @return \PHPUnit_Framework_MockObject_MockObject|\SS6\ShopBundle\Model\Pricing\Group\PricingGroupSettingFacade
+	 */
+	private function getPricingGroupSettingFacadeMockReturningDefaultPricingGroup(PricingGroup $defaultPricingGroup) {
+		$pricingGroupSettingFacadeMock = $this->getMockBuilder(PricingGroupSettingFacade::class)
+			->setMethods(['getDefaultPricingGroupByCurrentDomain'])
+			->disableOriginalConstructor()
+			->getMock();
+
+		$pricingGroupSettingFacadeMock
+			->method('getDefaultPricingGroupByCurrentDomain')
+			->willReturn($defaultPricingGroup);
+
+		return $pricingGroupSettingFacadeMock;
+	}
+
+	/**
+	 * @param \SS6\ShopBundle\Model\Pricing\Group\PricingGroup $pricingGroup
+	 * @return \SS6\ShopBundle\Model\Customer\User
+	 */
+	private function getUserWithPricingGroup(PricingGroup $pricingGroup) {
+		$billingAddress = $this->getMock(BillingAddress::class, [], [], '', false);
+		$userData = new UserData();
+		$userData->pricingGroup = $pricingGroup;
+
+		return new User($userData, $billingAddress, null);
+	}
+
+	/**
+	 * @param \SS6\ShopBundle\Model\Customer\User $user
+	 * @return \PHPUnit_Framework_MockObject_MockObject|\Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage
+	 */
+	private function getTokenStorageMockForUser(User $user) {
+		$tokenMock = $this->getMockBuilder(TokenInterface::class)
+			->setMethods(['getUser'])
+			->getMockForAbstractClass();
+		$tokenMock->method('getUser')->willReturn($user);
+
+		$tokenStorageMock = $this->getMock(TokenStorage::class, ['getToken'], [], '', false);
+		$tokenStorageMock->method('getToken')->willReturn($tokenMock);
+
+		return $tokenStorageMock;
+	}
 }
