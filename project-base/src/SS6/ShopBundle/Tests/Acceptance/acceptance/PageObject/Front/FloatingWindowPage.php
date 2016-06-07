1b66726113767c77afca73699cf6e578eb53bff6
Petr Kadlec <kadlec@shopsys.cz>
2016-06-07 15:48:45 +0200

    [BG-646] fix acceptance tests with close floating window

--- project-base/src/SS6/ShopBundle/Tests/Acceptance/acceptance/PageObject/Front/FloatingWindowPage.php
+++ project-base/src/SS6/ShopBundle/Tests/Acceptance/acceptance/PageObject/Front/FloatingWindowPage.php
@@ -8,6 +8,7 @@ class FloatingWindowPage extends AbstractPage {
 
 	public function closeFloatingWindow() {
 		$this->tester->clickByCss('.js-window-button-close');
+		$this->tester->wait(1); // animation of closing sometime hides page content
 	}
 
 }
