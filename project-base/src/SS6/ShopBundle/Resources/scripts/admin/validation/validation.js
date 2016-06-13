efe2f5cb8527ce9f4776bbe2789f227f533d29d5
Pavel Jurecka <jurecka@shopsys.cz>
2016-06-13 12:59:06 +0200

    validation.js: common functions for admin and frontend moved to customizeBundle.js

--- project-base/src/SS6/ShopBundle/Resources/scripts/admin/validation/validation.js
+++ project-base/src/SS6/ShopBundle/Resources/scripts/admin/validation/validation.js
@@ -22,47 +22,6 @@
 		}
 	};
 
-	SS6.validation.showErrors = function (errors, elementName) {
-		var $errorList = SS6.validation.findOrCreateErrorList($(this), elementName);
-		var $errorListUl = $errorList.find('ul:first');
-		var $elementsToHighlight = SS6.validation.findElementsToHighlight($(this));
-
-		var elementErrorClass = 'js-' + elementName;
-		$errorListUl.find('li').remove();
-
-		if (errors.length > 0) {
-			$elementsToHighlight.addClass('form-input-error');
-			$.each(errors, function (key, message) {
-				$errorListUl
-					.append($('<li/>')
-					.addClass('js-validation-errors-message')
-					.addClass(elementErrorClass)
-					.text(message));
-			});
-			$errorList.show();
-		} else if ($errorListUl.find('li').size() === 0) {
-			$elementsToHighlight.removeClass('form-input-error');
-			$errorList.hide();
-		}
-
-		SS6.validation.highlightSubmitButtons($(this).closest('form'));
-	};
-
-	SS6.validation.findOrCreateErrorList = function ($formInput, elementName) {
-		var errorListClass = SS6.validation.getErrorListClass(elementName);
-		var $errorList = $('.' + errorListClass);
-		if ($errorList.size() === 0) {
-			$errorList = $($.parseHTML(
-				'<div class="in-message in-message--danger js-validation-errors-list ' + errorListClass + '">\
-					<ul class="in-message__list"></ul>\
-				</div>'
-			));
-			$errorList.insertBefore($formInput);
-		}
-
-		return $errorList;
-	};
-
 	SS6.validation.findElementsToHighlight = function ($formInput) {
 		return $formInput.filter('input, select, textarea, .form-line, .table-form');
 	};
