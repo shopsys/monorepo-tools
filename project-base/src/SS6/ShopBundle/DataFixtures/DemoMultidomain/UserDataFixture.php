da4c92521d11dce84ba2c929d9dc2c60d1e04c6c
Miroslav Sustek <miroslav.sustek@shopsys.cz>
2016-06-14 16:04:28 +0200

    UserDataFixture: separate to singledomain and multidomain

--- project-base/src/SS6/ShopBundle/DataFixtures/DemoMultidomain/UserDataFixture.php
+++ project-base/src/SS6/ShopBundle/DataFixtures/DemoMultidomain/UserDataFixture.php
@@ -2,19 +2,13 @@
 
 namespace SS6\ShopBundle\DataFixtures\Demo;
 
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use SS6\ShopBundle\Component\DataFixture\AbstractReferenceFixture;
-use SS6\ShopBundle\Component\Domain\Domain;
-use SS6\ShopBundle\DataFixtures\Base\SettingValueDataFixture;
 use SS6\ShopBundle\DataFixtures\Demo\CountryDataFixture;
 use SS6\ShopBundle\DataFixtures\Demo\UserDataFixtureLoader;
 use SS6\ShopBundle\Model\Customer\CustomerFacade;
 
-/**
- * @multidomain
- */
-class UserDataFixture extends AbstractReferenceFixture implements DependentFixtureInterface {
+class UserDataFixture extends AbstractReferenceFixture {
 
 	/**
 	 * @param \Doctrine\Common\Persistence\ObjectManager $manager
@@ -26,25 +20,16 @@ class UserDataFixture extends AbstractReferenceFixture implements DependentFixtu
 		/* @var $loaderService \SS6\ShopBundle\DataFixtures\Demo\UserDataFixtureLoader */
 
 		$countries = [
-			$this->getReference(CountryDataFixture::COUNTRY_CZECH_REPUBLIC_1),
-			$this->getReference(CountryDataFixture::COUNTRY_SLOVAKIA_1),
+			$this->getReference(CountryDataFixture::COUNTRY_CZECH_REPUBLIC_2),
+			$this->getReference(CountryDataFixture::COUNTRY_SLOVAKIA_2),
 		];
 		$loaderService->injectReferences($countries);
 
-		$customersData = $loaderService->getCustomersDataByDomainId(Domain::FIRST_DOMAIN_ID);
+		$customersData = $loaderService->getCustomersDataByDomainId(2);
 
 		foreach ($customersData as $customerData) {
 			$customerFacade->create($customerData);
 		}
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public function getDependencies() {
-		return [
-			SettingValueDataFixture::class,
-			CountryDataFixture::class,
-		];
-	}
 }
