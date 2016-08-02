8043a0bfa2815a6b6730a93d878f4d63c13028b6
Petr Kadlec <kadlec@shopsys.cz>
2016-08-02 09:28:11 +0200

    developer's error pages are accessible in all environments

--- project-base/src/SS6/ShopBundle/Controller/Admin/SuperadminController.php
+++ project-base/src/SS6/ShopBundle/Controller/Admin/SuperadminController.php
@@ -3,7 +3,6 @@
 namespace SS6\ShopBundle\Controller\Admin;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use SS6\Environment;
 use SS6\ShopBundle\Component\Controller\AdminBaseController;
 use SS6\ShopBundle\Component\Grid\ArrayDataSource;
 use SS6\ShopBundle\Component\Grid\GridFactory;
@@ -77,9 +76,7 @@ class SuperadminController extends AdminBaseController {
 	 * @Route("/superadmin/errors/")
 	 */
 	public function errorsAction() {
-		return $this->render('@SS6Shop/Admin/Content/Superadmin/errors.html.twig', [
-			'ENVIRONMENT_DEVELOPMENT' => Environment::ENVIRONMENT_DEVELOPMENT,
-		]);
+		return $this->render('@SS6Shop/Admin/Content/Superadmin/errors.html.twig');
 	}
 
 	/**
