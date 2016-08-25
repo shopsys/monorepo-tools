8f3d5f6bb4a416b193b516b50c8a7cdf1da31c05
Petr Kadlec <kadlec@shopsys.cz>
2016-08-25 06:11:52 +0200

    fix flushing of new ProductVisibility entities

--- project-base/src/SS6/ShopBundle/Model/Product/ProductEditFacade.php
+++ project-base/src/SS6/ShopBundle/Model/Product/ProductEditFacade.php
@@ -377,9 +377,9 @@ class ProductEditFacade {
 	 * @param \SS6\ShopBundle\Model\Product\Product $product
 	 */
 	private function createProductVisibilities(Product $product) {
+		$toFlush = [];
 		foreach ($this->domain->getAll() as $domainConfig) {
 			$domainId = $domainConfig->getId();
-			$toFlush = [];
 			foreach ($this->pricingGroupRepository->getPricingGroupsByDomainId($domainId) as $pricingGroup) {
 				$productVisibility = new ProductVisibility($product, $pricingGroup, $domainId);
 				$this->em->persist($productVisibility);
