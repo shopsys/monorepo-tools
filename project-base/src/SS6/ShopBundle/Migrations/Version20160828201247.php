9d628bc3853f73baf376cac2cde9399a78454985
Petr Kadlec <kadlec@shopsys.cz>
2016-09-02 13:37:38 +0200

    DB migration with google analttics is multidomain

--- project-base/src/SS6/ShopBundle/Migrations/Version20160828201247.php
+++ project-base/src/SS6/ShopBundle/Migrations/Version20160828201247.php
@@ -4,20 +4,21 @@ namespace SS6\ShopBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use ShopSys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
-use SS6\ShopBundle\Model\Script\Script;
+use SS6\ShopBundle\Component\Migration\MultidomainMigrationTrait;
 
 class Version20160828201247 extends AbstractMigration {
+
+	use MultidomainMigrationTrait;
 
 	/**
 	 * @param \Doctrine\DBAL\Schema\Schema $schema
 	 */
 	public function up(Schema $schema) {
-		$this->sql('INSERT INTO setting_values (name, domain_id, value, type) VALUES
-			(\'' . Script::GOOGLE_ANALYTICS_TRACKING_ID_SETTING_NAME . '\', 1, null, \'string\');
-		');
-		$this->sql('INSERT INTO setting_values (name, domain_id, value, type) VALUES
-			(\'' . Script::GOOGLE_ANALYTICS_TRACKING_ID_SETTING_NAME . '\', 2, null, \'string\');
-		');
+		foreach ($this->getAllDomainIds() as $domainId) {
+			$this->sql('INSERT INTO setting_values (name, domain_id, value, type) VALUES
+				(\'googleAnalyticsTrackingId\', :domainId, null, \'string\');
+			', ['domainId' => $domainId]);
+		}
 	}
 
 	/**
