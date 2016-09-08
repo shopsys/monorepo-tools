07d620959d720291d29d37d0a5a64f35654d8d03
Petr Kadlec <kadlec@shopsys.cz>
2016-09-08 07:10:46 +0200

    fix multidomain migrations after rebase

--- project-base/src/SS6/ShopBundle/Migrations/Version20160902145842.php
+++ project-base/src/SS6/ShopBundle/Migrations/Version20160902145842.php
@@ -4,17 +4,21 @@ namespace SS6\ShopBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use ShopSys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+use SS6\ShopBundle\Component\Migration\MultidomainMigrationTrait;
 
 class Version20160902145842 extends AbstractMigration {
+
+	use MultidomainMigrationTrait;
 
 	/**
 	 * @param \Doctrine\DBAL\Schema\Schema $schema
 	 */
 	public function up(Schema $schema) {
-		$sql = 'INSERT INTO setting_values VALUES
-			(\'heurekaWidgetCode\', 1, null, \'string\'),
-			(\'heurekaWidgetCode\', 2, null, \'string\');';
-		$this->sql($sql);
+		foreach ($this->getAllDomainIds() as $domainId) {
+			$this->sql('INSERT INTO setting_values (name, domain_id, value, type) VALUES
+				(\'heurekaWidgetCode\', :domainId, null, \'string\');
+			', ['domainId' => $domainId]);
+		}
 	}
 
 	/**
