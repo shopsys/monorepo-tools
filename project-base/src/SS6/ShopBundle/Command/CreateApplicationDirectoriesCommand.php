8180721b71ff860e04d78cfc2bb1fe4999e1fed4
Miroslav Sustek <miroslav.sustek@shopsys.cz>
2016-10-04 17:43:39 +0200

    move phing "dirs-create" target implementation into CreateApplicationDirectoriesCommand
    The main reason is that Phing's MkdirTask does not work well with ACL.
    (It clamps permissions to umask while with ACL the permissions are set according to default mask ignoring umask.)

---
+++ project-base/src/SS6/ShopBundle/Command/CreateApplicationDirectoriesCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace SS6\ShopBundle\Command;
+
+use SS6\ShopBundle\Component\Image\DirectoryStructureCreator as ImageDirectoryStructureCreator;
+use SS6\ShopBundle\Component\UploadedFile\DirectoryStructureCreator as UploadedFileDirectoryStructureCreator;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+class CreateApplicationDirectoriesCommand extends ContainerAwareCommand {
+
+	protected function configure() {
+		$this
+			->setName('ss6:create-directories')
+			->setDescription('Create application directories for locks, docs, content, images, uploaded files, etc.');
+	}
+
+	/**
+	 * @param \Symfony\Component\Console\Input\InputInterface $input
+	 * @param \Symfony\Component\Console\Output\OutputInterface $output
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$this->createMiscellaneousDirectories($output);
+		$this->createImageDirectories($output);
+		$this->createUploadedFileDirectories($output);
+	}
+
+	private function createMiscellaneousDirectories(OutputInterface $output) {
+		$directories = [];
+		$directories[] = $this->getContainer()->getParameter('kernel.root_dir') . '/lock';
+		$directories[] = $this->getContainer()->getParameter('kernel.root_dir') . '/errorPages';
+		$directories[] = $this->getContainer()->getParameter('ss6.root_dir') . '/docs/admin';
+		$directories[] = $this->getContainer()->getParameter('ss6.root_dir') . '/docs/frontend';
+		$directories[] = $this->getContainer()->getParameter('ss6.web_dir') . '/content/feeds';
+		$directories[] = $this->getContainer()->getParameter('ss6.web_dir') . '/content/sitemaps';
+		$directories[] = $this->getContainer()->getParameter('ss6.web_dir') . '/content/wysiwyg';
+
+		$filesystem = $this->getContainer()->get(Filesystem::class);
+		/* @var $filesystem \Symfony\Component\Filesystem\Filesystem */
+
+		$filesystem->mkdir($directories);
+
+		$output->writeln('<fg=green>Miscellaneous application directories were successfully created.</fg=green>');
+	}
+
+	/**
+	 * @param \Symfony\Component\Console\Output\OutputInterface $output
+	 */
+	private function createImageDirectories(OutputInterface $output) {
+		$imageDirectoryStructureCreator = $this->getContainer()->get(ImageDirectoryStructureCreator::class);
+		/* @var $imageDirectoryStructureCreator \SS6\ShopBundle\Component\Image\DirectoryStructureCreator */
+		$imageDirectoryStructureCreator->makeImageDirectories();
+
+		$output->writeln('<fg=green>Directories for images were successfully created.</fg=green>');
+	}
+
+	/**
+	 * @param \Symfony\Component\Console\Output\OutputInterface $output
+	 */
+	private function createUploadedFileDirectories(OutputInterface $output) {
+		$uploadedFileDirectoryStructureCreator = $this->getContainer()->get(UploadedFileDirectoryStructureCreator::class);
+		/* @var $uploadedFileDirectoryStructureCreator \SS6\ShopBundle\Component\UploadedFile\DirectoryStructureCreator */
+		$uploadedFileDirectoryStructureCreator->makeUploadedFileDirectories();
+
+		$output->writeln('<fg=green>Directories for UploadedFile entities were successfully created.</fg=green>');
+	}
+
+}
