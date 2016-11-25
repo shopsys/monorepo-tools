9840a48c6f5f89fcf0c27951cd29b3378f78c824
Peter Turek <peter.turek@shopsys.com>
2016-11-25 14:31:55 +0100

    [US-689] Extended sitemap by priority

--- project-base/src/SS6/ShopBundle/Component/Sitemap/SitemapListener.php
+++ project-base/src/SS6/ShopBundle/Component/Sitemap/SitemapListener.php
@@ -15,6 +15,11 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class SitemapListener implements SitemapListenerInterface {
 
+	const PRIORITY_HOMEPAGE = 1;
+	const PRIORITY_CATEGORIES = 0.8;
+	const PRIORITY_PRODUCTS = 0.7;
+	const PRIORITY_ARTICLES = 0.5;
+
 	/**
 	 * @var \SS6\ShopBundle\Component\Sitemap\SitemapFacade
 	 */
@@ -57,16 +62,16 @@ class SitemapListener implements SitemapListenerInterface {
 		$generator = $event->getGenerator();
 		$domainConfig = $this->domain->getDomainConfigById($domainId);
 
-		$this->addHomepageUrl($generator, $domainConfig, $section);
+		$this->addHomepageUrl($generator, $domainConfig, $section, self::PRIORITY_HOMEPAGE);
 
 		$productSitemapItems = $this->sitemapFacade->getSitemapItemsForVisibleProducts($domainConfig);
-		$this->addUrlsBySitemapItems($productSitemapItems, $generator, $domainConfig, $section);
+		$this->addUrlsBySitemapItems($productSitemapItems, $generator, $domainConfig, $section, self::PRIORITY_PRODUCTS);
 
 		$categorySitemapItems = $this->sitemapFacade->getSitemapItemsForVisibleCategories($domainConfig);
-		$this->addUrlsBySitemapItems($categorySitemapItems, $generator, $domainConfig, $section);
+		$this->addUrlsBySitemapItems($categorySitemapItems, $generator, $domainConfig, $section, self::PRIORITY_CATEGORIES);
 
 		$articleSitemapItems = $this->sitemapFacade->getSitemapItemsForArticlesOnDomain($domainConfig);
-		$this->addUrlsBySitemapItems($articleSitemapItems, $generator, $domainConfig, $section);
+		$this->addUrlsBySitemapItems($articleSitemapItems, $generator, $domainConfig, $section, self::PRIORITY_ARTICLES);
 	}
 
 	/**
@@ -74,16 +79,18 @@ class SitemapListener implements SitemapListenerInterface {
 	 * @param \Presta\SitemapBundle\Service\AbstractGenerator $generator
 	 * @param \SS6\ShopBundle\Component\Domain\Config\DomainConfig $domainConfig
 	 * @param string $section
+	 * @param int $elementPriority
 	 */
 	private function addUrlsBySitemapItems(
 		array $sitemapItems,
 		AbstractGenerator $generator,
 		DomainConfig $domainConfig,
-		$section
+		$section,
+		$elementPriority
 	) {
 		foreach ($sitemapItems as $sitemapItem) {
 			$absoluteUrl = $this->friendlyUrlService->getAbsoluteUrlByDomainConfigAndSlug($domainConfig, $sitemapItem->slug);
-			$urlConcrete = new UrlConcrete($absoluteUrl);
+			$urlConcrete = new UrlConcrete($absoluteUrl, null, null, $elementPriority);
 			$generator->addUrl($urlConcrete, $section);
 		}
 	}
@@ -92,15 +99,17 @@ class SitemapListener implements SitemapListenerInterface {
 	 * @param \Presta\SitemapBundle\Service\AbstractGenerator $generator
 	 * @param \SS6\ShopBundle\Component\Domain\Config\DomainConfig $domainConfig
 	 * @param string $section
+	 * @param int $elementPriority
 	 */
 	private function addHomepageUrl(
 		AbstractGenerator $generator,
 		DomainConfig $domainConfig,
-		$section
+		$section,
+		$elementPriority
 	) {
 		$domainRouter = $this->domainRouterFactory->getRouter($domainConfig->getId());
 		$absoluteUrl = $domainRouter->generate('front_homepage', [], UrlGeneratorInterface::ABSOLUTE_URL);
-		$urlConcrete = new UrlConcrete($absoluteUrl);
+		$urlConcrete = new UrlConcrete($absoluteUrl, null, null, $elementPriority);
 		$generator->addUrl($urlConcrete, $section);
 	}
 
