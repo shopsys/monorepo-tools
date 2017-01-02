c51b2d8105bf2e8465a57aa8a892b16af8621b34
Peter Turek <peter.turek@shopsys.com>
2017-01-02 12:00:41 +0100

    [BG-1092] authentication record is not added

--- project-base/src/SS6/ShopBundle/Migrations/Version20161207135225.php
+++ project-base/src/SS6/ShopBundle/Migrations/Version20161207135225.php
@@ -16,12 +16,22 @@ class Version20161207135225 extends AbstractMigration {
 	public function up(Schema $schema) {
 		foreach ($this->getAllDomainIds() as $domainId) {
 			$this->sql('DELETE FROM migrations WHERE version = \'201601207135225\';');
-			$this->sql('INSERT INTO setting_values (name, domain_id, value, type) VALUES
-				(\'shopInfoPhoneNumber\', :domainId, \'+420123456789\', \'string\') ON CONFLICT DO NOTHING;
-			', ['domainId' => $domainId]);
-			$this->sql('INSERT INTO setting_values (name, domain_id, value, type) VALUES
-				(\'shopInfoEmail\', :domainId, \'no-reply@shopsys.com\', \'string\') ON CONFLICT DO NOTHING;
-			', ['domainId' => $domainId]);
+
+			$phoneNumber = $this->sql('SELECT COUNT(*) FROM setting_values WHERE name = \'shopInfoPhoneNumber\' AND domain_id = :domainId;
+			', ['domainId' => $domainId])->fetchColumn(0);
+			$infoMail = $this->sql('SELECT COUNT(*) FROM setting_values WHERE name = \'shopInfoEmail\' AND domain_id = :domainId;
+			', ['domainId' => $domainId])->fetchColumn(0);
+
+			if ($phoneNumber <= 0) {
+				$this->sql('INSERT INTO setting_values (name, domain_id, value, type) VALUES
+					(\'shopInfoPhoneNumber\', :domainId, \'+420123456789\', \'string\');
+				', ['domainId' => $domainId]);
+			}
+			if ($infoMail <= 0) {
+				$this->sql('INSERT INTO setting_values (name, domain_id, value, type) VALUES
+					(\'shopInfoEmail\', :domainId, \'no-reply@shopsys.com\', \'string\');
+				', ['domainId' => $domainId]);
+			}
 		}
 	}
 
--- project-base/src/SS6/ShopBundle/Migrations/Version20161207144725.php
+++ project-base/src/SS6/ShopBundle/Migrations/Version20161207144725.php
@@ -16,9 +16,15 @@ class Version20161207144725 extends AbstractMigration {
 	public function up(Schema $schema) {
 		foreach ($this->getAllDomainIds() as $domainId) {
 			$this->sql('DELETE FROM migrations WHERE version = \'201601207144725\';');
-			$this->sql('INSERT INTO setting_values (name, domain_id, value, type) VALUES
-				(\'shopInfoPhoneHours\', :domainId, \'(po-pá, 10:00 - 16:00)\', \'string\') ON CONFLICT DO NOTHING;
-			', ['domainId' => $domainId]);
+
+			$phoneHours = $this->sql('SELECT COUNT(*) FROM setting_values WHERE name = \'shopInfoPhoneHours\' AND domain_id = :domainId;
+			', ['domainId' => $domainId])->fetchColumn(0);
+
+			if ($phoneHours <= 0) {
+				$this->sql('INSERT INTO setting_values (name, domain_id, value, type) VALUES
+					(\'shopInfoPhoneHours\', :domainId, \'(po-pá, 10:00 - 16:00)\', \'string\');
+				', ['domainId' => $domainId]);
+			}
 		}
 	}
 
