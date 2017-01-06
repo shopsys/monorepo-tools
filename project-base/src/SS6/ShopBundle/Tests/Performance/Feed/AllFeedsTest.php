5d7d3bf1344caa1227d35167e6991bd2175a9599
Petr Heinz <petr.heinz@shopsys.com>
2017-01-06 09:15:13 +0100

    Feed generation in performance test is executed by client request to avoid memory leaks

--- project-base/src/SS6/ShopBundle/Tests/Performance/Feed/AllFeedsTest.php
+++ project-base/src/SS6/ShopBundle/Tests/Performance/Feed/AllFeedsTest.php
@@ -4,16 +4,20 @@ namespace SS6\ShopBundle\Tests\Performance\Feed;
 
 use SS6\ShopBundle\Component\Domain\Config\DomainConfig;
 use SS6\ShopBundle\Component\Domain\Domain;
+use SS6\ShopBundle\Component\Router\CurrentDomainRouter;
 use SS6\ShopBundle\Model\Feed\FeedConfig;
 use SS6\ShopBundle\Model\Feed\FeedConfigFacade;
-use SS6\ShopBundle\Model\Feed\FeedFacade;
 use SS6\ShopBundle\Tests\Test\FunctionalTestCase;
 use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Routing\RouterInterface;
 
 class AllFeedsTest extends FunctionalTestCase {
 
 	const MAX_DURATION_FEED_SECONDS = 180;
 	const MAX_DURATION_DELIVERY_FEED_SECONDS = 20;
+
+	const ADMIN_USERNAME = 'admin';
+	const ADMIN_PASSWORD = 'admin123';
 
 	/**
 	 * @param \SS6\ShopBundle\Model\Feed\FeedConfig $feedConfig
@@ -22,8 +26,6 @@ class AllFeedsTest extends FunctionalTestCase {
 	 * @dataProvider getAllFeedGenerationData
 	 */
 	public function testFeedGeneration(FeedConfig $feedConfig, DomainConfig $domainConfig, $maxDuration) {
-		$feedFacade = $this->getContainer()->get(FeedFacade::class);
-		/* @var $feedFacade \SS6\ShopBundle\Model\Feed\FeedFacade */
 		$consoleOutput = new ConsoleOutput();
 
 		$consoleOutput->writeln('');
@@ -38,7 +40,7 @@ class AllFeedsTest extends FunctionalTestCase {
 		);
 
 		$startTime = microtime(true);
-		$feedFacade->generateFeed($feedConfig, $domainConfig);
+		$this->generateFeed($feedConfig, $domainConfig);
 		$endTime = microtime(true);
 
 		$duration = $endTime - $startTime;
@@ -103,6 +105,30 @@ class AllFeedsTest extends FunctionalTestCase {
 				sprintf('<fg=red>Feed generated in %.2F s, exceeding limit of %d s.</fg=red>', $realDuration, $maxDuration)
 			);
 			$this->fail(sprintf('Feed generation exceeded limit of %d s.', $maxDuration));
+		}
+	}
+
+	/**
+	 * @param \SS6\ShopBundle\Model\Feed\FeedConfig $feedConfig
+	 * @param \SS6\ShopBundle\Component\Domain\Config\DomainConfig $domainConfig
+	 */
+	private function generateFeed(FeedConfig $feedConfig, DomainConfig $domainConfig) {
+		$router = $this->getContainer()->get(CurrentDomainRouter::class);
+		/* @var $router \SS6\ShopBundle\Component\Router\CurrentDomainRouter */
+		$client = $this->getClient(true, self::ADMIN_USERNAME, self::ADMIN_PASSWORD);
+
+		$feedGenerationParameters = [
+			'feedName' => $feedConfig->getFeedName(),
+			'domainId' => $domainConfig->getId(),
+		];
+		$uri = $router->generate('admin_feed_generate', $feedGenerationParameters, RouterInterface::RELATIVE_PATH);
+
+		$client->request('GET', $uri);
+
+		$expectedStatusCode = 302;
+		$statusCode = $client->getResponse()->getStatusCode();
+		if ($statusCode !== $expectedStatusCode) {
+			$this->fail(sprintf('Admin request on %s failed with status code %d, expected %d.', $uri, $statusCode, $expectedStatusCode));
 		}
 	}
 
