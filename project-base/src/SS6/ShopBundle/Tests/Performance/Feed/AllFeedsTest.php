de14def7b7cb66a23de0091144e1919ca25cd773
Petr Heinz <petr.heinz@shopsys.com>
2017-01-09 11:54:01 +0100

    Test feed generation outputs CSV report
    - tests rewritten into one test with internal data provider to allow summary report
    - new data object Feed/PerformanceTestSample for better data handling

--- project-base/src/SS6/ShopBundle/Tests/Performance/Feed/AllFeedsTest.php
+++ project-base/src/SS6/ShopBundle/Tests/Performance/Feed/AllFeedsTest.php
@@ -8,6 +8,8 @@ use SS6\ShopBundle\Component\Domain\Domain;
 use SS6\ShopBundle\Component\Router\CurrentDomainRouter;
 use SS6\ShopBundle\Model\Feed\FeedConfig;
 use SS6\ShopBundle\Model\Feed\FeedConfigFacade;
+use SS6\ShopBundle\Tests\Performance\Feed\PerformanceResultsCsvExporter;
+use SS6\ShopBundle\Tests\Performance\Feed\PerformanceTestSample;
 use SS6\ShopBundle\Tests\Test\FunctionalTestCase;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Routing\RouterInterface;
@@ -18,32 +20,60 @@ class AllFeedsTest extends FunctionalTestCase {
 	const MAX_DURATION_DELIVERY_FEED_SECONDS = 20;
 	const SUSPICIOUSLY_LOW_DURATION_SECONDS = 5;
 
+	const ROUTE_NAME_GENERATE_FEED = 'admin_feed_generate';
 	const ADMIN_USERNAME = 'admin';
 	const ADMIN_PASSWORD = 'admin123';
+
+	public function testAllFeedsGeneration() {
+		$performanceResultsCsvExporter = $this->getContainer()->get(PerformanceResultsCsvExporter::class);
+		/* @var $performanceResultsCsvExporter \SS6\ShopBundle\Tests\Performance\Feed\PerformanceResultsCsvExporter */
+		$consoleOutput = new ConsoleOutput();
+
+		$consoleOutput->writeln('');
+		$consoleOutput->writeln('<fg=cyan>Testing generation of all feeds:</fg=cyan>');
+
+		$performanceTestSamples = [];
+		$allFeedGenerationData = $this->getAllFeedGenerationData();
+		foreach ($allFeedGenerationData as $feedGenerationData) {
+			list($feedConfig, $domainConfig, $maxDuration) = $feedGenerationData;
+			/* @var $feedConfig \SS6\ShopBundle\Model\Feed\FeedConfig */
+			/* @var $domainConfig \SS6\ShopBundle\Component\Domain\Config\DomainConfig */
+
+			$consoleOutput->writeln(
+				sprintf(
+					'Generating feed "%s" (%s) for %s (domain ID %d)...',
+					$feedConfig->getLabel(),
+					$feedConfig->getFeedName(),
+					$domainConfig->getName(),
+					$domainConfig->getId()
+				)
+			);
+
+			$performanceTestSample = $this->doTestFeedGeneration($feedConfig, $domainConfig, $maxDuration);
+			$consoleOutput->writeln($performanceTestSample->getMessage());
+
+			$performanceTestSamples[] = $performanceTestSample;
+		}
+
+		$performanceResultsCsvExporter->exportJmeterCsvReport(
+			$performanceTestSamples,
+			$this->getContainer()->getParameter('ss6.root_dir') . '/build/stats/performance-tests-feeds.csv'
+		);
+
+		$this->assertSamplesAreSuccessful($performanceTestSamples);
+	}
 
 	/**
 	 * @param \SS6\ShopBundle\Model\Feed\FeedConfig $feedConfig
 	 * @param \SS6\ShopBundle\Component\Domain\Config\DomainConfig $domainConfig
 	 * @param int $maxDuration
-	 * @dataProvider getAllFeedGenerationData
+	 * @return \SS6\ShopBundle\Tests\Performance\Feed\PerformanceTestSample
 	 */
-	public function testFeedGeneration(FeedConfig $feedConfig, DomainConfig $domainConfig, $maxDuration) {
-		$consoleOutput = new ConsoleOutput();
+	private function doTestFeedGeneration(FeedConfig $feedConfig, DomainConfig $domainConfig, $maxDuration) {
+		$performanceTestSample = $this->generateFeed($feedConfig, $domainConfig);
+		$this->setPerformanceTestSampleMessage($performanceTestSample, $maxDuration, $performanceTestSample->getDuration());
 
-		$consoleOutput->writeln('');
-		$consoleOutput->writeln(
-			sprintf(
-				'Generating feed "%s" (%s) for %s (domain ID %d)...',
-				$feedConfig->getLabel(),
-				$feedConfig->getFeedName(),
-				$domainConfig->getName(),
-				$domainConfig->getId()
-			)
-		);
-
-		$duration = $this->generateFeed($feedConfig, $domainConfig);
-
-		$this->assertFeedGenerationDuration($maxDuration, $duration, $consoleOutput);
+		return $performanceTestSample;
 	}
 
 	/**
@@ -79,8 +109,7 @@ class AllFeedsTest extends FunctionalTestCase {
 		$feedGenerationData = [];
 		foreach ($domainConfigs as $domainConfig) {
 			foreach ($feedConfigs as $feedConfig) {
-				$dataSetName = sprintf('feed "%s" on domain with ID %d', $feedConfig->getFeedName(), $domainConfig->getId());
-				$feedGenerationData[$dataSetName] = [$feedConfig, $domainConfig, $maxDuration];
+				$feedGenerationData[] = [$feedConfig, $domainConfig, $maxDuration];
 			}
 		}
 
@@ -88,34 +117,32 @@ class AllFeedsTest extends FunctionalTestCase {
 	}
 
 	/**
+	 * @param \SS6\ShopBundle\Tests\Performance\Feed\PerformanceTestSample $performanceTestSample
 	 * @param int $maxDuration
 	 * @param float $realDuration
-	 * @param \Symfony\Component\Console\Output\ConsoleOutput $consoleOutput
 	 */
-	private function assertFeedGenerationDuration($maxDuration, $realDuration, ConsoleOutput $consoleOutput) {
-		$this->addToAssertionCount(1);
+	private function setPerformanceTestSampleMessage(PerformanceTestSample $performanceTestSample, $maxDuration, $realDuration) {
+		$minDuration = self::SUSPICIOUSLY_LOW_DURATION_SECONDS;
 
-		if ($realDuration < self::SUSPICIOUSLY_LOW_DURATION_SECONDS) {
-			$consoleOutput->writeln(
-				sprintf('<fg=yellow>Feed generated in %.2F s, which is suspiciously fast and should be checked.</fg=yellow>', $realDuration)
-			);
-			$this->fail(sprintf('Feed was generated faster than in %d s, which is suspicious and should be checked.', $maxDuration));
+		if ($realDuration < $minDuration) {
+			$message = sprintf('<fg=yellow>Feed generated in %.2F s, which is suspiciously fast.</fg=yellow>', $realDuration);
+			$failMessage = sprintf('Feed was generated faster than in %d s, which is suspicious and should be checked.', $minDuration);
+			$performanceTestSample->addFailMessage($failMessage);
 		} elseif ($realDuration <= $maxDuration) {
-			$consoleOutput->writeln(
-				sprintf('<fg=green>Feed generated in %.2F s.</fg=green>', $realDuration)
-			);
+			$message = sprintf('<fg=green>Feed generated in %.2F s.</fg=green>', $realDuration);
 		} else {
-			$consoleOutput->writeln(
-				sprintf('<fg=red>Feed generated in %.2F s, exceeding limit of %d s.</fg=red>', $realDuration, $maxDuration)
-			);
-			$this->fail(sprintf('Feed generation exceeded limit of %d s.', $maxDuration));
+			$message = sprintf('<fg=red>Feed generated in %.2F s, exceeding limit of %d s.</fg=red>', $realDuration, $maxDuration);
+			$failMessage = sprintf('Feed generation exceeded limit of %d s.', $maxDuration);
+			$performanceTestSample->addFailMessage($failMessage);
 		}
+
+		$performanceTestSample->setMessage($message);
 	}
 
 	/**
 	 * @param \SS6\ShopBundle\Model\Feed\FeedConfig $feedConfig
 	 * @param \SS6\ShopBundle\Component\Domain\Config\DomainConfig $domainConfig
-	 * @return float
+	 * @return \SS6\ShopBundle\Tests\Performance\Feed\PerformanceTestSample
 	 */
 	private function generateFeed(FeedConfig $feedConfig, DomainConfig $domainConfig) {
 		$client = $this->getClient(true, self::ADMIN_USERNAME, self::ADMIN_PASSWORD);
@@ -129,7 +156,7 @@ class AllFeedsTest extends FunctionalTestCase {
 			'feedName' => $feedConfig->getFeedName(),
 			'domainId' => $domainConfig->getId(),
 		];
-		$uri = $router->generate('admin_feed_generate', $feedGenerationParameters, RouterInterface::RELATIVE_PATH);
+		$uri = $router->generate(self::ROUTE_NAME_GENERATE_FEED, $feedGenerationParameters, RouterInterface::RELATIVE_PATH);
 
 		$clientEntityManager->beginTransaction();
 
@@ -139,13 +166,41 @@ class AllFeedsTest extends FunctionalTestCase {
 
 		$clientEntityManager->rollback();
 
-		$expectedStatusCode = 302;
+		$duration = $endTime - $startTime;
 		$statusCode = $client->getResponse()->getStatusCode();
+
+		$performanceTestSample = new PerformanceTestSample($feedConfig, $domainConfig, $uri, $duration, $statusCode);
+
+		$expectedStatusCode = 302;
 		if ($statusCode !== $expectedStatusCode) {
-			$this->fail(sprintf('Admin request on %s failed with status code %d, expected %d.', $uri, $statusCode, $expectedStatusCode));
+			$failMessage = sprintf('Admin request on %s ended with status code %d, expected %d.', $uri, $statusCode, $expectedStatusCode);
+			$performanceTestSample->addFailMessage($failMessage);
 		}
 
-		return $endTime - $startTime;
+		return $performanceTestSample;
+	}
+
+	/**
+	 * @param \SS6\ShopBundle\Tests\Performance\Feed\PerformanceTestSample[] $performanceTestSamples
+	 */
+	private function assertSamplesAreSuccessful(array $performanceTestSamples) {
+		$failMessages = [];
+
+		foreach ($performanceTestSamples as $performanceTestSample) {
+			if (!$performanceTestSample->isSuccessful()) {
+				$failMessages[] = sprintf(
+					'Generation of feed "%s" on domain with ID %d failed: %s',
+					$performanceTestSample->getFeedConfig()->getFeedName(),
+					$performanceTestSample->getDomainConfig()->getId(),
+					implode(' ', $performanceTestSample->getFailMessages())
+				);
+			}
+			$this->addToAssertionCount(1);
+		}
+
+		if (count($failMessages) > 0) {
+			$this->fail(implode("\n", $failMessages));
+		}
 	}
 
 }
