a0c0e3429f16984bdc1453668de9a82afc812f73
Petr Heinz <petr.heinz@shopsys.com>
2017-02-22 10:21:26 +0100

    Server-side validation errors have appropriate JS classes
    - same classes as added in Shopsys.validation.showErrors
    - prevents creating error duplicates in server-side and client-side validation

---
+++ project-base/src/Shopsys/ShopBundle/Twig/FormExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Shopsys\ShopBundle\Twig;
+
+use Symfony\Component\Form\FormError;
+use Twig_SimpleFunction;
+
+class FormExtension extends \Twig_Extension
+{
+    /**
+     * @return array
+     */
+    public function getFunctions()
+    {
+        return [
+            new Twig_SimpleFunction('getFormErrorSourceId', [$this, 'getFormErrorSourceId']),
+        ];
+    }
+
+    /**
+     * Creates source id of FormError equally as it is created in JS function FpJsFormValidator.validate
+     * @param \Symfony\Component\Form\FormError $formError
+     * @return string
+     */
+    public function getFormErrorSourceId(FormError $formError)
+    {
+        $form = $formError->getOrigin();
+        $sourceIdParts = [];
+        do {
+            $sourceIdParts[] = str_replace('_', '-', $form->getName());
+            $form = $form->getParent();
+        } while ($form !== null);
+
+        return 'form-error-' . implode('-', array_reverse($sourceIdParts));
+    }
+}
