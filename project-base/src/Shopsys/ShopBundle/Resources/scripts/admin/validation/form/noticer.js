5ffef79d8a62239d6c587d396de2d4aff8a9a770
Petr Heinz <petr.heinz@shopsys.com>
2017-02-27 15:01:44 +0100

    Fix JS for AdvertForm

--- project-base/src/Shopsys/ShopBundle/Resources/scripts/admin/validation/form/noticer.js
+++ project-base/src/Shopsys/ShopBundle/Resources/scripts/admin/validation/form/noticer.js
@@ -1,27 +1,33 @@
+// Script is named "noticer.js" because scripts named "advert.js" are often blocked by browser (e.g. by AdBlock plugin)
+
 (function ($){
     $(document).ready(function () {
         var $advertForm = $('form[name="advert_form"]');
 
-        $.fn.initAdvertType = function(){
-            var $checkedType = $(this).find('input[name="advert_form[type]"]:checked').val();
-            $(this).find('.js-advert-type-content').hide();
-            $(this).find('.js-advert-type-content[data-type=' + $checkedType + ']').show();
+        var getCheckedType = function () {
+            return $advertForm.find('input[name="advert_form[type]"]:checked').val();
         };
 
-        $advertForm.initAdvertType();
-        $advertForm.find('input[name="advert_form[type]"]').on('change',function(){
-            $advertForm.initAdvertType();
-        });
+        var initAdvertForm = function () {
+            $advertForm
+                .find('.js-advert-type-content').hide()
+                .filter('[data-type=' + getCheckedType() + ']').show();
+        };
+
+        $advertForm.find('input[name="advert_form[type]"]').change(initAdvertForm);
+        initAdvertForm();
 
         $advertForm.jsFormValidator({
             'groups': function () {
                 var groups = [Shopsys.constant('\\Shopsys\\ShopBundle\\Form\\ValidationGroup::VALIDATION_GROUP_DEFAULT')];
 
-                if ($('input[name="advert[type]"]:checked').val() === Shopsys.constant('\\Shopsys\\ShopBundle\\Model\\Advert\\Advert::TYPE_CODE')) {
+                var checkedType = getCheckedType();
+                if (checkedType === Shopsys.constant('\\Shopsys\\ShopBundle\\Model\\Advert\\Advert::TYPE_CODE')) {
                     groups.push(Shopsys.constant('\\Shopsys\\ShopBundle\\Form\\Admin\\Advert\\AdvertFormType::VALIDATION_GROUP_TYPE_CODE'));
-                } else if ($('input[name="advert[type]"]:checked').val() === Shopsys.constant('\\Shopsys\\ShopBundle\\Model\\Advert\\Advert::TYPE_IMAGE')) {
+                } else if (checkedType === Shopsys.constant('\\Shopsys\\ShopBundle\\Model\\Advert\\Advert::TYPE_IMAGE')) {
                     groups.push(Shopsys.constant('\\Shopsys\\ShopBundle\\Form\\Admin\\Advert\\AdvertFormType::VALIDATION_GROUP_TYPE_IMAGE'));
                 }
+
                 return groups;
             }
         });
