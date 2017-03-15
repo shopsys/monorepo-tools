546359cf32779b30f594719ef22b16baf669e476
Petr Heinz <petr.heinz@shopsys.com>
2017-03-15 08:46:22 +0100

    Simplification of SimpleBreadcrumbGenerator as it is basically only a mapping

--- project-base/src/Shopsys/ShopBundle/Model/Breadcrumb/SimpleBreadcrumbGenerator.php
+++ project-base/src/Shopsys/ShopBundle/Model/Breadcrumb/SimpleBreadcrumbGenerator.php
@@ -11,48 +11,22 @@ use Shopsys\ShopBundle\Component\Breadcrumb\BreadcrumbItem;
 class SimpleBreadcrumbGenerator implements BreadcrumbGeneratorInterface
 {
     /**
+     * @var string[]|null
+     */
+    private $routeNameMap;
+
+    /**
      * @param string $routeName
      * @param array $routeParameters
      * @return \Shopsys\ShopBundle\Component\Breadcrumb\BreadcrumbItem[]
-     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
      */
     public function getBreadcrumbItems($routeName, array $routeParameters = [])
     {
-        switch ($routeName) {
-            case 'front_customer_edit':
-                return [
-                    new BreadcrumbItem(t('Edit data')),
-                ];
-            case 'front_customer_orders':
-                return [
-                    new BreadcrumbItem(t('Orders')),
-                ];
-            case 'front_registration_reset_password':
-                return [
-                    new BreadcrumbItem(t('Forgotten password')),
-                ];
-            case 'front_customer_order_detail_registered':
-            case 'front_customer_order_detail_unregistered':
-                return [
-                    new BreadcrumbItem(t('Order detail')),
-                ];
-            case 'front_login':
-                return [
-                    new BreadcrumbItem(t('Login')),
-                ];
-            case 'front_product_search':
-                return [
-                    new BreadcrumbItem(t('Search [noun]')),
-                ];
-            case 'front_registration_register':
-                return [
-                    new BreadcrumbItem(t('Registration')),
-                ];
-            case 'front_brand_list':
-                return [
-                    new BreadcrumbItem(t('Brand overview')),
-                ];
-        }
+        $routeNameMap = $this->getRouteNameMap();
+
+        return [
+            new BreadcrumbItem($routeNameMap[$routeName]),
+        ];
     }
 
     /**
@@ -60,17 +34,29 @@ class SimpleBreadcrumbGenerator implements BreadcrumbGeneratorInterface
      */
     public function getRouteNames()
     {
-        return [
-            'front_customer_edit',
-            'front_customer_orders',
-            'front_registration_reset_password',
-            'front_customer_order_detail_registered',
-            'front_customer_order_detail_unregistered',
-            'front_login',
-            'front_product_search',
-            'front_registration_register',
-            'front_brand_list',
-            'front_customer_edit',
-        ];
+        return array_keys($this->getRouteNameMap());
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getRouteNameMap()
+    {
+        if ($this->routeNameMap === null) {
+            // Caching in order to translate breadcrumb item names only once
+            $this->routeNameMap = [
+                'front_customer_edit' => t('Edit data'),
+                'front_customer_orders' => t('Orders'),
+                'front_registration_reset_password' => t('Forgotten password'),
+                'front_customer_order_detail_registered' => t('Order detail'),
+                'front_customer_order_detail_unregistered' => t('Order detail'),
+                'front_login' => t('Login'),
+                'front_product_search' => t('Search [noun]'),
+                'front_registration_register' => t('Registration'),
+                'front_brand_list' => t('Brand overview'),
+            ];
+        }
+
+        return $this->routeNameMap;
     }
 }
