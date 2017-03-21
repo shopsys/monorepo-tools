6536a73dfd304910732202531a51f5fb3232ee7d
Petr Heinz <petr.heinz@shopsys.com>
2017-03-21 11:27:21 +0100

    Fix deprecation: OrderStatusFilter - The default value "false" for the "choices_as_values" option of Symfony\Component\Form\Extension\Core\Type\ChoiceType is deprecated since Symfony 2.8 and will not be supported anymore in 3.0. Setting this option to "true" and passing property path for label and value instead.
    - dependency on repository replaced by dependency on facade
    - o.status is compared to OrderStatus instance instead of its ID

--- project-base/src/Shopsys/ShopBundle/Model/AdvancedSearchOrder/Filter/OrderStatusFilter.php
+++ project-base/src/Shopsys/ShopBundle/Model/AdvancedSearchOrder/Filter/OrderStatusFilter.php
@@ -4,16 +4,19 @@ namespace Shopsys\ShopBundle\Model\AdvancedSearchOrder\Filter;
 
 use Doctrine\ORM\QueryBuilder;
 use Shopsys\ShopBundle\Model\AdvancedSearch\AdvancedSearchFilterInterface;
-use Shopsys\ShopBundle\Model\Order\Status\OrderStatusRepository;
+use Shopsys\ShopBundle\Model\Order\Status\OrderStatusFacade;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class OrderStatusFilter implements AdvancedSearchFilterInterface
 {
-    private $orderStatusRepository;
+    /**
+     * @var \Shopsys\ShopBundle\Model\Order\Status\OrderStatusFacade
+     */
+    private $orderStatusFacade;
 
-    public function __construct(OrderStatusRepository $orderStatusRepository)
+    public function __construct(OrderStatusFacade $orderStatusFacade)
     {
-        $this->orderStatusRepository = $orderStatusRepository;
+        $this->orderStatusFacade = $orderStatusFacade;
     }
 
     /**
@@ -48,14 +51,11 @@ class OrderStatusFilter implements AdvancedSearchFilterInterface
      */
     public function getValueFormOptions()
     {
-        $orderStatusChoices = [];
-        foreach ($this->orderStatusRepository->getAll() as $orderStatus) {
-            /* @var $orderStatus \Shopsys\ShopBundle\Model\Order\Status\OrderStatus */
-            $orderStatusChoices[$orderStatus->getId()] = $orderStatus->getName();
-        }
-
         return [
-            'choices' => $orderStatusChoices,
+            'choices' => $this->orderStatusFacade->getAll(),
+            'choice_name' => 'name',
+            'choice_value' => 'id',
+            'choices_as_values' => true, // Switches to Symfony 3 choice mode, remove after upgrade from 2.8
             'expanded' => false,
             'multiple' => false,
         ];
