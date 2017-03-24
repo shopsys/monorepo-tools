c583c1d7a6a2cd96ba720b96720806209b1579c6
Petr Heinz <petr.heinz@shopsys.com>
2017-03-24 08:14:33 +0100

    annotation typo
    - format /* @var <variable name> <fully qualified type> */ is commonly used for inline @var annotations

--- project-base/src/Shopsys/ShopBundle/Form/Admin/Module/ModulesFormType.php
+++ project-base/src/Shopsys/ShopBundle/Form/Admin/Module/ModulesFormType.php
@@ -23,7 +23,7 @@ class ModulesFormType extends AbstractType
             ->add('save', SubmitType::class);
 
         $moduleList = $options['module_list'];
-        /** @var \Shopsys\ShopBundle\Model\Module\ModuleList $moduleList */
+        /* @var $moduleList \Shopsys\ShopBundle\Model\Module\ModuleList */
         foreach ($moduleList->getNamesIndexedByLabel() as $moduleLabel => $moduleName) {
             $builder->get('modules')
                 ->add($moduleName, YesNoType::class, [
