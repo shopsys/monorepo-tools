4f316d5ff444035d80f0ea37cd526235e5aa0651
Petr Heinz <petr.heinz@shopsys.com>
2017-06-07 10:26:14 +0200

    [BG-1322] fix multi-domain shop info setting demo data fixture
    - data fixtures in \Shopsys\ShopBundle\DataFixtures\Demo are executed with only 1 configured domain
    - after data fixtures other domains are configured and setting values are copied
    - \Shopsys\ShopBundle\DataFixtures\DemoMultidomain\SettingValueShopInfoDataFixture now just overwrites the copied values by localized values for the second domain
    - demo data is hard-coded and independent on configured locale (for simplicity and consistency with other fixtures)

--- project-base/src/Shopsys/ShopBundle/DataFixtures/DemoMultidomain/SettingValueShopInfoDataFixture.php
+++ project-base/src/Shopsys/ShopBundle/DataFixtures/DemoMultidomain/SettingValueShopInfoDataFixture.php
@@ -1,18 +1,17 @@
 <?php
 
-namespace Shopsys\ShopBundle\DataFixtures\Demo;
+namespace Shopsys\ShopBundle\DataFixtures\DemoMultidomain;
 
 use Doctrine\Common\Persistence\ObjectManager;
 use Shopsys\ShopBundle\Component\DataFixture\AbstractReferenceFixture;
-use Shopsys\ShopBundle\Component\Domain\Domain;
 use Shopsys\ShopBundle\Model\ShopInfo\ShopInfoSettingFacade;
 
 class SettingValueShopInfoDataFixture extends AbstractReferenceFixture
 {
     const SETTING_VALUES = [
-        ShopInfoSettingFacade::SHOP_INFO_PHONE_NUMBER => '+1-234-567-8989',
-        ShopInfoSettingFacade::SHOP_INFO_PHONE_HOURS => '(Mon - Sat: 9 - 10 a.m. to 8 - 10 p.m.)',
-        ShopInfoSettingFacade::SHOP_INFO_EMAIL => 'no-reply@shopsys.com',
+        ShopInfoSettingFacade::SHOP_INFO_PHONE_NUMBER => '+420123456789',
+        ShopInfoSettingFacade::SHOP_INFO_PHONE_HOURS => '(po-pá, 10:00 - 16:00)',
+        ShopInfoSettingFacade::SHOP_INFO_EMAIL => 'no-reply@shopsys.cz',
     ];
 
     /**
@@ -28,8 +27,9 @@ class SettingValueShopInfoDataFixture extends AbstractReferenceFixture
         // As EM identity map is cleared after each fixture we should clear the Setting cache before editing the values.
         $setting->clearCache();
 
+        $domainId = 2;
         foreach (self::SETTING_VALUES as $key => $value) {
-            $setting->setForDomain($key, $value, Domain::FIRST_DOMAIN_ID);
+            $setting->setForDomain($key, $value, $domainId);
         }
     }
 }
