a4c89b65c296721c892fbf39187519dea6af7e05
Matouš Czerner <czerner@shopsys.cz>
2017-06-09 16:00:20 +0200

    Custom Translation section in admin is deleted
    - done because its not used anymore
    - URL addresses admin page is now situated in superadmin section
    - whole Translation model was deleted, because it was only used for custom translations
    - translations/custom folder was deleted since it has no usage now

--- project-base/src/Shopsys/ShopBundle/Model/Localization/Translation/Grid/Exception/TranslationGridException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Shopsys\ShopBundle\Model\Localization\Translation\Grid\Exception;
-
-use Shopsys\ShopBundle\Model\Localization\Exception\LocalizationException;
-
-interface TranslationGridException extends LocalizationException
-{
-}
