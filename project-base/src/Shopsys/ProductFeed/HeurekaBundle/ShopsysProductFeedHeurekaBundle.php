1a98acc8db561fd27840703cd955e20edda8a66e
Petr Heinz <petr.heinz@shopsys.com>
2017-07-13 13:59:48 +0200

    Extracted interface of product feed plugin along with Heureka and Zboží.cz implementations into separate packages
    - Product feed plugin interface: shopsys/product-feed-interface
    - Heureka implementation: shopsys/product-feed-heureka
    - Zboží.cz implementation: shopsys/product-feed-zbozi

--- project-base/src/Shopsys/ProductFeed/HeurekaBundle/ShopsysProductFeedHeurekaBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Shopsys\ProductFeed\HeurekaBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ShopsysProductFeedHeurekaBundle extends Bundle
-{
-}
--- project-base/src/Shopsys/ProductFeed/ZboziBundle/ShopsysProductFeedZboziBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Shopsys\ProductFeed\ZboziBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ShopsysProductFeedZboziBundle extends Bundle
-{
-}
