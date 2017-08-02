bbf3b8a7ed0d267d738fe38c5993646e73dd5390
Petr Heinz <petr.heinz@shopsys.com>
2017-08-02 14:06:53 +0200

    implemented DataStorageInterface and PluginDataStorageProviderInterface
    - implementations use Doctrine as data storage
    - new entity PluginDataValue that represents plugin custom data record

---
+++ project-base/src/Shopsys/ShopBundle/Migrations/Version20170802141735.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Shopsys\ShopBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use PDO;
+use ShopSys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20170802141735 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->sql(
+            'CREATE TABLE plugin_data_values (
+                plugin_name VARCHAR(255) NOT NULL,
+                context VARCHAR(255) NOT NULL,
+                key VARCHAR(255) NOT NULL,
+                json_value TEXT NOT NULL,
+                PRIMARY KEY(plugin_name, context, key)
+            )'
+        );
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+    }
+}
