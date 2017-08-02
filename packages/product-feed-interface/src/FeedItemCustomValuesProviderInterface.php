c734aef2b348113b62452cbfa99c99905ba42060
Petr Heinz <petr.heinz@shopsys.com>
2017-08-02 08:31:23 +0200

    FeedItemCustomValuesProviderInterface: removed method getCustomValuesForItems()
    - functionality extracted into separate package shopsys/plugin-interface

--- packages/product-feed-interface/src/FeedItemCustomValuesProviderInterface.php
+++ packages/product-feed-interface/src/FeedItemCustomValuesProviderInterface.php
@@ -5,13 +5,6 @@ namespace Shopsys\ProductFeed;
 interface FeedItemCustomValuesProviderInterface
 {
     /**
-     * @param \Shopsys\ProductFeed\FeedItemInterface[] $items
-     * @param \Shopsys\ProductFeed\DomainConfigInterface $domainConfig
-     * @return \Shopsys\ProductFeed\FeedItemCustomValuesInterface[]
-     */
-    public function getCustomValuesForItems(array $items, DomainConfigInterface $domainConfig);
-
-    /**
      * @param \Shopsys\ProductFeed\FeedItemInterface $item
      * @param \Shopsys\ProductFeed\DomainConfigInterface $domainConfig
      * @return string|null
