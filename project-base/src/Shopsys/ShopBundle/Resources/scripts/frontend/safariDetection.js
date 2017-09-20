9aad2544c364645a1a78e68e1dc6b7174352735c
Peter Turek <peter.turek@shopsys.com>
2017-09-20 15:41:44 +0200

    ESLint fix: Infix operators must be spaced

--- project-base/src/Shopsys/ShopBundle/Resources/scripts/frontend/safariDetection.js
+++ project-base/src/Shopsys/ShopBundle/Resources/scripts/frontend/safariDetection.js
@@ -2,7 +2,7 @@ if (navigator.vendor
     && navigator.vendor.indexOf('Apple') > -1
     && navigator.userAgent
     && !navigator.userAgent.match('CriOS')) {
-        document.getElementsByTagName('html')[0].className+=' is-safari'
+        document.getElementsByTagName('html')[0].className += ' is-safari'
 } else {
-        document.getElementsByTagName('html')[0].className+=' is-no-safari'
+        document.getElementsByTagName('html')[0].className += ' is-no-safari'
 }
