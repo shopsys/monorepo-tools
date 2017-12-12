733c4ebf292f284240643a6a24d946907b9a4a40
Jakub Dolba <jakub.dolba@shopsys.com>
2017-12-12 15:24:14 +0100

    maintenance page - wrong HTTP header spelling caused Server Internal Error  - on apache server, this "typo" cause HTTP 500 error (typically during deployment process with DB Migrations)  - nginx and other servers ignore this wrong header silently, header was not present in response

--- project-base/app/maintenance.php
+++ project-base/app/maintenance.php
@@ -3,7 +3,7 @@
     header('Status: 503 Service Temporarily Unavailable');
     header('Retry-after: 300');
     header('Expires: Thu, 01 Dec 1994 16:00:00 GMT');
-    header('Last modified: ' . gmdate('D, d M Y H:i:s', time()) . ' GMT');
+    header('Last-Modified: ' . gmdate('D, d M Y H:i:s', time()) . ' GMT');
     header('Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate');
 ?>
 <!DOCTYPE html>
