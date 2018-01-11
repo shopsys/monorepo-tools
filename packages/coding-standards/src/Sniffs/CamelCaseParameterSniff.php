38f1909c8b80d907f8c36ea310f41211d6454a65
TomasVotruba <tomas.vot@gmail.com>
2018-01-11 20:06:21 +0100

    remove fix ability from CamelCaseParameterSniff, not working propperly

--- packages/coding-standards/src/Sniffs/CamelCaseParameterSniff.php
+++ packages/coding-standards/src/Sniffs/CamelCaseParameterSniff.php
@@ -11,11 +11,6 @@ use PHP_CodeSniffer\Util\Common;
 final class CamelCaseParameterSniff extends AbstractVariableSniff
 {
     /**
-     * @var string
-     */
-    private const NOT_LETTER_THEN_LETTER_PATTERN = '([^a-zA-Z]{1}(?<nextLetter>[A-Za-z]{1}))';
-
-    /**
      * @param int $position
      */
     protected function processVariable(File $file, $position): void
@@ -27,14 +22,10 @@ final class CamelCaseParameterSniff extends AbstractVariableSniff
             return;
         }
 
-        $fix = $file->addFixableError(sprintf(
+        $file->addError(sprintf(
             'Variable "$%s" should be camel case',
             $variableName
         ), $position, self::class);
-
-        if ($fix) {
-            $this->fixVariableName($file, $position, $variableName);
-        }
     }
 
     /**
@@ -49,28 +40,5 @@ final class CamelCaseParameterSniff extends AbstractVariableSniff
      */
     protected function processMemberVar(File $file, $position): void
     {
-    }
-
-    /**
-     * Changes:
-     * _someVariable => someVariable
-     * some_variable => someVariable
-     */
-    private function fixVariableName(File $file, int $position, string $variableName): void
-    {
-        $newVariableName = preg_replace_callback(
-            '#(?<prefixLetter>[a-z]{1})?' . self::NOT_LETTER_THEN_LETTER_PATTERN . '#',
-            function (array $match): string {
-                $isFirstLetter = !(bool)$match['prefixLetter'];
-                if ($isFirstLetter) {
-                    return $match['nextLetter'];
-                }
-
-                return $match['prefixLetter'] . strtoupper($match['nextLetter']);
-            },
-            $variableName
-        );
-
-        $file->fixer->replaceToken($position, '$' . $newVariableName);
     }
 }
