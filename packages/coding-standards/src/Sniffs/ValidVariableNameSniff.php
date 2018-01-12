8b4fe64c78cf1b880e5d461122d4eb178979841d
Rostislav Vitek <rostislav.vitek@shopsys.com>
2018-01-12 18:13:37 +0100

    custom ValidVariableNameSniff (renamed from CamelCaseParameterSniff) now provides functionality which is missing from default ValidVariableNameSniff
    - method parameters should not start with underscore
    - private class members are checked for camelCase format even when PrivateNoUnderscore property is disabled for default ValidVariableNameSniff

---
+++ packages/coding-standards/src/Sniffs/ValidVariableNameSniff.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ShopSys\CodingStandards\Sniffs;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\AbstractVariableSniff;
+use PHP_CodeSniffer\Util\Common;
+
+/*
+ * \PHP_CodeSniffer\Standards\Squiz\Sniffs\NamingConventions\ValidVariableNameSniff
+ * does not report method parameters in $_var format as an violation but it should.
+ * It also skips checking of private members when PrivateNoUnderscore property is disabled.
+ *
+ * see https://github.com/squizlabs/PHP_CodeSniffer/issues/1851
+ *
+ * This sniff provides the missing functionality and is intended to be used as an addition to the sniff mentioned above.
+ */
+final class ValidVariableNameSniff extends AbstractVariableSniff
+{
+    /**
+     * @param int $position
+     */
+    protected function processVariable(File $file, $position): void
+    {
+        $errorMessageFormat = 'Variable "$%s" should be camel case';
+        $this->checkCamelCaseFormatViolation($file, $position, $errorMessageFormat);
+    }
+
+    /**
+     * @param int $position
+     */
+    protected function processVariableInString(File $file, $position): void
+    {
+    }
+
+    /**
+     * @param int $position
+     */
+    protected function processMemberVar(File $file, $position): void
+    {
+        $errorMessageFormat = 'Class member variable "$%s" should be camel case';
+        $this->checkCamelCaseFormatViolation($file, $position, $errorMessageFormat);
+    }
+
+    private function checkCamelCaseFormatViolation(File $file, int $position, string $errorMessageFormat): void
+    {
+        $currentToken = $file->getTokens()[$position];
+
+        $variableName = ltrim($currentToken['content'], '$');
+        if (Common::isCamelCaps($variableName)) {
+            return;
+        }
+
+        $file->addError(sprintf(
+            $errorMessageFormat,
+            $variableName
+        ), $position, self::class);
+    }
+}
