cea9c76b19a9dc793dc6c0988b536990405d7e4e
Matous Czerner <matous.czerner@shopsys.com>
2018-03-05 17:44:03 +0100

    InvalidServiceExcpetion: deleted property $serviceName since there is no reason to use it

--- packages/framework/src/Component/Grid/InlineEdit/Exception/InvalidServiceException.php
+++ packages/framework/src/Component/Grid/InlineEdit/Exception/InvalidServiceException.php
@@ -7,26 +7,12 @@ use Exception;
 class InvalidServiceException extends Exception implements InlineEditException
 {
     /**
-     * @var string
-     */
-    private $serviceName;
-
-    /**
      * @param string $serviceName
      * @param \Exception|null $previous
      */
     public function __construct($serviceName, Exception $previous = null)
     {
-        $this->serviceName = $serviceName;
-        $message = 'Service with name "' . $this->serviceName . '" does not exists or not implement necessary interface.';
+        $message = 'Service with name "' . $serviceName . '" does not exist or not implement necessary interface.';
         parent::__construct($message, 0, $previous);
-    }
-
-    /**
-     * @return string
-     */
-    public function getServiceName()
-    {
-        return $this->serviceName;
     }
 }
