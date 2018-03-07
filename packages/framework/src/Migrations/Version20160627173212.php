6307586c54f9093ffe3b9b4e1959f58fb097f440
Tomas Ludvik <tomas.ludvik@shopsys.com>
2018-03-07 14:53:03 +0100

    all framework migrations are now part of framework bundle

---
+++ packages/framework/src/Migrations/Version20160627173212.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use ShopSys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20160627173212 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->sql('CREATE TABLE newsletter_subscribers (email VARCHAR(255) NOT NULL, PRIMARY KEY(email));');
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+    }
+}
---
+++ packages/framework/src/Migrations/Version20161111124254.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use ShopSys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
+
+class Version20161111124254 extends AbstractMigration
+{
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->sql('ALTER TABLE cart_items RENAME COLUMN session_id TO cart_identifier');
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+    }
+}
