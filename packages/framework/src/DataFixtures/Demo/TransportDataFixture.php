f590f91bc7eb25fff1011846b2ec7e4d5df582b5
Petr Heinz <petr.heinz@shopsys.com>
2018-04-12 14:10:38 +0200

    TransportDataFixture: improve demo data

--- packages/framework/src/DataFixtures/Demo/TransportDataFixture.php
+++ packages/framework/src/DataFixtures/Demo/TransportDataFixture.php
@@ -43,19 +43,11 @@ class TransportDataFixture extends AbstractReferenceFixture implements Dependent
             $this->getReference(CurrencyDataFixture::CURRENCY_CZK)->getId() => 99.95,
             $this->getReference(CurrencyDataFixture::CURRENCY_EUR)->getId() => 3.95,
         ];
-        $transportEditData->transportData->description = [
-            'cs' => 'Pouze na vlastní nebezpečí',
-            'en' => 'Only if you are crazy',
-        ];
-        $transportEditData->transportData->instructions = [
-            'cs' => '<b>Pozor!</b> Česká pošta pouze na vlastní nebezpečí.',
-            'en' => '<b>Warning!</b> Use Czech Post only if you are crazy.',
-        ];
         $transportEditData->transportData->vat = $this->getReference(VatDataFixture::VAT_HIGH);
         $transportEditData->transportData->domains = [Domain::FIRST_DOMAIN_ID];
-        $transportEditData->transportData->hidden = false;
         $this->createTransport(self::TRANSPORT_CZECH_POST, $transportEditData);
 
+        $transportEditData = new TransportEditData();
         $transportEditData->transportData->name = [
             'cs' => 'PPL',
             'en' => 'PPL',
@@ -64,13 +56,11 @@ class TransportDataFixture extends AbstractReferenceFixture implements Dependent
             $this->getReference(CurrencyDataFixture::CURRENCY_CZK)->getId() => 199.95,
             $this->getReference(CurrencyDataFixture::CURRENCY_EUR)->getId() => 6.95,
         ];
-        $transportEditData->transportData->description = [
-            'cs' => null,
-            'en' => null,
-        ];
-        $transportEditData->transportData->instructions = [];
+        $transportEditData->transportData->vat = $this->getReference(VatDataFixture::VAT_HIGH);
+        $transportEditData->transportData->domains = [Domain::FIRST_DOMAIN_ID];
         $this->createTransport(self::TRANSPORT_PPL, $transportEditData);
 
+        $transportEditData = new TransportEditData();
         $transportEditData->transportData->name = [
             'cs' => 'Osobní převzetí',
             'en' => 'Personal collection',
@@ -81,9 +71,14 @@ class TransportDataFixture extends AbstractReferenceFixture implements Dependent
         ];
         $transportEditData->transportData->description = [
             'cs' => 'Uvítá Vás milý personál!',
-            'en' => 'You will be welcomed friendly staff!',
+            'en' => 'You will be welcomed by friendly staff!',
+        ];
+        $transportEditData->transportData->instructions = [
+            'cs' => 'Těšíme se na Vaši návštěvu.',
+            'en' => 'We are looking forward to your visit.',
         ];
         $transportEditData->transportData->vat = $this->getReference(VatDataFixture::VAT_ZERO);
+        $transportEditData->transportData->domains = [Domain::FIRST_DOMAIN_ID];
         $this->createTransport(self::TRANSPORT_PERSONAL, $transportEditData);
     }
 
