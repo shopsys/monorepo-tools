7e1b62565cf9c23ac28460baf15bf9f440d42297
Svaťa Šimara <svatopluk.simara@shopsys.com>
2018-04-30 09:53:23 +0200

    refactored Administrator entity to factory

---
+++ packages/framework/src/Model/Administrator/AdministratorFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Model\Administrator;
+
+class AdministratorFactory implements AdministratorFactoryInterface
+{
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Administrator\AdministratorData $data
+     * @return \Shopsys\FrameworkBundle\Model\Administrator\Administrator
+     */
+    public function create(AdministratorData $data): Administrator
+    {
+        return new Administrator($data);
+    }
+}
---
+++ packages/framework/src/Model/Administrator/AdministratorFactoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Model\Administrator;
+
+interface AdministratorFactoryInterface
+{
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Administrator\AdministratorData $data
+     * @return \Shopsys\FrameworkBundle\Model\Administrator\Administrator
+     */
+    public function create(AdministratorData $data): Administrator;
+}
