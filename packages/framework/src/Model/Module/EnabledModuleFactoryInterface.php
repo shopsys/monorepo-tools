8331a7409646c7b2d91bc5c62613d8ca8fc9cbf4
Svaťa Šimara <svatopluk.simara@shopsys.com>
2018-05-02 12:26:53 +0200

    refactored EnabledModule entity to factory

---
+++ packages/framework/src/Model/Module/EnabledModuleFactoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Model\Module;
+
+interface EnabledModuleFactoryInterface
+{
+
+    /**
+     * @param string $name
+     * @return \Shopsys\FrameworkBundle\Model\Module\EnabledModule
+     */
+    public function create(string $name): EnabledModule;
+}
