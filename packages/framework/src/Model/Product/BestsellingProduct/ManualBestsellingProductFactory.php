02de8bdbee3eb18d4c17390d2620e90e87a25aba
Svaťa Šimara <svatopluk.simara@shopsys.com>
2018-05-02 13:50:52 +0200

    refactored ManualBestsellingProduct entity to factory

---
+++ packages/framework/src/Model/Product/BestsellingProduct/ManualBestsellingProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Model\Product\BestsellingProduct;
+
+use Shopsys\FrameworkBundle\Model\Category\Category;
+use Shopsys\FrameworkBundle\Model\Product\Product;
+
+class ManualBestsellingProductFactory implements ManualBestsellingProductFactoryInterface
+{
+
+    /**
+     * @param int $domainId
+     * @param \Shopsys\FrameworkBundle\Model\Category\Category $category
+     * @param \Shopsys\FrameworkBundle\Model\Product\Product $product
+     * @param int $position
+     * @return \Shopsys\FrameworkBundle\Model\Product\BestsellingProduct\ManualBestsellingProduct
+     */
+    public function create(
+        int $domainId,
+        Category $category,
+        Product $product,
+        int $position
+    ): ManualBestsellingProduct {
+        return new ManualBestsellingProduct($domainId, $category, $product, $position);
+    }
+}
