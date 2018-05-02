9492772f78b98942c3bb03bf11be3a3ec733fd7b
Svaťa Šimara <svatopluk.simara@shopsys.com>
2018-05-02 16:23:22 +0200

    refactored OrderStatus entity to factory

---
+++ packages/framework/src/Model/Order/Status/OrderStatusFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Model\Order\Status;
+
+class OrderStatusFactory implements OrderStatusFactoryInterface
+{
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Order\Status\OrderStatusData $data
+     * @param int $type
+     * @return \Shopsys\FrameworkBundle\Model\Order\Status\OrderStatus
+     */
+    public function create(OrderStatusData $data, int $type): OrderStatus
+    {
+        return new OrderStatus($data, $type);
+    }
+}
---
+++ packages/framework/src/Model/Order/Status/OrderStatusFactoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Model\Order\Status;
+
+interface OrderStatusFactoryInterface
+{
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Order\Status\OrderStatusData $data
+     * @param int $type
+     * @return \Shopsys\FrameworkBundle\Model\Order\Status\OrderStatus
+     */
+    public function create(OrderStatusData $data, int $type): OrderStatus;
+}
