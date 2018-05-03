a0ed258e8e500db778bacbb1fcd366187d02596f
Svaťa Šimara <svatopluk.simara@shopsys.com>
2018-05-03 12:29:43 +0200

    refactored PromoCode entity to factory

---
+++ packages/framework/src/Model/Order/PromoCode/PromoCodeFactoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Model\Order\PromoCode;
+
+interface PromoCodeFactoryInterface
+{
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Order\PromoCode\PromoCodeData $data
+     * @return \Shopsys\FrameworkBundle\Model\Order\PromoCode\PromoCode
+     */
+    public function create(PromoCodeData $data): PromoCode;
+}
