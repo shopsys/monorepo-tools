4eb0effa5ce9e37f8cf76c2b831ff9f406638c75
Svaťa Šimara <svatopluk.simara@shopsys.com>
2018-05-03 12:34:42 +0200

    refactored Script entity to factory

---
+++ packages/framework/src/Model/Script/ScriptFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Model\Script;
+
+class ScriptFactory implements ScriptFactoryInterface
+{
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Script\ScriptData $data
+     * @return \Shopsys\FrameworkBundle\Model\Script\Script
+     */
+    public function create(ScriptData $data): Script
+    {
+        return new Script($data);
+    }
+}
---
+++ packages/framework/src/Model/Script/ScriptFactoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Model\Script;
+
+interface ScriptFactoryInterface
+{
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Script\ScriptData $data
+     * @return \Shopsys\FrameworkBundle\Model\Script\Script
+     */
+    public function create(ScriptData $data): Script;
+}
