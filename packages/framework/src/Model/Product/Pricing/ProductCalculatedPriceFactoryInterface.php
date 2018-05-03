d63aa49633aaf61fded3c7713da4a0f10899c0d3
Svaťa Šimara <svatopluk.simara@shopsys.com>
2018-05-03 10:19:06 +0200

    refactored ProductCalculatedPrice entity to factory

---
+++ packages/framework/src/Model/Product/Pricing/ProductCalculatedPriceFactoryInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Model\Product\Pricing;
+
+use Shopsys\FrameworkBundle\Model\Pricing\Group\PricingGroup;
+use Shopsys\FrameworkBundle\Model\Product\Product;
+
+interface ProductCalculatedPriceFactoryInterface
+{
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Product\Product $product
+     * @param \Shopsys\FrameworkBundle\Model\Pricing\Group\PricingGroup $pricingGroup
+     * @param string|null $priceWithVat
+     * @return \Shopsys\FrameworkBundle\Model\Product\Pricing\ProductCalculatedPrice
+     */
+    public function create(
+        Product $product,
+        PricingGroup $pricingGroup,
+        ?string $priceWithVat
+    ): ProductCalculatedPrice;
+}
