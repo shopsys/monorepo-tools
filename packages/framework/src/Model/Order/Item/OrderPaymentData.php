fcb54804515a587493b25fd5348e0c8b20f5e69e
Svaťa Šimara <svatopluk.simara@shopsys.com>
2018-06-22 11:09:07 +0200

    OrderPaymentData: added "|null" to parameters

--- packages/framework/src/Model/Order/Item/OrderPaymentData.php
+++ packages/framework/src/Model/Order/Item/OrderPaymentData.php
@@ -5,7 +5,7 @@ namespace Shopsys\FrameworkBundle\Model\Order\Item;
 class OrderPaymentData extends OrderItemData
 {
     /**
-     * @var \Shopsys\FrameworkBundle\Model\Payment\Payment
+     * @var \Shopsys\FrameworkBundle\Model\Payment\Payment|null
      */
     public $payment;
 
