8dee6cd9c113cb784d3f16e233b5ccb9734d78f0
Svaťa Šimara <svatopluk.simara@shopsys.com>
2018-06-22 11:09:26 +0200

    OrderTransportData: added "|null" to parameters

--- packages/framework/src/Model/Order/Item/OrderTransportData.php
+++ packages/framework/src/Model/Order/Item/OrderTransportData.php
@@ -5,7 +5,7 @@ namespace Shopsys\FrameworkBundle\Model\Order\Item;
 class OrderTransportData extends OrderItemData
 {
     /**
-     * @var \Shopsys\FrameworkBundle\Model\Transport\Transport
+     * @var \Shopsys\FrameworkBundle\Model\Transport\Transport|null
      */
     public $transport;
 
