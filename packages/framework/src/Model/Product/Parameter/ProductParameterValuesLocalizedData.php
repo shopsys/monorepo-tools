07b65b6f27e20714016ec78b81c74474475b5565
Svaťa Šimara <svatopluk.simara@shopsys.com>
2018-06-25 09:13:50 +0200

    ProductParameterValuesLocalizedData: added "|null" to parameters phpdoc, initialized array parameter

--- packages/framework/src/Model/Product/Parameter/ProductParameterValuesLocalizedData.php
+++ packages/framework/src/Model/Product/Parameter/ProductParameterValuesLocalizedData.php
@@ -5,12 +5,12 @@ namespace Shopsys\FrameworkBundle\Model\Product\Parameter;
 class ProductParameterValuesLocalizedData
 {
     /**
-     * @var \Shopsys\FrameworkBundle\Model\Product\Parameter\Parameter
+     * @var \Shopsys\FrameworkBundle\Model\Product\Parameter\Parameter|null
      */
     public $parameter;
 
     /**
      * @var string[]
      */
-    public $valueTextsByLocale;
+    public $valueTextsByLocale = [];
 }
