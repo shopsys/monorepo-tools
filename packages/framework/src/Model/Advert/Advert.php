e8c78f0ad7edc4f56a02c74b2a49e6c079d04811
Boris Brtáň <boris.brtan@shopsys.com>
2018-08-13 11:13:47 +0200

    fixer PhpdocAnnotationWithoutDotFixer is applied

--- packages/framework/src/Model/Advert/Advert.php
+++ packages/framework/src/Model/Advert/Advert.php
@@ -14,7 +14,7 @@ class Advert
     const TYPE_CODE = 'code';
 
     /**
-     * @deprecated Use of position constants is discouraged, use literal strings instead.
+     * @deprecated use of position constants is discouraged, use literal strings instead
      */
     const POSITION_HEADER = 'header';
     const POSITION_FOOTER = 'footer';
