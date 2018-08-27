6e07428a855d45cde2a317330f06b8a4f7a5170d
Rostislav Vitek <rostislav.vitek@shopsys.com>
2018-08-27 18:19:54 +0200

    admin/validation/form/product.js: fixed validation groups settings
    - the validation was reinitialized over non-existing form

--- packages/framework/src/Resources/scripts/admin/validation/form/product.js
+++ packages/framework/src/Resources/scripts/admin/validation/form/product.js
@@ -10,15 +10,6 @@
                     groups.push(Shopsys.constant('\\Shopsys\\FrameworkBundle\\Form\\Admin\\Product\\ProductFormType::VALIDATION_GROUP_MANUAL_PRICE_CALCULATION'));
                 }
 
-                return groups;
-            }
-        });
-        var $productForm = $('#product_form');
-        $productForm.jsFormValidator({
-            'groups': function () {
-
-                var groups = [Shopsys.constant('\\Shopsys\\FrameworkBundle\\Form\\ValidationGroup::VALIDATION_GROUP_DEFAULT')];
-
                 if ($('input[name="product_form[displayAvailabilityGroup][usingStock]"]:checked').val() === '1') {
                     groups.push(Shopsys.constant('\\Shopsys\\FrameworkBundle\\Form\\Admin\\Product\\ProductFormType::VALIDATION_GROUP_USING_STOCK'));
                     if ($('select[name="product_form[displayAvailabilityGroup][stockGroup][outOfStockAction]"]').val() === Shopsys.constant('\\Shopsys\\FrameworkBundle\\Model\\Product\\Product::OUT_OF_STOCK_ACTION_SET_ALTERNATE_AVAILABILITY')) {
