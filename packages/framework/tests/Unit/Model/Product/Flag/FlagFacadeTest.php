d85fa65cb5ad40f4046f44d74c11fd2bf66be28b
David Ostrožlík <david.ostrozlik@shopsys.com>
2018-12-03 08:41:35 +0100

    removed unnecessary method FlagService::edit
    removed FlagService

--- packages/framework/tests/Unit/Model/Product/Flag/FlagFacadeTest.php
+++ packages/framework/tests/Unit/Model/Product/Flag/FlagFacadeTest.php
@@ -4,6 +4,7 @@ namespace Tests\FrameworkBundle\Unit\Model\Product\Flag;
 
 use PHPUnit\Framework\TestCase;
 use Shopsys\FrameworkBundle\Component\EntityExtension\EntityNameResolver;
+use Shopsys\FrameworkBundle\Model\Product\Flag\Flag;
 use Shopsys\FrameworkBundle\Model\Product\Flag\FlagData;
 use Shopsys\FrameworkBundle\Model\Product\Flag\FlagDataFactory;
 use Shopsys\FrameworkBundle\Model\Product\Flag\FlagFactory;
@@ -23,5 +24,23 @@ class FlagFacadeTest extends TestCase
         $flagDataNew = $flagDataFactory->createFromFlag($flag);
 
         $this->assertEquals($flagDataOriginal, $flagDataNew);
+    }
+
+    public function testEdit()
+    {
+        $flagDataOld = new FlagData();
+        $flagDataOld->name = ['cs' => 'flagNameCs', 'en' => 'flagNameEn'];
+        $flagDataOld->rgbColor = '#336699';
+        $flagDataEdit = new FlagData();
+        $flagDataEdit->name = ['cs' => 'editFlagNameCs', 'en' => 'editFlagNameEn'];
+        $flagDataEdit->rgbColor = '#00CCFF';
+        $flag = new Flag($flagDataOld);
+
+        $flag->edit($flagDataEdit);
+
+        $flagDataFactory = new FlagDataFactory();
+        $flagDataNew = $flagDataFactory->createFromFlag($flag);
+
+        $this->assertEquals($flagDataEdit, $flagDataNew);
     }
 }
