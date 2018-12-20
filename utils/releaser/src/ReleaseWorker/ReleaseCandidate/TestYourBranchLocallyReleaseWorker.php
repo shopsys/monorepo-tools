02798804a13971b12d81ad54e0a973953b053fd9
Rostislav Vitek <rostislav.vitek@shopsys.com>
2018-12-20 19:38:38 +0100

    TestYourBranchLocallyReleaseWorker: now it is possible to see the output of phing targets when something goes wrong and re-run the checks

--- utils/releaser/src/ReleaseWorker/ReleaseCandidate/TestYourBranchLocallyReleaseWorker.php
+++ utils/releaser/src/ReleaseWorker/ReleaseCandidate/TestYourBranchLocallyReleaseWorker.php
@@ -7,6 +7,7 @@ namespace Shopsys\Releaser\ReleaseWorker\ReleaseCandidate;
 use PharIo\Version\Version;
 use Shopsys\Releaser\ReleaseWorker\AbstractShopsysReleaseWorker;
 use Shopsys\Releaser\Stage;
+use Symplify\MonorepoBuilder\Release\Message;
 
 final class TestYourBranchLocallyReleaseWorker extends AbstractShopsysReleaseWorker
 {
@@ -16,7 +17,7 @@ final class TestYourBranchLocallyReleaseWorker extends AbstractShopsysReleaseWor
      */
     public function getDescription(Version $version): string
     {
-        return 'Test your branch locally - running composer-dev, standards and tests"';
+        return 'Test your branch locally - running composer-dev, standards and tests - this might take a few minutes';
     }
 
     /**
@@ -33,11 +34,17 @@ final class TestYourBranchLocallyReleaseWorker extends AbstractShopsysReleaseWor
      */
     public function work(Version $version): void
     {
-        $this->processRunner->run('php phing composer-dev');
-        $this->processRunner->run('php phing standards');
-        $this->processRunner->run('php phing tests');
-
-        $this->confirm('Confirm standards and tests are passing');
+        try {
+            $this->processRunner->run('php phing composer-dev standards tests', true);
+        } catch (\Symfony\Component\Process\Exception\ProcessFailedException $ex) {
+            $this->symfonyStyle->caution($ex->getProcess()->getOutput());
+            $this->symfonyStyle->note('A problem occurred, check the output and fix it please.');
+            $runChecksAgain = $this->symfonyStyle->ask('Run the checks again?', 'yes');
+            if ($runChecksAgain === 'yes') {
+                $this->work($version);
+            }
+        }
+        $this->symfonyStyle->success(Message::SUCCESS);
     }
 
     /**
