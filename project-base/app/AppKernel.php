e6fd806db55975b5bdbf0bb27bf333ba8fb36ce6
Mathew Czerner <matous.czerner@shopsys.com>
2019-01-11 12:01:58 +0100

    created shopsys/google-cloud-bundle prepared for google cloud storage filesystem implementation

--- project-base/app/AppKernel.php
+++ project-base/app/AppKernel.php
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
             new Prezent\Doctrine\TranslatableBundle\PrezentDoctrineTranslatableBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Shopsys\FormTypesBundle\ShopsysFormTypesBundle(),
+            new Shopsys\GoogleCloudBundle\ShopsysGoogleCloudBundle(),
             new Shopsys\MigrationBundle\ShopsysMigrationBundle(),
             new Shopsys\ProductFeed\HeurekaBundle\ShopsysProductFeedHeurekaBundle(),
             new Shopsys\ProductFeed\HeurekaDeliveryBundle\ShopsysProductFeedHeurekaDeliveryBundle(),
