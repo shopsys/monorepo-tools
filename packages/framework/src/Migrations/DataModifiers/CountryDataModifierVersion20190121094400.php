f1ee1f95167bc7527690874011f45f937311f488
Martin Grossmann <martin@vyvoj.net>
2019-01-22 17:21:06 +0100

    added database migration for countries

---
+++ packages/framework/src/Migrations/DataModifiers/CountryDataModifierVersion20190121094400.php
@@ -0,0 +1,195 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\FrameworkBundle\Migrations\DataModifiers;
+
+class CountryDataModifierVersion20190121094400
+{
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * @var array
+     */
+    private $tmpIds;
+
+    /**
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+        $this->tmpIds = [];
+    }
+
+    /**
+     * @return array
+     */
+    public function getGroupedByCode(): array
+    {
+        $tmp = [];
+        foreach ($this->data as $row) {
+            $tmp[$row['code']][] = $row;
+        }
+
+        return $tmp;
+    }
+
+    /**
+     * @return array
+     */
+    public function getNewIdCodePair(): array
+    {
+        $data = $this->groupDataIntoDomains($this->data);
+
+        $tmp = [];
+        foreach ($data as $domainId => $domainData) {
+            foreach ($domainData as $row) {
+                if ($domainId === 1 || !array_key_exists($row['code'], $tmp)) {
+                    $tmp[$row['code']] = $row['id'];
+                }
+            }
+        }
+
+        return $tmp;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAllCodes(): array
+    {
+        return array_keys($this->getNewIdCodePair());
+    }
+
+    /**
+     * @return array
+     */
+    public function getAllIds(): array
+    {
+        $tmp = [];
+        foreach ($this->data as $row) {
+            $tmp[$row['id']] = $row['id'];
+        }
+
+        return $tmp;
+    }
+
+    /**
+     * @param int $oldId
+     * @return int
+     */
+    public function getNewId(int $oldId): int
+    {
+        if (empty($this->tmpIds)) {
+            $this->loadIdPairs();
+        }
+
+        return $this->tmpIds[$oldId];
+    }
+
+    private function loadIdPairs(): void
+    {
+        $pair = $this->getNewIdCodePair();
+
+        foreach ($this->data as $row) {
+            $this->tmpIds[$row['id']] = $pair[$row['code']];
+        }
+    }
+
+    /**
+     * @param $domainId
+     * @param $countryCode
+     * @return bool
+     */
+    private function codeExistsForDomain($domainId, $countryCode): bool
+    {
+        foreach ($this->data as $row) {
+            if ($row['code'] === $countryCode && $row['domain_id'] === $domainId) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param int $domainId
+     * @param string $countryCode
+     * @return array
+     */
+    public function getDomainDataForCountry(int $domainId, string $countryCode): array
+    {
+        $codeIdPairs = $this->getNewIdCodePair();
+
+        return [
+            'country_id' => $codeIdPairs[$countryCode],
+            'domain_id' => $domainId,
+            'enabled' => $this->codeExistsForDomain($domainId, $countryCode),
+            'priority' => 0,
+        ];
+    }
+
+    /**
+     * @param int $domainId
+     * @param string $countryCode
+     * @return array
+     */
+    public function getTranslatableDataForCountry(int $domainId, string $countryCode): array
+    {
+        $codeIdPairs = $this->getNewIdCodePair();
+
+        return [
+            'translatable_id' => $codeIdPairs[$countryCode],
+            'name' => $this->getNameForCountryAndDomain($domainId, $countryCode),
+        ];
+    }
+
+    /**
+     * @param int $domainId
+     * @param string $countryCode
+     * @return string
+     */
+    private function getNameForCountryAndDomain(int $domainId, string $countryCode): string
+    {
+        foreach ($this->data as $row) {
+            if ($row['code'] === $countryCode && $row['domain_id'] === $domainId) {
+                return $row['name'];
+            }
+        }
+
+        return $countryCode;
+    }
+
+    /**
+     * @return array
+     */
+    public function getObsoleteCountryIds(): array
+    {
+        $obsoleteIds = [];
+        foreach ($this->data as $row) {
+            $obsoleteIds[] = $row['id'];
+        }
+
+        $usedIds = array_values($this->getNewIdCodePair());
+
+        return array_values(array_diff($obsoleteIds, $usedIds));
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     */
+    private function groupDataIntoDomains(array $data): array
+    {
+        $tmp = [];
+        foreach ($data as $row) {
+            $tmp[$row['domain_id']][] = $row;
+        }
+
+        return $tmp;
+    }
+}
--- packages/framework/src/Migrations/Version20190121094400.php
+++ packages/framework/src/Migrations/Version20190121094400.php
@@ -2,11 +2,15 @@
 
 namespace Shopsys\FrameworkBundle\Migrations;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Schema\Schema;
+use Shopsys\FrameworkBundle\Migrations\DataModifiers\CountryDataModifierVersion20190121094400;
 use Shopsys\MigrationBundle\Component\Doctrine\Migrations\AbstractMigration;
 
 class Version20190121094400 extends AbstractMigration
 {
+    use MultidomainMigrationTrait;
+
     /**
      * @param \Doctrine\DBAL\Schema\Schema $schema
      */
@@ -48,8 +52,65 @@ class Version20190121094400 extends AbstractMigration
         $this->sql('ALTER TABLE countries ALTER COLUMN name DROP NOT NULL');
         $this->sql('ALTER TABLE countries ALTER COLUMN domain_id DROP NOT NULL');
 
+        $countries = $this->sql('SELECT * FROM countries')->fetchAll();
+
+        $transformer = new CountryDataModifierVersion20190121094400($countries);
+
+        foreach ($this->getAllDomainIds() as $domainId) {
+            foreach ($transformer->getAllIds() as $oldId) {
+                $newId = $transformer->getNewId($oldId);
+
+                $this->sql(
+                    'UPDATE orders SET country_id = :newCountryId WHERE country_id = :originalCountryId AND domain_id = :domainId',
+                    [
+                        'newCountryId' => $newId,
+                        'originalCountryId' => $oldId,
+                        'domainId' => $domainId,
+                    ]
+                );
+
+                $this->sql(
+                    'UPDATE orders SET delivery_country_id = :newCountryId WHERE delivery_country_id = :originalCountryId AND domain_id = :domainId',
+                    [
+                        'newCountryId' => $newId,
+                        'originalCountryId' => $oldId,
+                        'domainId' => $domainId,
+                    ]
+                );
+
+                $this->sql(
+                    'UPDATE billing_addresses SET country_id = :newCountryId WHERE country_id = :originalCountryId',
+                    [
+                        'newCountryId' => $newId,
+                        'originalCountryId' => $oldId,
+                    ]
+                );
+
+                $this->sql(
+                    'UPDATE delivery_addresses SET country_id = :newCountryId WHERE country_id = :originalCountryId',
+                    [
+                        'newCountryId' => $newId,
+                        'originalCountryId' => $oldId,
+                    ]
+                );
+            }
+
+            foreach ($transformer->getAllCodes() as $code) {
+                $domainData = $transformer->getDomainDataForCountry($domainId, $code);
+                $this->sql('INSERT INTO country_domains (country_id, domain_id, enabled) VALUES (:countryId, :domainId, :enabled)', [$domainData['country_id'], $domainData['domain_id'], (int)$domainData['enabled']]);
+
+                $translatableData = $transformer->getTranslatableDataForCountry($domainId, $code);
+                $this->sql('INSERT INTO country_translations (translatable_id, name, locale) VALUES (:translatableId, :name, :locale)', [$translatableData['translatable_id'], $translatableData['name'], $this->getDomainLocale($domainId)]);
+            }
+        }
+
+        $this->sql('DELETE FROM countries WHERE id IN (:ids)', ['ids' => $transformer->getObsoleteCountryIds()], ['ids' => Connection::PARAM_INT_ARRAY]);
+
         $this->sql('ALTER TABLE countries DROP COLUMN name');
         $this->sql('ALTER TABLE countries DROP COLUMN domain_id');
+        $this->sql('ALTER TABLE countries ALTER code SET NOT NULL');
+        $this->sql('ALTER TABLE country_domains ALTER priority DROP DEFAULT');
+        $this->sql('CREATE UNIQUE INDEX countries_code_uni ON countries (code)');
     }
 
     /**
