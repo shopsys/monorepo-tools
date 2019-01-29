Reed13/shopsys

countries are now unified
- domain is part of country definition
- updated form types
- updated data fixtures

packages/framework/src/DataFixtures/Demo/CountryDataFixture.php
@@ -10,8 +10,8 @@ use Shopsys\FrameworkBundle\Model\Country\CountryFacade;
 
 class CountryDataFixture extends AbstractReferenceFixture
 {
-    const COUNTRY_CZECH_REPUBLIC_1 = 'country_czech_republic_1';
-    const COUNTRY_SLOVAKIA_1 = 'country_slovakia_1';
+    public const COUNTRY_CZECH_REPUBLIC = 'country_czech_republic';
+    public const COUNTRY_SLOVAKIA = 'country_slovakia';
 
     /**
      * @var \Shopsys\FrameworkBundle\Model\Country\CountryFacade
@@ -36,28 +36,33 @@ class CountryDataFixture extends AbstractReferenceFixture
     /**
      * @param \Doctrine\Common\Persistence\ObjectManager $manager
      */
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
-        $domainId = 1;
         $countryData = $this->countryDataFactory->create();
-        $countryData->name = 'Czech republic';
+        $countryData->name = [
+            'cs' => 'Česká republika',
+            'en' => 'Czech republic',
+        ];
         $countryData->code = 'CZ';
-        $this->createCountry($countryData, $domainId, self::COUNTRY_CZECH_REPUBLIC_1);
+        $this->createCountry($countryData, self::COUNTRY_CZECH_REPUBLIC);
 
         $countryData = $this->countryDataFactory->create();
-        $countryData->name = 'Slovakia';
+        $countryData->name = [
+            'cs' => 'Slovenská republika',
+            'en' => 'Slovakia',
+        ];
         $countryData->code = 'SK';
-        $this->createCountry($countryData, $domainId, self::COUNTRY_SLOVAKIA_1);
+
+        $this->createCountry($countryData, self::COUNTRY_SLOVAKIA);
     }
 
     /**
      * @param \Shopsys\FrameworkBundle\Model\Country\CountryData $countryData
-     * @param int $domainId
      * @param string $referenceName
      */
-    private function createCountry(CountryData $countryData, $domainId, $referenceName)
+    private function createCountry(CountryData $countryData, $referenceName): void
     {
-        $country = $this->countryFacade->create($countryData, $domainId);
+        $country = $this->countryFacade->create($countryData);
         $this->addReference($referenceName, $country);
     }
 }

packages/framework/src/Form/Admin/Country/CountryFormType.php
@@ -2,8 +2,15 @@
 
 namespace Shopsys\FrameworkBundle\Form\Admin\Country;
 
+use Shopsys\FormTypesBundle\MultidomainType;
+use Shopsys\FrameworkBundle\Component\Domain\Domain;
+use Shopsys\FrameworkBundle\Form\DisplayOnlyType;
+use Shopsys\FrameworkBundle\Form\DomainsType;
+use Shopsys\FrameworkBundle\Form\Locale\LocalizedType;
+use Shopsys\FrameworkBundle\Model\Country\Country;
 use Shopsys\FrameworkBundle\Model\Country\CountryData;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,38 +19,84 @@ use Symfony\Component\Validator\Constraints;
 class CountryFormType extends AbstractType
 {
     /**
+     * @var \Shopsys\FrameworkBundle\Component\Domain\Domain
+     */
+    protected $domain;
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\Domain\Domain $domain
+     */
+    public function __construct(Domain $domain)
+    {
+        $this->domain = $domain;
+    }
+
+    /**
      * @param \Symfony\Component\Form\FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        /** @var \Shopsys\FrameworkBundle\Model\Country\Country|null $country */
+        $country = $options['country'];
+
+        if ($country instanceof Country) {
+            $builder->add('formId', DisplayOnlyType::class, [
+                'label' => t('ID'),
+                'data' => $country->getId(),
+            ]);
+        }
+
         $builder
-            ->add(
-                'name',
-                TextType::class,
-                [
+            ->add('name', LocalizedType::class, [
                 'required' => true,
-                'constraints' => [
+                'main_constraints' => [
                     new Constraints\NotBlank(['message' => 'Please enter country name']),
-                    new Constraints\Length(['max' => 255, 'maxMessage' => 'Country name cannot be longer than {{ limit }} characters']),
-                ], ]
-            )
+                ],
+                'entry_options' => [
+                    'required' => true,
+                    'constraints' => [
+                        new Constraints\NotBlank(['message' => 'Please enter country name']),
+                        new Constraints\Length(['max' => 255, 'maxMessage' => 'Country name cannot be longer than {{ limit }} characters']),
+                    ],
+                ],
+                'label' => t('Name'),
+            ])
             ->add('code', TextType::class, [
                 'required' => false,
                 'constraints' => [
                     new Constraints\Length(['max' => 2, 'maxMessage' => 'Country code cannot be longer than {{ limit }} characters']),
                 ],
-            ]);
+            ])
+            ->add('enabled', DomainsType::class, [
+                'required' => false,
+                'label' => t('Display on'),
+            ])
+            ->add('priority', MultidomainType::class, [
+                'entry_type' => TextType::class,
+                'entry_options' => [
+                    'required' => false,
+                    'constraints' => [
+                        new Constraints\GreaterThanOrEqual(['value' => 0]),
+                    ],
+                ],
+                'required' => false,
+                'label' => t('Priority'),
+            ])
+            ->add('save', SubmitType::class);
     }
 
     /**
      * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
      */
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            'data_class' => CountryData::class,
-            'attr' => ['novalidate' => 'novalidate'],
-        ]);
+        $resolver
+            ->setDefined('country')
+            ->setAllowedTypes('country', [Country::class, 'null'])
+            ->setDefaults([
+                'data_class' => CountryData::class,
+                'attr' => ['novalidate' => 'novalidate'],
+            ]);
     }
 }

packages/framework/src/Form/Admin/Customer/BillingAddressFormType.php
@@ -39,7 +39,7 @@ class BillingAddressFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $countries = $this->countryFacade->getAllByDomainId($options['domain_id']);
+        $countries = $this->countryFacade->getAllEnabledByDomainId($options['domain_id']);
 
         $builderCompanyDataGroup = $builder->create('companyData', GroupType::class, [
             'label' => t('Company data'),

packages/framework/src/Form/Admin/Customer/DeliveryAddressFormType.php
@@ -39,7 +39,7 @@ class DeliveryAddressFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $countries = $this->countryFacade->getAllByDomainId($options['domain_id']);
+        $countries = $this->countryFacade->getAllEnabledByDomainId($options['domain_id']);
 
         $builderDeliveryAdress = $builder->create('deliveryAddress', GroupType::class, [
             'label' => t('Shipping address'),

project-base/src/Shopsys/ShopBundle/Form/Front/Order/PersonalInfoFormType.php
@@ -59,7 +59,7 @@ class PersonalInfoFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $countries = $this->countryFacade->getAllByDomainId($options['domain_id']);
+        $countries = $this->countryFacade->getAllEnabledByDomainId($options['domain_id']);
 
         $builder
             ->add('firstName', TextType::class, [

project-base/tests/ShopBundle/Functional/PersonalData/PersonalDataExportXmlTest.php
@@ -66,9 +66,9 @@ class PersonalDataExportXmlTest extends TransactionFunctionalTestCase
     private function createCountry()
     {
         $countryData = new CountryData();
-        $countryData->name = 'Czech Republic';
+        $countryData->name = ['cz' => 'Czech Republic'];
         $countryData->code = 'CZ';
-        $country = new Country($countryData, self::DOMAIN_ID_FIRST);
+        $country = new Country($countryData);
 
         return $country;
     }

shopsys/monorepo-tools

packages/framework/src/DataFixtures/Demo/CountryDataFixture.php
@@ -10,8 +10,8 @@ use Shopsys\FrameworkBundle\Model\Country\CountryFacade;
 
 class CountryDataFixture extends AbstractReferenceFixture
 {
-    const COUNTRY_CZECH_REPUBLIC_1 = 'country_czech_republic_1';
-    const COUNTRY_SLOVAKIA_1 = 'country_slovakia_1';
+    public const COUNTRY_CZECH_REPUBLIC = 'country_czech_republic';
+    public const COUNTRY_SLOVAKIA = 'country_slovakia';
 
     /**
      * @var \Shopsys\FrameworkBundle\Model\Country\CountryFacade
@@ -36,28 +36,33 @@ class CountryDataFixture extends AbstractReferenceFixture
     /**
      * @param \Doctrine\Common\Persistence\ObjectManager $manager
      */
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
-        $domainId = 1;
         $countryData = $this->countryDataFactory->create();
-        $countryData->name = 'Czech republic';
+        $countryData->names = [
+            'cs' => 'Česká republika',
+            'en' => 'Czech republic',
+        ];
         $countryData->code = 'CZ';
-        $this->createCountry($countryData, $domainId, self::COUNTRY_CZECH_REPUBLIC_1);
+        $this->createCountry($countryData, self::COUNTRY_CZECH_REPUBLIC);
 
         $countryData = $this->countryDataFactory->create();
-        $countryData->name = 'Slovakia';
+        $countryData->names = [
+            'cs' => 'Slovenská republika',
+            'en' => 'Slovakia',
+        ];
         $countryData->code = 'SK';
-        $this->createCountry($countryData, $domainId, self::COUNTRY_SLOVAKIA_1);
+
+        $this->createCountry($countryData, self::COUNTRY_SLOVAKIA);
     }
 
     /**
      * @param \Shopsys\FrameworkBundle\Model\Country\CountryData $countryData
-     * @param int $domainId
      * @param string $referenceName
      */
-    private function createCountry(CountryData $countryData, $domainId, $referenceName)
+    private function createCountry(CountryData $countryData, $referenceName): void
     {
-        $country = $this->countryFacade->create($countryData, $domainId);
+        $country = $this->countryFacade->create($countryData);
         $this->addReference($referenceName, $country);
     }
 }

packages/framework/src/Form/Admin/Country/CountryFormType.php
@@ -2,48 +2,134 @@
 
 namespace Shopsys\FrameworkBundle\Form\Admin\Country;
 
+use Shopsys\FormTypesBundle\MultidomainType;
+use Shopsys\FrameworkBundle\Form\DisplayOnlyType;
+use Shopsys\FrameworkBundle\Form\DomainsType;
+use Shopsys\FrameworkBundle\Form\Locale\LocalizedType;
+use Shopsys\FrameworkBundle\Model\Country\Country;
 use Shopsys\FrameworkBundle\Model\Country\CountryData;
+use Shopsys\FrameworkBundle\Model\Country\CountryFacade;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class CountryFormType extends AbstractType
 {
     /**
+     * @var \Shopsys\FrameworkBundle\Model\Country\Country
+     */
+    protected $country;
+
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Country\CountryFacade
+     */
+    protected $countryFacade;
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Country\CountryFacade $countryFacade
+     */
+    public function __construct(CountryFacade $countryFacade)
+    {
+        $this->countryFacade = $countryFacade;
+    }
+
+    /**
      * @param \Symfony\Component\Form\FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        /** @var \Shopsys\FrameworkBundle\Model\Country\Country|null $country */
+        $this->country = $options['country'];
+
+        if ($this->country instanceof Country) {
+            $builder->add('formId', DisplayOnlyType::class, [
+                'label' => t('ID'),
+                'data' => $this->country->getId(),
+            ]);
+        }
+
         $builder
-            ->add(
-                'name',
-                TextType::class,
-                [
+            ->add('names', LocalizedType::class, [
+                'required' => true,
+                'main_constraints' => [
+                    new Constraints\NotBlank(['message' => 'Please enter country name']),
+                ],
+                'entry_options' => [
+                    'required' => true,
+                    'constraints' => [
+                        new Constraints\NotBlank(['message' => 'Please enter country name']),
+                        new Constraints\Length(['max' => 255, 'maxMessage' => 'Country name cannot be longer than {{ limit }} characters']),
+                    ],
+                ],
+                'label' => t('Name'),
+            ])
+            ->add('code', TextType::class, [
                 'required' => true,
                 'constraints' => [
-                    new Constraints\NotBlank(['message' => 'Please enter country name']),
-                    new Constraints\Length(['max' => 255, 'maxMessage' => 'Country name cannot be longer than {{ limit }} characters']),
-                ], ]
-            )
-            ->add('code', TextType::class, [
-                'required' => false,
-                'constraints' => [
+                    new Constraints\NotBlank(['message' => 'Please enter country code']),
                     new Constraints\Length(['max' => 2, 'maxMessage' => 'Country code cannot be longer than {{ limit }} characters']),
+                    new Constraints\Callback([$this, 'validateUniqueCode']),
                 ],
-            ]);
+                'label' => t('Code'),
+                'attr' => [
+                    'icon' => true,
+                    'iconTitle' => t('Country code in ISO 3166-1 alpha-2'),
+                ],
+            ])
+            ->add('enabled', DomainsType::class, [
+                'required' => false,
+                'label' => t('Display on'),
+            ])
+            ->add('priority', MultidomainType::class, [
+                'entry_type' => TextType::class,
+                'entry_options' => [
+                    'attr' => [
+                        'icon' => true,
+                        'iconTitle' => t('The higher the priority, the higher the country will be shown in the listings. Countries with the same priority will be sorted alphabetically.'),
+                    ],
+                    'required' => false,
+                    'constraints' => [
+                        new Constraints\GreaterThanOrEqual(['value' => 0]),
+                    ],
+                ],
+                'required' => false,
+                'label' => t('Priority'),
+            ])
+            ->add('save', SubmitType::class);
     }
 
     /**
      * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
      */
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            'data_class' => CountryData::class,
-            'attr' => ['novalidate' => 'novalidate'],
-        ]);
+        $resolver
+            ->setDefined('country')
+            ->setAllowedTypes('country', [Country::class, 'null'])
+            ->setDefaults([
+                'data_class' => CountryData::class,
+                'attr' => ['novalidate' => 'novalidate'],
+            ]);
+    }
+
+    /**
+     * @param $countryCodeValue
+     * @param \Symfony\Component\Validator\Context\ExecutionContextInterface $context
+     */
+    public function validateUniqueCode($countryCodeValue, ExecutionContextInterface $context): void
+    {
+        if ($this->country === null || $countryCodeValue !== $this->country->getCode()) {
+            $country = $this->countryFacade->findByCode($countryCodeValue);
+
+            if ($country !== null) {
+                $this->country = $country;
+                $context->addViolation('Country code with this code already exists');
+            }
+        }
     }
 }

packages/framework/src/Form/Admin/Customer/BillingAddressFormType.php
@@ -39,7 +39,7 @@ class BillingAddressFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $countries = $this->countryFacade->getAllByDomainId($options['domain_id']);
+        $countries = $this->countryFacade->getAllEnabledOnDomain($options['domain_id']);
 
         $builderCompanyDataGroup = $builder->create('companyData', GroupType::class, [
             'label' => t('Company data'),

packages/framework/src/Form/Admin/Customer/DeliveryAddressFormType.php
@@ -39,7 +39,7 @@ class DeliveryAddressFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $countries = $this->countryFacade->getAllByDomainId($options['domain_id']);
+        $countries = $this->countryFacade->getAllEnabledOnDomain($options['domain_id']);
 
         $builderDeliveryAdress = $builder->create('deliveryAddress', GroupType::class, [
             'label' => t('Shipping address'),

project-base/src/Shopsys/ShopBundle/Form/Front/Order/PersonalInfoFormType.php
@@ -59,7 +59,7 @@ class PersonalInfoFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $countries = $this->countryFacade->getAllByDomainId($options['domain_id']);
+        $countries = $this->countryFacade->getAllEnabledOnDomain($options['domain_id']);
 
         $builder
             ->add('firstName', TextType::class, [

project-base/tests/ShopBundle/Functional/PersonalData/PersonalDataExportXmlTest.php
@@ -67,9 +67,9 @@ class PersonalDataExportXmlTest extends TransactionFunctionalTestCase
     private function createCountry()
     {
         $countryData = new CountryData();
-        $countryData->name = 'Czech Republic';
+        $countryData->names = ['cz' => 'Czech Republic'];
         $countryData->code = 'CZ';
-        $country = new Country($countryData, self::DOMAIN_ID_FIRST);
+        $country = new Country($countryData);
 
         return $country;
     }