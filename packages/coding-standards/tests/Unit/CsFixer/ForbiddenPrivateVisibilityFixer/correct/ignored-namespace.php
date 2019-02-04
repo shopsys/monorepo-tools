26135c3c586b9b1e4cd5fd498eb454dd0318ddea
Martin Grossmann <martin@vyvoj.net>
2019-02-04 12:44:15 +0100

    added new forbidden private visibility in namespace coding standard fixer

---
+++ packages/coding-standards/tests/Unit/CsFixer/ForbiddenPrivateVisibilityFixer/correct/ignored-namespace.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace OtherNamespace\TestSubNamespace;
+
+class SomeClass
+{
+    /**
+     * @var int
+     */
+    private $field;
+
+    /**
+     * @return bool
+     */
+    private function method()
+    {
+        return true;
+    }
+}
---
+++ packages/coding-standards/tests/Unit/CsFixer/ForbiddenPrivateVisibilityFixer/fixed/fixed.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TestNamespace\TestSubNamespace;
+
+class SomeClass
+{
+    /**
+     * @var int
+     */
+    protected $field;
+
+    /**
+     * @return bool
+     */
+    protected function method()
+    {
+        return true;
+    }
+}
