098800211dd0dbd52d3322333ed01a73dc8dd7eb
Petr Heinz <petr.heinz@shopsys.com>
2019-02-22 12:34:30 +0100

    fixed form IDs in orderTransportAndPayment.js
    - prefilling of transport and payment prices during order edit in administration now works correctly

--- packages/framework/src/Resources/scripts/admin/orderTransportAndPayment.js
+++ packages/framework/src/Resources/scripts/admin/orderTransportAndPayment.js
@@ -5,11 +5,11 @@
     Shopsys.order.transportAndPayment = Shopsys.order.transportAndPayment || {};
 
     Shopsys.order.transportAndPayment.Prefiller = function () {
-        var $transportSelect = $('#order_form_orderTransport_transport');
+        var $transportSelect = $('#order_form_orderItems_orderTransport_transport');
         var transportPricesWithVatByTransportId = $transportSelect.closest('.js-order-transport-row').data('transport-prices-with-vat-by-transport-id');
         var transportVatPercentsByTransportId = $transportSelect.closest('.js-order-transport-row').data('transport-vat-percents-by-transport-id');
 
-        var $paymentSelect = $('#order_form_orderPayment_payment');
+        var $paymentSelect = $('#order_form_orderItems_orderPayment_payment');
         var paymentPricesWithVatByPaymentId = $paymentSelect.closest('.js-order-payment-row').data('payment-prices-with-vat-by-payment-id');
         var paymentVatPercentsByPaymentId = $paymentSelect.closest('.js-order-payment-row').data('payment-vat-percents-by-payment-id');
 
@@ -20,14 +20,14 @@
 
         var onOrderTransportChange = function () {
             var selectedTransportId = $transportSelect.val();
-            $('#order_form_orderTransport_priceWithVat').val(transportPricesWithVatByTransportId[selectedTransportId].amount);
-            $('#order_form_orderTransport_vatPercent').val(transportVatPercentsByTransportId[selectedTransportId]);
+            $('#order_form_orderItems_orderTransport_priceWithVat').val(transportPricesWithVatByTransportId[selectedTransportId].amount);
+            $('#order_form_orderItems_orderTransport_vatPercent').val(transportVatPercentsByTransportId[selectedTransportId]);
         };
 
         var onOrderPaymentChange = function () {
             var selectedPaymentId = $paymentSelect.val();
-            $('#order_form_orderPayment_priceWithVat').val(paymentPricesWithVatByPaymentId[selectedPaymentId].amount);
-            $('#order_form_orderPayment_vatPercent').val(paymentVatPercentsByPaymentId[selectedPaymentId]);
+            $('#order_form_orderItems_orderPayment_priceWithVat').val(paymentPricesWithVatByPaymentId[selectedPaymentId].amount);
+            $('#order_form_orderItems_orderPayment_vatPercent').val(paymentVatPercentsByPaymentId[selectedPaymentId]);
         };
     };
 
