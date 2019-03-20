3e65b8a784611d9a9339d0bdee994e4356c98167
Martin Grossmann <martin@vyvoj.net>
2019-03-20 16:38:10 +0100

    added fixer forcing late static binding for protected constants

---
+++ packages/coding-standards/tests/Unit/Sniffs/ForceLateStaticBindingForProtectedConstantsSniff/wrong/SelfWithMethodsAndVariables.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\CodingStandards\Sniffs\ForceLateStaticBindingForProtectedConstantsSniff;
+
+class SelfWithMethodsAndVariables
+{
+    public const A = 'value';
+    protected const B = 'value';
+    private const C = 'value';
+
+    public static $publicProperty;
+
+    protected static $protectedProperty;
+
+    private static $privateProperty;
+
+    public function method()
+    {
+        echo self::A;
+        self::class;
+        self::publicStaticMethod();
+        self::protectedStaticMethod();
+        self::privateStaticMethod();
+
+        echo self::B;
+        self::class;
+        self::publicStaticMethod();
+        self::protectedStaticMethod();
+        self::privateStaticMethod();
+
+        echo self::C;
+        self::class;
+        self::publicStaticMethod();
+        self::protectedStaticMethod();
+        self::privateStaticMethod();
+
+        echo self::A;
+        echo self::B;
+        echo self::C;
+
+        echo self::$publicProperty;
+        echo self::$protectedProperty;
+        echo self::$privateProperty;
+
+        echo static::$publicProperty;
+        echo static::$protectedProperty;
+        echo static::$privateProperty;
+    }
+
+    public static function publicStaticMethod()
+    {
+        echo 'value';
+    }
+
+    protected static function protectedStaticMethod()
+    {
+        echo 'value';
+    }
+
+    private static function privateStaticMethod()
+    {
+        echo 'value';
+    }
+}
