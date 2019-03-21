25eddef5b91a7b716f08f0282b99aa1f8d1d8f06
Rostislav Vitek <rostislav.vitek@shopsys.com>
2019-03-21 12:05:36 +0100

    DumpTranslationsReleaseWorker now contains more descriptive information

--- utils/releaser/src/ReleaseWorker/ReleaseCandidate/DumpTranslationsReleaseWorker.php
+++ utils/releaser/src/ReleaseWorker/ReleaseCandidate/DumpTranslationsReleaseWorker.php
@@ -51,7 +51,7 @@ final class DumpTranslationsReleaseWorker extends AbstractShopsysReleaseWorker
                 $this->commit('dump translations');
                 $this->symfonyStyle->success('Translations were dumped and only deleted were found and committed');
             } else {
-                $this->symfonyStyle->note('There are new translations, check the changed files, fill in the missing translations and commit the changes');
+                $this->symfonyStyle->note('There are new translations, check the changed files (you can use "git status") command, fill in the missing translations and commit the changes');
                 $this->confirm('Confirm files are checked, missing translations completed and the changes are committed');
             }
         } else {
