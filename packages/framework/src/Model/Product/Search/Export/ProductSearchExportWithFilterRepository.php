7440d4b0897ecc20e93aa3a41f467151c9e922f4
Martin Grossmann <martin@vyvoj.net>
2019-04-12 12:49:35 +0200

    elasticsearch product structure is now extended with data for filtering

---
+++ packages/framework/src/Model/Product/Search/Export/ProductSearchExportWithFilterRepository.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Model\Product\Search\Export;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\QueryBuilder;
+use Shopsys\FrameworkBundle\Model\Product\Parameter\ParameterRepository;
+use Shopsys\FrameworkBundle\Model\Product\Product;
+use Shopsys\FrameworkBundle\Model\Product\ProductFacade;
+use Shopsys\FrameworkBundle\Model\Product\ProductVisibility;
+
+class ProductSearchExportWithFilterRepository extends ProductSearchExportRepository
+{
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Product\Parameter\ParameterRepository
+     */
+    protected $parameterRepository;
+
+    /**
+     * @var \Shopsys\FrameworkBundle\Model\Product\ProductFacade
+     */
+    protected $productFacade;
+
+    /**
+     * @param \Doctrine\ORM\EntityManagerInterface $em
+     * @param \Shopsys\FrameworkBundle\Model\Product\Parameter\ParameterRepository $parameterRepository
+     * @param \Shopsys\FrameworkBundle\Model\Product\ProductFacade $productFacade
+     */
+    public function __construct(EntityManagerInterface $em, ParameterRepository $parameterRepository, ProductFacade $productFacade)
+    {
+        parent::__construct($em);
+
+        $this->parameterRepository = $parameterRepository;
+        $this->productFacade = $productFacade;
+    }
+
+    /**
+     * @param int $domainId
+     * @param string $locale
+     * @param int $startFrom
+     * @param int $batchSize
+     * @return array
+     */
+    public function getProductsData(int $domainId, string $locale, int $startFrom, int $batchSize): array
+    {
+        $queryBuilder = $this->createQueryBuilder($domainId, $locale)
+            ->setFirstResult($startFrom)
+            ->setMaxResults($batchSize);
+
+        $query = $queryBuilder->getQuery();
+
+        $result = [];
+        /** @var \Shopsys\FrameworkBundle\Model\Product\Product $product */
+        foreach ($query->getResult() as $product) {
+            $flagIds = $this->extractFlags($product);
+            $categoryIds = $this->extractCategories($domainId, $product);
+            $parameters = $this->extractParameters($locale, $product);
+            $prices = $this->extractPrices($domainId, $product);
+
+            $result[] = [
+                'id' => $product->getId(),
+                'catnum' => $product->getCatnum(),
+                'partno' => $product->getPartno(),
+                'ean' => $product->getEan(),
+                'name' => $product->getName($locale),
+                'description' => $product->getDescription($domainId),
+                'shortDescription' => $product->getShortDescription($domainId),
+                'brand' => $product->getBrand() ? $product->getBrand()->getId() : '',
+                'flags' => $flagIds,
+                'categories' => $categoryIds,
+                'in_stock' => $product->getCalculatedAvailability()->getDispatchTime() === 0,
+                'prices' => $prices,
+                'parameters' => $parameters,
+                'ordering_priority' => $product->getOrderingPriority(),
+                'calculated_selling_denied' => $product->getCalculatedSellingDenied(),
+            ];
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param int $domainId
+     * @param string $locale
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    protected function createQueryBuilder(int $domainId, string $locale): QueryBuilder
+    {
+        $queryBuilder = $this->em->createQueryBuilder()
+            ->select('p')
+            ->from(Product::class, 'p')
+                ->where('p.variantType != :variantTypeVariant')
+            ->join(ProductVisibility::class, 'prv', Join::WITH, 'prv.product = p.id')
+                ->andWhere('prv.domainId = :domainId')
+                ->andWhere('prv.visible = TRUE')
+            ->groupBy('p.id')
+            ->orderBy('p.id');
+
+        $queryBuilder->setParameter('domainId', $domainId)
+            ->setParameter('variantTypeVariant', Product::VARIANT_TYPE_VARIANT);
+
+        return $queryBuilder;
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Product\Product $product
+     * @return int[]
+     */
+    protected function extractFlags(Product $product): array
+    {
+        $flagIds = [];
+        foreach ($product->getFlags() as $flag) {
+            $flagIds[] = $flag->getId();
+        }
+
+        return $flagIds;
+    }
+
+    /**
+     * @param int $domainId
+     * @param \Shopsys\FrameworkBundle\Model\Product\Product $product
+     * @return int[]
+     */
+    protected function extractCategories(int $domainId, Product $product): array
+    {
+        $categoryIds = [];
+        foreach ($product->getCategoriesIndexedByDomainId()[$domainId] as $category) {
+            $categoryIds[] = $category->getId();
+        }
+
+        return $categoryIds;
+    }
+
+    /**
+     * @param string $locale
+     * @param \Shopsys\FrameworkBundle\Model\Product\Product $product
+     * @return array
+     */
+    protected function extractParameters(string $locale, Product $product): array
+    {
+        $parameters = [];
+        $productParameterValues = $this->parameterRepository->getProductParameterValuesByProductSortedByName($product, $locale);
+        foreach ($productParameterValues as $productParameterValue) {
+            $parameter = $productParameterValue->getParameter();
+            $parameterValue = $productParameterValue->getValue();
+            if ($parameter->getName($locale) !== null && $parameterValue->getLocale() === $locale) {
+                $parameters[] = [
+                    'parameter_id' => $parameter->getId(),
+                    'parameter_value_id' => $parameterValue->getId(),
+                ];
+            }
+        }
+
+        return $parameters;
+    }
+
+    /**
+     * @param int $domainId
+     * @param \Shopsys\FrameworkBundle\Model\Product\Product $product
+     * @return array
+     */
+    protected function extractPrices(int $domainId, Product $product): array
+    {
+        $prices = [];
+        $productSellingPrices = $this->productFacade->getAllProductSellingPricesIndexedByDomainId($product)[$domainId];
+        /** @var \Shopsys\FrameworkBundle\Model\Product\Pricing\ProductSellingPrice $productSellingPrice */
+        foreach ($productSellingPrices as $productSellingPrice) {
+            $prices[] = [
+                'pricing_group_id' => $productSellingPrice->getPricingGroup()->getId(),
+                'amount' => (float)$productSellingPrice->getSellingPrice()->getPriceWithVat()->getAmount(),
+            ];
+        }
+
+        return $prices;
+    }
+}
