42da0bb279933de5e390caa2059b6d26ff651612
Tomáš Gottvald <tomas.gottvald@shopsys.com>
2019-05-17 08:41:43 +0200

    design - admin dropdown element

---
+++ packages/framework/src/Resources/scripts/admin/dropdown.js
@@ -0,0 +1,34 @@
+(function ($) {
+
+    Shopsys = window.Shopsys || {};
+    Shopsys.dropdown = Shopsys.dropdown || {};
+
+    Shopsys.dropdown.DropDown = function ($dropdown) {
+        this.init = function () {
+            var $items = $dropdown.filterAllNodes('.js-dropdown-item');
+
+            $items.click(function (event) {
+                hideAllSubmenus();
+                $(this).addClass('open');
+
+                event.stopPropagation();
+            });
+
+            $(document).on('click', function () {
+                hideAllSubmenus();
+            });
+
+            var hideAllSubmenus = function () {
+                $items.removeClass('open');
+            };
+        };
+    };
+
+    Shopsys.register.registerCallback(function ($container) {
+        $container.filterAllNodes('.js-dropdown').each(function () {
+            var toggleMenu = new Shopsys.dropdown.DropDown($(this));
+            DropDown.init();
+        });
+    });
+
+})(jQuery);
