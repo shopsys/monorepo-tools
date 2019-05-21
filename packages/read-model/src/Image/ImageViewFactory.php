pesovo/shopsys

the view objects can be now created for all use cases
- added new ListedProductViewFacade covering all use cases for listing

packages/read-model/src/Image/ImageViewFactory.php
@@ -1,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\ReadModelBundle\Image;
+
+use Shopsys\FrameworkBundle\Component\Image\Image;
+
+/**
+ * @experimental
+ */
+class ImageViewFactory
+{
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\Image\Image $image
+     * @return \Shopsys\ReadModelBundle\Image\ImageViewInterface
+     */
+    public function createFromImage(Image $image): ImageViewInterface
+    {
+        return new ImageView(
+            $image->getId(),
+            $image->getExtension(),
+            $image->getEntityName(),
+            $image->getType()
+        );
+    }
+}

packages/read-model/src/Product/Action/ProductActionViewFactory.php
@@ -1,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\ReadModelBundle\Product\Action;
+
+use Shopsys\FrameworkBundle\Model\Product\Product;
+
+/**
+ * @experimental
+ */
+class ProductActionViewFactory
+{
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Product\Product $product
+     * @param string $absoluteUrl
+     * @return \Shopsys\ReadModelBundle\Product\Action\ProductActionViewInterface
+     */
+    public function createFromProduct(Product $product, string $absoluteUrl): ProductActionViewInterface
+    {
+        return new ProductActionView(
+            $product->getId(),
+            $product->isSellingDenied(),
+            $product->isMainVariant(),
+            $absoluteUrl
+        );
+    }
+}

shopsys/monorepo-tools

packages/read-model/src/Image/ImageViewFactory.php
@@ -1,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\ReadModelBundle\Image;
+
+use Shopsys\FrameworkBundle\Component\Image\Image;
+
+/**
+ * @experimental
+ */
+class ImageViewFactory
+{
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\Image\Image $image
+     * @return \Shopsys\ReadModelBundle\Image\ImageView
+     */
+    public function createFromImage(Image $image): ImageView
+    {
+        return new ImageView(
+            $image->getId(),
+            $image->getExtension(),
+            $image->getEntityName(),
+            $image->getType()
+        );
+    }
+}

packages/read-model/src/Product/Action/ProductActionViewFactory.php
@@ -1,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\ReadModelBundle\Product\Action;
+
+use Shopsys\FrameworkBundle\Model\Product\Product;
+
+/**
+ * @experimental
+ */
+class ProductActionViewFactory
+{
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Product\Product $product
+     * @param string $absoluteUrl
+     * @return \Shopsys\ReadModelBundle\Product\Action\ProductActionView
+     */
+    public function createFromProduct(Product $product, string $absoluteUrl): ProductActionView
+    {
+        return new ProductActionView(
+            $product->getId(),
+            $product->isSellingDenied(),
+            $product->isMainVariant(),
+            $absoluteUrl
+        );
+    }
+}