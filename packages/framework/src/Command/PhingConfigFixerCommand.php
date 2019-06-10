bdc353200ac09c06fe7cd7da10f76d664a1e078c
Petr Heinz <petr.heinz@shopsys.com>
2019-06-10 15:17:57 +0200

    rewrite of shopsys:phing-targets:sort command (#1116)
    - renamed to shopsys:phing-config:fix
    - the class was renamed to PhingConfigFixerCommand
    - the phing targets were renamed to phing-config-check and phing-config-fix
    - SymfonyStyle is used for input / output
    - return codes are used instead of throwing Exceptions (RuntimeExceptions are still thrown for actual errors)
    - messages were tweaked for better understandability
    - the command also normalizes the whitespace in XML

--- packages/framework/src/Command/PhingConfigFixerCommand.php
+++ packages/framework/src/Command/PhingConfigFixerCommand.php
@@ -1,59 +1,85 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Shopsys\FrameworkBundle\Command;
 
-use Exception;
+use RuntimeException;
 use SimpleXMLElement;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
-class SortPhingTargetsCommand extends Command
+class PhingConfigFixerCommand extends Command
 {
+    protected const RETURN_CODE_OK = 0;
+    protected const RETURN_CODE_ERROR = 1;
     protected const ARG_XML_PATH = 'xml';
     protected const OPTION_ONLY_CHECK = 'check';
 
     /**
      * @var string
      */
-    protected static $defaultName = 'shopsys:phing-targets:sort';
+    protected static $defaultName = 'shopsys:phing-config:fix';
 
-    protected function configure()
+    protected function configure(): void
     {
         $this
-            ->setDescription('Sort Phing targets alphabetically')
+            ->setDescription('Fixes syntax of Phing configuration automatically (sorts targets alphabetically and normalizes whitespace).')
             ->addArgument(static::ARG_XML_PATH, InputArgument::REQUIRED | InputArgument::IS_ARRAY, 'Path(-s) to the Phing XML configuration')
-            ->addOption(static::OPTION_ONLY_CHECK, null, InputOption::VALUE_NONE, 'Will not modify the XML, only fail if the output would be different');
+            ->addOption(static::OPTION_ONLY_CHECK, null, InputOption::VALUE_NONE, 'Will not modify the XML, only fails if the output would be different');
     }
 
     /**
      * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return int
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $returnCode = static::RETURN_CODE_OK;
+        $io = new SymfonyStyle($input, $output);
+
+        $checkOnly = $input->getOption(static::OPTION_ONLY_CHECK);
         $paths = $input->getArgument(static::ARG_XML_PATH);
         foreach ($paths as $path) {
             $content = file_get_contents($path);
 
-            $sortedContent = $this->sortTargetBlocks($content);
+            $sortedContent = $this->fixConfiguration($content);
 
-            if (!$input->getOption(static::OPTION_ONLY_CHECK)) {
+            $isContentChanged = $content !== $sortedContent;
+            if ($checkOnly && $isContentChanged) {
+                $returnCode = static::RETURN_CODE_ERROR;
+
+                $io->error(sprintf('The Phing configuration in "%s" in not OK.', $path));
+            } elseif ($isContentChanged) {
                 file_put_contents($path, $sortedContent);
-            } elseif ($content !== $sortedContent) {
-                throw new Exception('The targets are not alphabetically sorted. Re-run the command without the "' . static::OPTION_ONLY_CHECK . '" option to fix it.');
+
+                $io->success(sprintf('The Phing configuration in "%s" was fixed.', $path));
             }
         }
+
+        if ($returnCode === static::RETURN_CODE_OK) {
+            $io->success('All Phing configuration files are OK.');
+        } elseif ($returnCode === static::RETURN_CODE_ERROR) {
+            $io->error('Some Phing configuration files are not OK.');
+
+            $io->comment(sprintf('Re-run the command without the "%s" option to fix it automatically.', static::OPTION_ONLY_CHECK));
+        }
+
+        return $returnCode;
     }
 
     /**
      * @param string $content
      * @return string
      */
-    protected function sortTargetBlocks(string $content): string
+    protected function fixConfiguration(string $content): string
     {
+        $content = $this->normalizeXml($content);
         $targetBlocks = $this->extractTargetBlocksIndexedByName($content);
         $content = $this->replaceTargetsByPlaceholders($content, $targetBlocks);
         $content = $this->normalizeWhitespaceBetweenPlaceholders($content);
@@ -63,6 +89,17 @@ class SortPhingTargetsCommand extends Command
         $content = $this->replacePlaceholdersByTargets($content, $targetBlocks);
 
         return $content;
+    }
+
+    /**
+     * @param string $content
+     * @return string
+     */
+    protected function normalizeXml(string $content): string
+    {
+        $xml = new SimpleXMLElement($content);
+
+        return $xml->asXML();
     }
 
     /**
@@ -115,7 +152,7 @@ class SortPhingTargetsCommand extends Command
             $replacedContent = str_replace($targetBlock, $targetPlaceholder, $content);
 
             if ($content === $replacedContent) {
-                throw new Exception("Target block was not found in the original XML, probably because of unexpected formatting:\n\n" . $targetBlock);
+                throw new RuntimeException("This block was not found in the XML content and could not be replaced:\n\n" . $targetBlock);
             }
 
             $content = $replacedContent;
@@ -135,8 +172,15 @@ class SortPhingTargetsCommand extends Command
         foreach ($targetBlocks as $targetBlock) {
             $targetPlaceholder = $this->getTargetPlaceholder($position++);
 
-            $content = str_replace($targetPlaceholder, $targetBlock, $content);
+            $replacedContent = str_replace($targetPlaceholder, $targetBlock, $content);
+
+            if ($content === $replacedContent) {
+                throw new RuntimeException(sprintf('The placeholder for target #%d was not found in the XML content and could not be replaced.', $position));
+            }
+
+            $content = $replacedContent;
         }
+
         return $content;
     }
 }
