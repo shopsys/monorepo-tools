pesovo/shopsys

added shopsys/api bundle
- working endpoints
    - /api/v1/products (includes filter by uuids, pagination)
    - /api/v1/products/{uuid}
- using FOSRestBundle
- for finding products from facade is used query so the code is prepared for further extension by extending the ProductQuery and the query method ProductRepository::findByQuery()
- routes in project-base must be added for each version - that is a compromise between "routes works automatically when I add the bundle" and "user have to add all routes manually"
- api extension works form project-base in a way "what is extended is taken from project-base; what is not extended is taken from framework"

packages/api/src/Component/HeaderLinks/HeaderLinksTransformer.php
@@ -1,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\ApiBundle\Component\HeaderLinks;
+
+use Shopsys\FrameworkBundle\Component\Paginator\PaginationResult;
+use Symfony\Component\HttpFoundation\Request;
+
+class HeaderLinksTransformer
+{
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\Paginator\PaginationResult $paginationResult
+     * @param string $baseUrl
+     * @return \Shopsys\ApiBundle\Component\HeaderLinks\HeaderLinks
+     */
+    public function fromPaginationResult(PaginationResult $paginationResult, string $baseUrl): HeaderLinks
+    {
+        $links = new HeaderLinks();
+
+        if (!$paginationResult->isFirst()) {
+            $firstUrl = $this->changeParameter($baseUrl, 'page', '1');
+            $previousUrl = $this->changeParameter($baseUrl, 'page', (string)$paginationResult->getPrevious());
+
+            $links = $links
+                ->add($firstUrl, 'first')
+                ->add($previousUrl, 'prev');
+        }
+
+        if (!$paginationResult->isLast()) {
+            $nextUrl = $this->changeParameter($baseUrl, 'page', (string)$paginationResult->getNext());
+            $lastUrl = $this->changeParameter($baseUrl, 'page', (string)$paginationResult->getPageCount());
+
+            $links = $links
+                ->add($nextUrl, 'next')
+                ->add($lastUrl, 'last');
+        }
+
+        return $links;
+    }
+
+    /**
+     * @param string $baseUrl
+     * @param string $parameter
+     * @param string $value
+     * @return string
+     */
+    protected function changeParameter(string $baseUrl, string $parameter, string $value): string
+    {
+        return Request::create($baseUrl, 'GET', [$parameter => $value])->getUri();
+    }
+}

packages/framework/src/Model/Product/ProductFacade.php
@@ -5,6 +5,7 @@ namespace Shopsys\FrameworkBundle\Model\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Shopsys\FrameworkBundle\Component\Domain\Domain;
 use Shopsys\FrameworkBundle\Component\Image\ImageFacade;
+use Shopsys\FrameworkBundle\Component\Paginator\PaginationResult;
 use Shopsys\FrameworkBundle\Component\Plugin\PluginCrudExtensionFacade;
 use Shopsys\FrameworkBundle\Component\Router\FriendlyUrl\FriendlyUrlFacade;
 use Shopsys\FrameworkBundle\Model\Pricing\Group\PricingGroupRepository;
@@ -210,6 +211,15 @@ class ProductFacade
     public function getById($productId)
     {
         return $this->productRepository->getById($productId);
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Product\ProductQuery $query
+     * @return \Shopsys\FrameworkBundle\Component\Paginator\PaginationResult
+     */
+    public function findByQuery(ProductQuery $query): PaginationResult
+    {
+        return $this->productRepository->findByQuery($query);
     }
 
     /**
@@ -425,5 +435,14 @@ class ProductFacade
     public function getOneByCatnumExcludeMainVariants($productCatnum)
     {
         return $this->productRepository->getOneByCatnumExcludeMainVariants($productCatnum);
+    }
+
+    /**
+     * @param string $uuid
+     * @return \Shopsys\FrameworkBundle\Model\Product\Product
+     */
+    public function getByUuid(string $uuid): Product
+    {
+        return $this->productRepository->getOneByUuid($uuid);
     }
 }

packages/framework/src/Model/Product/ProductRepository.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\ORM\QueryBuilder;
 use Shopsys\FrameworkBundle\Component\Doctrine\QueryBuilderExtender;
+use Shopsys\FrameworkBundle\Component\Paginator\PaginationResult;
 use Shopsys\FrameworkBundle\Component\Paginator\QueryPaginator;
 use Shopsys\FrameworkBundle\Model\Category\Category;
 use Shopsys\FrameworkBundle\Model\Localization\Localization;
@@ -752,5 +753,37 @@ class ProductRepository
         }
 
         return $product;
+    }
+
+    /**
+     * @param string $uuid
+     * @return \Shopsys\FrameworkBundle\Model\Product\Product
+     */
+    public function getOneByUuid(string $uuid): Product
+    {
+        $product = $this->getProductRepository()->findOneBy(['uuid' => $uuid]);
+
+        if ($product === null) {
+            throw new \Shopsys\FrameworkBundle\Model\Product\Exception\ProductNotFoundException('Product with UUID ' . $uuid . ' does not exist.');
+        }
+
+        return $product;
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Product\ProductQuery $query
+     * @return \Shopsys\FrameworkBundle\Component\Paginator\PaginationResult
+     */
+    public function findByQuery(ProductQuery $query): PaginationResult
+    {
+        $queryBuilder = $this->getProductRepository()->createQueryBuilder('p');
+        $queryBuilder->orderBy('p.id');
+        if ($query->getUuids()) {
+            $queryBuilder->andWhere('p.uuid IN (:uuids)');
+            $queryBuilder->setParameter(':uuids', $query->getUuids());
+        }
+
+        $queryPaginator = new QueryPaginator($queryBuilder);
+        return $queryPaginator->getResult($query->getPage(), $query->getPageSize());
     }
 }

shopsys/monorepo-tools

packages/backend-api/src/Component/HeaderLinks/HeaderLinksTransformer.php
@@ -1,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopsys\BackendApiBundle\Component\HeaderLinks;
+
+use Shopsys\FrameworkBundle\Component\Paginator\PaginationResult;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @experimental
+ */
+class HeaderLinksTransformer
+{
+    /**
+     * @param \Shopsys\FrameworkBundle\Component\Paginator\PaginationResult $paginationResult
+     * @param string $baseUrl
+     * @return \Shopsys\BackendApiBundle\Component\HeaderLinks\HeaderLinks
+     */
+    public function fromPaginationResult(PaginationResult $paginationResult, string $baseUrl): HeaderLinks
+    {
+        $links = new HeaderLinks();
+
+        if (!$paginationResult->isFirstPage()) {
+            $firstUrl = $this->createUrlWithParameter($baseUrl, 'page', '1');
+            $previousUrl = $this->createUrlWithParameter($baseUrl, 'page', (string)$paginationResult->getPreviousPage());
+
+            $links = $links
+                ->add($firstUrl, 'first')
+                ->add($previousUrl, 'prev');
+        }
+
+        if (!$paginationResult->isLastPage()) {
+            $nextUrl = $this->createUrlWithParameter($baseUrl, 'page', (string)$paginationResult->getNextPage());
+            $lastUrl = $this->createUrlWithParameter($baseUrl, 'page', (string)$paginationResult->getPageCount());
+
+            $links = $links
+                ->add($nextUrl, 'next')
+                ->add($lastUrl, 'last');
+        }
+
+        return $links;
+    }
+
+    /**
+     * @param string $baseUrl
+     * @param string $parameter
+     * @param string $value
+     * @return string
+     */
+    protected function createUrlWithParameter(string $baseUrl, string $parameter, string $value): string
+    {
+        return Request::create($baseUrl, 'GET', [$parameter => $value])->getUri();
+    }
+}

packages/framework/src/Model/Product/ProductFacade.php
@@ -1,10 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Shopsys\FrameworkBundle\Model\Product;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Shopsys\FrameworkBundle\Component\Domain\Domain;
 use Shopsys\FrameworkBundle\Component\Image\ImageFacade;
+use Shopsys\FrameworkBundle\Component\Paginator\PaginationResult;
 use Shopsys\FrameworkBundle\Component\Plugin\PluginCrudExtensionFacade;
 use Shopsys\FrameworkBundle\Component\Router\FriendlyUrl\FriendlyUrlFacade;
 use Shopsys\FrameworkBundle\Model\Pricing\Group\PricingGroupRepository;
@@ -210,6 +213,15 @@ class ProductFacade
     public function getById($productId)
     {
         return $this->productRepository->getById($productId);
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Product\ProductQueryParams $query
+     * @return \Shopsys\FrameworkBundle\Component\Paginator\PaginationResult
+     */
+    public function findByProductQueryParams(ProductQueryParams $query): PaginationResult
+    {
+        return $this->productRepository->findByProductQueryParams($query);
     }
 
     /**
@@ -441,5 +453,14 @@ class ProductFacade
     public function getOneByCatnumExcludeMainVariants($productCatnum)
     {
         return $this->productRepository->getOneByCatnumExcludeMainVariants($productCatnum);
+    }
+
+    /**
+     * @param string $uuid
+     * @return \Shopsys\FrameworkBundle\Model\Product\Product
+     */
+    public function getByUuid(string $uuid): Product
+    {
+        return $this->productRepository->getOneByUuid($uuid);
     }
 }

packages/framework/src/Model/Product/ProductRepository.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\ORM\QueryBuilder;
 use Shopsys\FrameworkBundle\Component\Doctrine\QueryBuilderExtender;
+use Shopsys\FrameworkBundle\Component\Paginator\PaginationResult;
 use Shopsys\FrameworkBundle\Component\Paginator\QueryPaginator;
 use Shopsys\FrameworkBundle\Model\Category\Category;
 use Shopsys\FrameworkBundle\Model\Localization\Localization;
@@ -752,5 +753,37 @@ class ProductRepository
         }
 
         return $product;
+    }
+
+    /**
+     * @param string $uuid
+     * @return \Shopsys\FrameworkBundle\Model\Product\Product
+     */
+    public function getOneByUuid(string $uuid): Product
+    {
+        $product = $this->getProductRepository()->findOneBy(['uuid' => $uuid]);
+
+        if ($product === null) {
+            throw new \Shopsys\FrameworkBundle\Model\Product\Exception\ProductNotFoundException('Product with UUID ' . $uuid . ' does not exist.');
+        }
+
+        return $product;
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Product\ProductQueryParams $query
+     * @return \Shopsys\FrameworkBundle\Component\Paginator\PaginationResult
+     */
+    public function findByProductQueryParams(ProductQueryParams $query): PaginationResult
+    {
+        $queryBuilder = $this->getProductRepository()->createQueryBuilder('p');
+        $queryBuilder->orderBy('p.id');
+        if ($query->getUuids()) {
+            $queryBuilder->andWhere('p.uuid IN (:uuids)');
+            $queryBuilder->setParameter(':uuids', $query->getUuids());
+        }
+
+        $queryPaginator = new QueryPaginator($queryBuilder);
+        return $queryPaginator->getResult($query->getPage(), $query->getPageSize());
     }
 }