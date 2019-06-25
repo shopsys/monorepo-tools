7f74c52c22cefb51f7fd7e56292cac6c1d783224
Petr Heinz <petr.heinz@shopsys.com>
2019-06-25 13:24:19 +0200

    OrderProductFacadeTest refactored
    - the test was a bit hard to read originally
    - strict typing enabled
    - creation of OrderProductFacade moved to setUp() method
    - extracted methods createProductWithStockQuantity() and createOrderItem() for DRY
    - Product::$outOfStockAction set to Product::OUT_OF_STOCK_ACTION_HIDE for more consistency with model (avoiding unexpected NULL)
    - some variables were inlined or renamed for better readability

--- packages/framework/tests/Unit/Model/Order/Item/OrderProductFacadeTest.php
+++ packages/framework/tests/Unit/Model/Order/Item/OrderProductFacadeTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\FrameworkBundle\Unit\Model\Order\Item;
 
 use Doctrine\ORM\EntityManager;
@@ -18,64 +20,24 @@ use Shopsys\FrameworkBundle\Model\Product\ProductHiddenRecalculator;
 use Shopsys\FrameworkBundle\Model\Product\ProductSellingDeniedRecalculator;
 use Shopsys\FrameworkBundle\Model\Product\ProductVisibilityFacade;
 
-class OrderProductFacadeTest extends TestCase
+final class OrderProductFacadeTest extends TestCase
 {
-    public function testSubtractOrderProductsFromStockUsingStock()
-    {
-        $productStockQuantity = 15;
-        $orderProductQuantity = 10;
-
-        $orderMock = $this->createMock(Order::class);
-
-        $productData = new ProductData();
-        $productData->usingStock = true;
-        $productData->stockQuantity = $productStockQuantity;
-        $product = Product::create($productData, new ProductCategoryDomainFactory(new EntityNameResolver([])));
-        $productPrice = Price::zero();
-
-        $orderProduct = new OrderItem($orderMock, 'productName', $productPrice, 0, $orderProductQuantity, OrderItem::TYPE_PRODUCT, null, null);
-        $orderProduct->setProduct($product);
-
-        $orderProductFacade = $this->createOrderProductFacade();
-        $orderProductFacade->subtractOrderProductsFromStock([$orderProduct]);
-
-        $this->assertSame($productStockQuantity - $orderProductQuantity, $product->getStockQuantity());
-    }
-
-    public function testAddOrderProductsToStockUsingStock()
-    {
-        $productStockQuantity = 15;
-        $orderProductQuantity = 10;
-
-        $orderMock = $this->createMock(Order::class);
-
-        $productData = new ProductData();
-        $productData->usingStock = true;
-        $productData->stockQuantity = $productStockQuantity;
-        $product = Product::create($productData, new ProductCategoryDomainFactory(new EntityNameResolver([])));
-        $productPrice = Price::zero();
-
-        $orderProduct = new OrderItem($orderMock, 'productName', $productPrice, 0, $orderProductQuantity, OrderItem::TYPE_PRODUCT, null, null);
-        $orderProduct->setProduct($product);
-
-        $orderProductFacade = $this->createOrderProductFacade();
-        $orderProductFacade->addOrderProductsToStock([$orderProduct]);
-
-        $this->assertSame($productStockQuantity + $orderProductQuantity, $product->getStockQuantity());
-    }
-
     /**
-     * @return \Shopsys\FrameworkBundle\Model\Order\Item\OrderProductFacade
+     * @var \Shopsys\FrameworkBundle\Model\Order\Item\OrderProductFacade
      */
-    private function createOrderProductFacade()
+    private $orderProductFacade;
+
+    protected function setUp(): void
     {
+        parent::setUp();
+
         $moduleFacadeMock = $this->getMockBuilder(ModuleFacade::class)
             ->setMethods(['isEnabled'])
             ->disableOriginalConstructor()
             ->getMock();
         $moduleFacadeMock->expects($this->any())->method('isEnabled')->willReturn(true);
 
-        return new OrderProductFacade(
+        $this->orderProductFacade = new OrderProductFacade(
             $this->createMock(EntityManager::class),
             $this->createMock(ProductHiddenRecalculator::class),
             $this->createMock(ProductSellingDeniedRecalculator::class),
@@ -83,5 +45,69 @@ class OrderProductFacadeTest extends TestCase
             $this->createMock(ProductVisibilityFacade::class),
             $moduleFacadeMock
         );
+    }
+
+    public function testSubtractOrderProductsFromStockUsingStock(): void
+    {
+        $stockQuantity = 15;
+        $orderedQuantity = 10;
+        $expectedStockQuantity = 5;
+
+        $product = $this->createProductWithStockQuantity($stockQuantity);
+        $orderProduct = $this->createOrderItem($product, $orderedQuantity);
+
+        $this->orderProductFacade->subtractOrderProductsFromStock([$orderProduct]);
+
+        $this->assertSame($expectedStockQuantity, $product->getStockQuantity());
+    }
+
+    public function testAddOrderProductsToStockUsingStock(): void
+    {
+        $stockQuantity = 15;
+        $orderedQuantity = 10;
+        $expectedStockQuantity = 25;
+
+        $product = $this->createProductWithStockQuantity($stockQuantity);
+        $orderProduct = $this->createOrderItem($product, $orderedQuantity);
+
+        $this->orderProductFacade->addOrderProductsToStock([$orderProduct]);
+
+        $this->assertSame($expectedStockQuantity, $product->getStockQuantity());
+    }
+
+    /**
+     * @param int $productStockQuantity
+     * @return \Shopsys\FrameworkBundle\Model\Product\Product
+     */
+    private function createProductWithStockQuantity(int $productStockQuantity): Product
+    {
+        $productData = new ProductData();
+        $productData->usingStock = true;
+        $productData->stockQuantity = $productStockQuantity;
+        $productData->outOfStockAction = Product::OUT_OF_STOCK_ACTION_HIDE;
+
+        return Product::create($productData, new ProductCategoryDomainFactory(new EntityNameResolver([])));
+    }
+
+    /**
+     * @param \Shopsys\FrameworkBundle\Model\Product\Product $product
+     * @param int $orderProductQuantity
+     * @return \Shopsys\FrameworkBundle\Model\Order\Item\OrderItem
+     */
+    private function createOrderItem(Product $product, int $orderProductQuantity): OrderItem
+    {
+        $orderProduct = new OrderItem(
+            $this->createMock(Order::class),
+            'productName',
+            Price::zero(),
+            '0',
+            $orderProductQuantity,
+            OrderItem::TYPE_PRODUCT,
+            null,
+            null
+        );
+        $orderProduct->setProduct($product);
+
+        return $orderProduct;
     }
 }
