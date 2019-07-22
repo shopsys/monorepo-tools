e7800a3041151493557cdbe2eeac570f6b00bb57
Petr Heinz <petr.heinz@shopsys.com>
2019-07-22 08:30:56 +0200

    DomainController: fix non-existing route (#1231)

--- packages/framework/src/Controller/Admin/DomainController.php
+++ packages/framework/src/Controller/Admin/DomainController.php
@@ -82,7 +82,7 @@ class DomainController extends AdminBaseController
 
         $referer = $request->server->get('HTTP_REFERER');
         if ($referer === null) {
-            return $this->redirectToRoute('admin_dashboard');
+            return $this->redirectToRoute('admin_default_dashboard');
         } else {
             return $this->redirect($referer);
         }
