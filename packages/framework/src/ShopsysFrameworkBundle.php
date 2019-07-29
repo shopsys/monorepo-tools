1753c0ab9fbc1585adab928e8f2c4807359440db
Petr Heinz <petr.heinz@shopsys.com>
2019-07-29 08:34:18 +0000

    ShopsysFrameworkBundle: version updated to "8.0.0"

--- packages/framework/src/ShopsysFrameworkBundle.php
+++ packages/framework/src/ShopsysFrameworkBundle.php
@@ -15,7 +15,7 @@ class ShopsysFrameworkBundle extends Bundle
     /**
      * @var string
      */
-    public const VERSION = '8.0.0-dev';
+    public const VERSION = '8.0.0';
 
     /**
      * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
